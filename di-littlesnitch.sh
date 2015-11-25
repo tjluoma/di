46835aa3f38bfcfaf20c6646b896d79c5024b5dc
TJ Luoma <luomat@gmail.com>
2015-11-25 11:24:33 -0500

    Initial Commit

---
+++ di-littlesnitch.sh
@@ -0,0 +1,67 @@
+#!/bin/zsh -f
+# Purpose: 
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2015-11-25
+
+NAME="$0:t:r"
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+INSTALL_TO='/Applications/Little Snitch Configuration.app'
+
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '3'`
+
+INFO=($(curl -sfL https://sw-update.obdev.at/update-feeds/littlesnitch3.plist \
+| egrep -A1 'BundleShortVersionString|DownloadURL' \
+| fgrep '<string>' \
+| tail -2 \
+| sed 's#.*<string>##g; s#</string>##g'))
+
+LATEST_VERSION="$INFO[1]"
+URL="$INFO[2]"
+
+autoload is-at-least
+
+is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+if [ "$?" = "0" ]
+then
+	echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	exit 0
+fi
+
+echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+FILENAME="$HOME/Downloads/LittleSnitch-$LATEST_VERSION.dmg"
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
+		| fgrep -A 1 '<key>mount-point</key>' \
+		| tail -1 \
+		| sed 's#</string>.*##g ; s#.*<string>##g')
+
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+fi
+
+open "$MNTPNT/Little Snitch Installer.app"
+
+exit 0
+#EOF
