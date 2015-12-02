5b8a5ca0523b6d05b1b9e110a00bed53e71eb25c
TJ Luoma <luomat@gmail.com>
2015-12-02 14:41:34 -0500

    XML feed outdated, try checking download URL instead

--- di-subtitles.sh
+++ di-subtitles.sh
@@ -1,10 +1,13 @@
-#!/bin/zsh
+#!/bin/zsh -f
 # Purpose:
 #
 # From:	Tj Luo.ma
 # Mail:	luomat at gmail dot com
 # Web: 	http://RhymesWithDiploma.com
 # Date:	2015-05-01
+
+## 2015-12-02 - Because the XML file is out of date, I'm actually checking to see where 'http://subtitlesapp.com/download/' redirects
+## 				and stripping the URL down to the version number 
 
 NAME="$0:t:r"
 
@@ -15,16 +18,36 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
+URL=`curl -sfL --head 'http://subtitlesapp.com/download/' | awk -F' ' '/^Location:/{print $2}' | tail -1 | tr -d '\r'`
 
-INFO=($(curl -sfL 'http://subtitlesapp.com/updates.xml' \
-| fgrep '<enclosure ' \
-| head -1 ))
+if [[ "$URL" == "" ]] 
+then
+			# 2015-12-02 the XML file is out of date 
 
-DOWNLOAD_URL=`echo "$INFO[2]" | sed 's#url="##g ; s#"##g'`
+	LATEST_VERSION=`curl -sfL 'http://subtitlesapp.com/updates.xml' \
+		| tidy --input-xml yes --output-xml yes --show-warnings no --force-output yes --quiet yes --wrap 0 \
+		| sed 's#&lt;#<#g ; s#&gt;#>#g' \
+		| fgrep '<title>' \
+		| head -1 \
+		| tr -dc '[0-9].'`
 
-LATEST_VERSION=`echo "$INFO[3]" | sed 's#sparkle:version="##g ; s#"##g'`
+	URL="http://subtitlesapp.com/download/Subtitles-mac-$LATEST_VERSION.zip"
 
-INSTALLED_VERSION=`defaults read /Applications/Subtitles.app/Contents/Info CFBundleShortVersionString 2>/dev/null`
+else
+	LATEST_VERSION=`echo "$URL:t:r" | tr -dc '[0-9].'`
+fi 
+
+STATUS=`curl -sfL --head "$URL" | awk -F' ' '/^HTTP/{print $2}'`
+
+if [[ "$STATUS" != "200" ]]
+then
+	echo "$NAME: Bad HTTP Status for $URL ($STATUS != 200)"
+	exit 0
+fi 
+
+INSTALL_TO='/Applications/Subtitles.app'
+
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null`
 
 autoload is-at-least
 
@@ -44,7 +67,8 @@ cd '/Volumes/Data/Websites/iusethis.luo.ma/subtitles/' 2>/dev/null \
 	|| cd "$HOME/Desktop/" 2>/dev/null \
 	|| cd "$HOME/"
 
-FILENAME="$PWD/$DOWNLOAD_URL:t"
+
+FILENAME="$PWD/Subtitles-$LATEST_VERSION.zip"
 
 if [[ -e "$FILENAME" ]]
 then
@@ -69,12 +93,12 @@ fi
 pgrep -xq Subtitles \
 && osascript -e 'tell application "Subtitles" to quit'
 
-if [[ -e "/Applications/Subtitles.app" ]]
+if [[ -e "$INSTALL_TO" ]]
 then
-	mv -f /Applications/Subtitles.app "$HOME/.Trash/Subtitles.$INSTALLED_VERSION.app"
+	mv -f "$INSTALL_TO" "$HOME/.Trash/Subtitles.$INSTALLED_VERSION.app"
 fi
 
-ditto --noqtn -xk "$FILENAME:t" /Applications
+ditto --noqtn -xk "$FILENAME:t" "$INSTALL_TO:h"
 
 exit 0
 #
