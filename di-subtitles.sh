f198fa86af6ffdb07d707ed465069c8bac1d81d0
TJ Luoma <luomat@gmail.com>
2015-12-04 19:00:36 -0500

    use proper variable name URL not DOWNLOAD_URL

--- di-subtitles.sh
+++ di-subtitles.sh
@@ -1,13 +1,10 @@
 #!/bin/zsh -f
-# Purpose:
+# Purpose: Download and install latest version of Subtitlesapp.com
 #
 # From:	Tj Luo.ma
 # Mail:	luomat at gmail dot com
 # Web: 	http://RhymesWithDiploma.com
 # Date:	2015-05-01
-
-## 2015-12-02 - Because the XML file is out of date, I'm actually checking to see where 'http://subtitlesapp.com/download/' redirects
-## 				and stripping the URL down to the version number 
 
 NAME="$0:t:r"
 
@@ -18,36 +15,35 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-URL=`curl -sfL --head 'http://subtitlesapp.com/download/' | awk -F' ' '/^Location:/{print $2}' | tail -1 | tr -d '\r'`
-
-if [[ "$URL" == "" ]] 
-then
-			# 2015-12-02 the XML file is out of date 
-
-	LATEST_VERSION=`curl -sfL 'http://subtitlesapp.com/updates.xml' \
-		| tidy --input-xml yes --output-xml yes --show-warnings no --force-output yes --quiet yes --wrap 0 \
-		| sed 's#&lt;#<#g ; s#&gt;#>#g' \
-		| fgrep '<title>' \
-		| head -1 \
-		| tr -dc '[0-9].'`
-
-	URL="http://subtitlesapp.com/download/Subtitles-mac-$LATEST_VERSION.zip"
-
-else
-	LATEST_VERSION=`echo "$URL:t:r" | tr -dc '[0-9].'`
-fi 
-
-STATUS=`curl -sfL --head "$URL" | awk -F' ' '/^HTTP/{print $2}'`
-
-if [[ "$STATUS" != "200" ]]
-then
-	echo "$NAME: Bad HTTP Status for $URL ($STATUS != 200)"
-	exit 0
-fi 
-
 INSTALL_TO='/Applications/Subtitles.app'
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null`
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo 3`
+
+	# XML file is... unusual.
+INFO=($(curl -sfL 'http://subtitlesapp.com/updates.xml' \
+| tidy --input-xml yes --output-xml yes --show-warnings no --force-output yes --quiet yes --wrap 0 \
+| sed 's#&lt;#<#g ; s#&gt;#>#g ' \
+| fgrep 'sparkle:version=' \
+| head -1 \
+| tr -s ' ' '\012' \
+| sort \
+| egrep 'sparkle:version=|url=' \
+| awk -F'"' '/^/{print $2}'))
+
+	# "Sparkle" will always come before "url" because of "sort"
+LATEST_VERSION="$INFO[1]"
+URL="$INFO[2]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+		echo 
+
+	URL=`curl -sfL --head 'http://subtitlesapp.com/download/' | awk -F' ' '/^Location:/{print $2}' | tail -1 | tr -d '\r'`
+
+	LATEST_VERSION=`echo "$URL:t:r" | tr -dc '[0-9].'`
+
+fi
 
 autoload is-at-least
 
@@ -61,19 +57,27 @@ fi
 
 echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
+
+STATUS=`curl -sfL --head "$URL" | awk -F' ' '/^HTTP/{print $2}'`
+
+if [[ "$STATUS" != "200" ]]
+then
+	echo "$NAME: Bad HTTP Status for $URL ($STATUS != 200)"
+	exit 0
+fi 
+
 cd '/Volumes/Data/Websites/iusethis.luo.ma/subtitles/' 2>/dev/null \
 	|| cd "$HOME/BitTorrent Sync/iusethis.luo.ma/subtitles/" 2>/dev/null \
 	|| cd "$HOME/Downloads/" 2>/dev/null \
 	|| cd "$HOME/Desktop/" 2>/dev/null \
 	|| cd "$HOME/"
 
-
 FILENAME="$PWD/Subtitles-$LATEST_VERSION.zip"
 
 if [[ -e "$FILENAME" ]]
 then
-	echo "$DOWNLOAD_URL"
-	curl --continue-at - --fail --location --progress-bar --output "$FILENAME" "$DOWNLOAD_URL"
+	echo "$URL"
+	curl --continue-at - --fail --location --progress-bar --output "$FILENAME" "$URL"
 
 else
 	# if filename does not exist in CWD
@@ -84,8 +88,8 @@ else
 		mv -vf *.zip old/ 2>/dev/null || true
 	fi
 
-	echo "$DOWNLOAD_URL"
-	curl -fL --progress-bar --output "$FILENAME" "$DOWNLOAD_URL"
+	echo "$URL"
+	curl -fL --progress-bar --output "$FILENAME" "$URL"
 fi
 
 
