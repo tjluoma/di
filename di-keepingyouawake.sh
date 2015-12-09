f9f86b1e6d793fba08d07dfa5b083e7648331c65
TJ Luoma <luomat@gmail.com>
2015-12-09 10:57:42 -0500

    Initial Commit

---
+++ di-keepingyouawake.sh
@@ -0,0 +1,100 @@
+#!/bin/zsh -f
+# Purpose: 
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2015-12-08
+
+NAME="$0:t:r"
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+INSTALL_TO='/Applications/KeepingYouAwake.app'
+
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '1.0.0'`
+
+
+
+XML_FEED='http://newmarcel.github.io/KeepingYouAwake/appcast.xml'
+
+INFO=($(curl -sfL "$XML_FEED" \
+| tr -s ' ' '\012' \
+| egrep 'sparkle:shortVersionString=|url=' \
+| head -2 \
+| sort \
+| awk -F'"' '/^/{print $2}'))
+
+	# "Sparkle" will always come before "url" because of "sort"
+LATEST_VERSION="$INFO[1]"
+URL="$INFO[2]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:\nINFO: $INFO"
+	exit 0
+fi
+
+autoload is-at-least
+
+is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+if [ "$?" = "0" ]
+then
+	echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	exit 0
+fi
+
+echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+FILENAME="$HOME/Downloads/KeepingYouAwake-${LATEST_VERSION}.zip"
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+
+if [ -e "$INSTALL_TO" ]
+then
+		# Quit app, if running
+	pgrep -xq "KeepingYouAwake" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "KeepingYouAwake" to quit'
+
+		# move installed version to trash 
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/KeepingYouAwake.$INSTALLED_VERSION.app"
+fi
+
+echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+
+	# Extract from the .zip file and install (this will leave the .zip file in place)
+ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+	
+	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
+	
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+fi
+
+
+
+
+
+exit 0
+#EOF
