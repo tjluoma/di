0a0d4e41be8c55e40224c7ae48bce1dac2605cf2
TJ Luoma <luomat@gmail.com>
2016-03-09 11:03:54 -0500

    overall improvements

--- di-iterm-nightly.sh
+++ di-iterm-nightly.sh
@@ -12,65 +12,103 @@ NAME="$0:t:r"
 
 INSTALL_TO='/Applications/iTerm.app'
 
-CURRENT=($(curl -sfL https://iterm2.com/appcasts/nightly.xml \
-	| tr '[:blank:]' '\012' \
-	| tr '"' ' ' \
-	| egrep 'sparkle:version|https://iterm2.com/downloads/nightly/iTerm2.*.zip' \
-	| sort -u \
-	| awk '{print $NF}'))
-
-LATEST_VERSION=`echo "$CURRENT[1]" | sed 's#-nightly##g'`
-
-DOWNLOAD_ACTUAL="$CURRENT[2]"
-
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null | tr -dc '[0-9].' || echo '2.0.0'`
-
-
-autoload is-at-least
-
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
-
-if [ "$?" = "0" ]
+if [ -e "$INSTALL_TO" ]
 then
-	echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+		# if the app is installed, check to see what version we are running 
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null | tr -dc '[0-9].'`
+else
+		# if it isn't installed, just use a default old version number to force an 'update'
+	 INSTALLED_VERSION='2.0.0'
+fi
+
+#    SUFeedURLForFinal = "https://iterm2.com/appcasts/final.xml";
+#    SUFeedURLForTesting = "https://iterm2.com/appcasts/nightly.xml";
+
+XML_FEED='https://iterm2.com/appcasts/nightly.xml'
+
+INFO=($(curl -sfL "$XML_FEED" \
+| tr -s ' ' '\012' \
+| egrep 'sparkle:version=|url=' \
+| head -2 \
+| sort \
+| awk -F'"' '/^/{print $2}'))
+
+	# "Sparkle" will always come before "url" because of "sort"
+LATEST_VERSION="$INFO[1]"
+URL="$INFO[2]"
+
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:\nINFO: $INFO"
 	exit 0
 fi
 
+	# Remote '-nightly' from version string from XML feed 
+	# because it is not used in CFBundleShortVersionString
+LATEST_VERSION=`echo "$LATEST_VERSION" | sed 's#-nightly##g'`
+
+ if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+ then
+ 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+ 	exit 0
+ fi
+	
+autoload is-at-least
+
+ is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+ 
+ if [ "$?" = "0" ]
+ then
+ 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+ 	exit 0
+ fi
+
 echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
-curl --continue-at - --max-time 3600 --fail --location --referer ";auto" --progress-bar --remote-name "$DOWNLOAD_ACTUAL"
+FILENAME="$HOME/Downloads/iTerm-${LATEST_VERSION}.zip"
 
-FILENAME="$HOME/Downloads/iTerm-$LATEST_VERSION.zip"
+echo "$NAME: Downloading $URL to $FILENAME"
 
-	# Unzip download to the current directory
-ditto --noqtn -xkv "$FILENAME" .
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
 
 if [ -e "$INSTALL_TO" ]
 then
-	# iTerm is already installed
-	mv -vn "$INSTALL_TO" "$HOME/.Trash/iTerm.$INSTALLED_VERSION.app"
+		# DON'T QUIT if running, or we might terminate ourselves! 
+		# Just move to the trash. The app will still work. Next time it's launched 
+		# it will launch new version 
+		
+		# move installed version to trash 
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/iTerm.$INSTALLED_VERSION.app"
 fi
 
-if [ -e "$INSTALL_TO" ]
+echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+
+	# Extract from the .zip file and install (this will leave the .zip file in place)
+ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
 then
-	# iTerm is STILL installed? Something went wrong
-
-	MSG="iTerm could not be removed from $INSTALL_TO"
-
-	echo "$NAME: $MSG" | tee -a "$HOME/Desktop/$NAME.error.log"
-
-	(( $+commands[growlnotify] )) && pgrep -xq Growl && growlnotify --sticky --appIcon "iTerm" --identifier "$NAME" --message "$MSG" --title "$NAME"
-
-	exit 1
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+	
+	(( $+commands[growlnotify] )) \
+	&& pgrep -xq Growl \
+	&& growlnotify --sticky --appIcon "iTerm" --identifier "$NAME" --message "Updated iTerm to $LATEST_VERSION" --title "$NAME"
+		
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
 fi
 
-mv -vn iTerm.app "$INSTALL_TO" \
-	&& MSG="Updated iTerm to $LATEST_VERSION" \
-	&& echo "$NAME: $MSG" \
-	&& (( $+commands[growlnotify] )) && pgrep -xq Growl && growlnotify --sticky --appIcon "iTerm" --identifier "$NAME" --message "$MSG" --title "$NAME"
+exit 0
 
-
-
-exit
 #
 #EOF
