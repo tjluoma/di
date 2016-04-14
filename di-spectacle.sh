49a29757e51f2a6eca5ed0a1ecbe5773d2e3223a
TJ Luoma <luomat@gmail.com>
2016-04-14 18:42:47 -0400

    no version installed = 0

--- di-spectacle.sh
+++ di-spectacle.sh
@@ -16,7 +16,7 @@ fi
 
 INSTALL_TO='/Applications/Spectacle.app'
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '1'`
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
 XML_FEED="https://spectacleapp.com/updates/appcast.xml"
 
