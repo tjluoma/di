243aece7126fb224e15be9f0949ed0d5d20ef547
TJ Luoma <luomat@m1.local>
2016-05-08 21:45:36 -0400

    Set version to zero if not installed

--- di-transmission.sh
+++ di-transmission.sh
@@ -16,7 +16,7 @@ fi
 
 INSTALL_TO='/Applications/Transmission.app'
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '2'`
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
 XML_FEED='http://update.transmissionbt.com/appcast.xml'
 
