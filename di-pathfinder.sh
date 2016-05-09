94bad6f53b1f17662711cb4d51962ee5f5cf22b5
TJ Luoma <luomat@m1.local>
2016-05-08 21:49:52 -0400

    Set version to zero if not installed

--- di-pathfinder.sh
+++ di-pathfinder.sh
@@ -16,7 +16,7 @@ fi
 
 INSTALL_TO='/Applications/Path Finder.app'
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '7.0'`
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
 XML_FEED='http://sparkle.cocoatech.com/PF7.xml'
 
