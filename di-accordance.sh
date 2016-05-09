848d53e59391df24c4d5ddd9771af7b19aaf4eaa
TJ Luoma <luomat@m1.local>
2016-05-08 21:27:40 -0400

    Set version to zero if not installed

--- di-accordance.sh
+++ di-accordance.sh
@@ -26,7 +26,7 @@ LATEST_VERSION="$INFO[2]"
 
 INSTALL_TO='/Applications/Accordance.app'
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '11.0.0'`
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
  if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
  then
