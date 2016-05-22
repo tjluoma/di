30e84510039fda82610f38e9a378ad842a79c141
TJ Luoma <luomat@m1.luo.ma>
2016-05-22 05:11:16 -0400

    new XML_FEED; no longer using cache

--- di-xquartz.sh
+++ di-xquartz.sh
@@ -9,6 +9,8 @@
 # GENERAL_URL='https://xquartz.macosforge.org/landing/'
 # DOWNLOAD_URL='http://xquartz.macosforge.org/trac/wiki/Releases'
 
+## 2016-05-22 - new XML_FEED is much faster, so no longer using cache 
+
 NAME="$0:t:r"
 
 zmodload zsh/datetime
@@ -19,71 +21,17 @@ INSTALL_TO='/Applications/Utilities/XQuartz.app'
 
 INSTALLED_VERSION=`defaults read "${INSTALL_TO}/Contents/Info.plist" "CFBundleVersion" 2>/dev/null || echo '0'`
 
-CACHE='/usr/local/install/xquartz.txt'
-
 ########################################################################################################################
 
-function update_cache {
+XML_FEED="https://www.xquartz.org/releases/sparkle/release.xml"
 
-	rm -f "$CACHE"
+INFO=($(curl -sfL "$XML_FEED" | tr -s ' ' '\012' | egrep 'sparkle:version=|url=' | head -2 | awk -F'"' '/^/{print $2}'))
 
-	UPDATES_URL='http://xquartz.macosforge.org/downloads/sparkle/release.xml'
+URL="$INFO[1]"
 
-	echo "$NAME: Updating cache at `timestamp`"
-
-	INFO=($(curl -sfL "$UPDATES_URL" | tr -s ' ' '\012' | egrep 'sparkle:version=|url=' | head -2 | awk -F'"' '/^/{print $2}'))
-
-	echo "$NAME: cache update finished at `timestamp`"
-
-	DOWNLOAD_ACTUAL="$INFO[1]"
-
-	LATEST_VERSION="$INFO[2]"
-
-	if [ "$LATEST_VERSION" = "" -o "$DOWNLOAD_ACTUAL" = "" ]
-	then
-		echo "$NAME: Failed to update cache (no response from $UPDATES_URL)"
-		exit 0
-	else
-
-		echo "$EPOCHSECONDS $LATEST_VERSION $DOWNLOAD_ACTUAL" > "$CACHE"
-	fi
-
-}
+LATEST_VERSION="$INFO[2]"
 
 ########################################################################################################################
-
-if [ -e "$CACHE" ]
-then
-
-	CACHE_TIME=`awk '{print $1}' "$CACHE"`
-
-	DIFF=$(($EPOCHSECONDS - $CACHE_TIME))
-
-	if [ "$DIFF" -gt "86400" ]
-	then
-			# If we get here the cache needs to be updated
-		echo "$NAME: Updating $CACHE"
-		update_cache
-	else
-			# if we get here, we can use the cached values
-
-			echo "$NAME: Using cache at $CACHE"
-
-			DOWNLOAD_ACTUAL=`awk '{print $3}' "$CACHE"`
-
-			LATEST_VERSION=`awk '{print $2}' "$CACHE"`
-	fi
-
-else
-	update_cache
-fi
-
-
-
-
-########################################################################################################################
-
-
 
 FILENAME="XQuartz-$LATEST_VERSION.dmg"
 
@@ -145,46 +93,11 @@ autoload is-at-least
 echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
 
-
-
-# if [ "$INSTALLED_VERSION" = "$LATEST_VERSION" ]
-# then
-# 		log "$INSTALL_TO:t is up-to-date ($INSTALLED_VERSION ~ $LATEST_VERSION)"
-# 		exit 0
-# else
-# 			# Update Needed
-# 		msgs "$INSTALL_TO:t is outdated ($INSTALLED_VERSION vs $LATEST_VERSION)"
-# fi
-
 cd "$HOME/Downloads" || cd "$HOME/Desktop" || cd "$HOME" || cd /tmp
 
-##
-## 2016-03-19 - the size checking code seems to causing an infinite loop
-##
-# REMOTE_SIZE=`curl -sL --head "${DOWNLOAD_ACTUAL}" | awk -F' ' '/Content-Length/{print $NF}'| tr -dc '[0-9]'`
-#
-#zmodload zsh/stat
-##
-# function get_local_size
-# {
-#
-# 	LOCAL_SIZE=$(zstat -L +size "$FILENAME" 2>/dev/null)
-#
-# }
-#
-# get_local_size
-#
-# while [ "$LOCAL_SIZE" -lt "$REMOTE_SIZE" ]
-# do
-#
-# 	curl -C - --max-time 3600 --fail --location --referer ";auto" --progress-bar --remote-name "${DOWNLOAD_ACTUAL}"
-#
-# 	get_local_size
-#
-# done
 
-echo "$NAME: Saving $DOWNLOAD_ACTUAL to $PWD/$FILENAME"
-curl -C - --max-time 3600 --fail --location --referer ";auto" --progress-bar --output "${FILENAME}" "${DOWNLOAD_ACTUAL}"
+echo "$NAME: Saving $URL to $PWD/$FILENAME"
+curl -C - --max-time 3600 --fail --location --referer ";auto" --progress-bar --output "${FILENAME}" "${URL}"
 
 
 [[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
@@ -209,7 +122,7 @@ then
 	exit 0
 fi
 
-msgs "Installing $PKG (this may take awhile...)"
+msgs "Installing $PKG (this will take awhile...)"
 
 sudo installer -verbose -pkg "$PKG" -target / -lang en 2>&1 | tee -a "$LOG"
 
