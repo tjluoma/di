e3a44e9287f111174c5015af84a4771e1e2e3f99
TJ Luoma <luomat@m1.luo.ma>
2016-05-22 05:02:05 -0400

    fixed typo

--- di-xquartz.sh
+++ di-xquartz.sh
@@ -15,9 +15,9 @@ zmodload zsh/datetime
 
 function timestamp { strftime "%Y-%m-%d--%H.%M.%S" "$EPOCHSECONDS" }
 
-INSATLL_TO='/Applications/Utilities/XQuartz.app'
+INSTALL_TO='/Applications/Utilities/XQuartz.app'
 
-INSTALLED_VERSION=`defaults read "${INSATLL_TO}/Contents/Info.plist" "CFBundleVersion" 2>/dev/null || echo '0'`
+INSTALLED_VERSION=`defaults read "${INSTALL_TO}/Contents/Info.plist" "CFBundleVersion" 2>/dev/null || echo '0'`
 
 CACHE='/usr/local/install/xquartz.txt'
 
@@ -149,11 +149,11 @@ echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSIO
 
 # if [ "$INSTALLED_VERSION" = "$LATEST_VERSION" ]
 # then
-# 		log "$INSATLL_TO:t is up-to-date ($INSTALLED_VERSION ~ $LATEST_VERSION)"
+# 		log "$INSTALL_TO:t is up-to-date ($INSTALLED_VERSION ~ $LATEST_VERSION)"
 # 		exit 0
 # else
 # 			# Update Needed
-# 		msgs "$INSATLL_TO:t is outdated ($INSTALLED_VERSION vs $LATEST_VERSION)"
+# 		msgs "$INSTALL_TO:t is outdated ($INSTALLED_VERSION vs $LATEST_VERSION)"
 # fi
 
 cd "$HOME/Downloads" || cd "$HOME/Desktop" || cd "$HOME" || cd /tmp
