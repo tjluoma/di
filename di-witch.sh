b64219032c583dabf6a5f2dbfbff4e7f4fd98a64
TJ Luoma <luomat@m1.luo.ma>
2016-05-22 21:12:28 -0400

    Initial Commit

---
+++ di-witch.sh
@@ -0,0 +1,103 @@
+#!/bin/zsh -f
+# Purpose: 
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2016-05-22
+
+NAME="$0:t:r"
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+INSTALL_TO="$HOME/Library/PreferencePanes/Witch.PrefPane"
+
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+
+XML_FEED='https://manytricks.com/witch/appcast.xml'
+
+INFO=($(curl -sfL "$XML_FEED" \
+| tr -s ' ' '\012' \
+| egrep 'sparkle:shortVersionString=|url=' \
+| head -2 \
+| sort \
+| awk -F'"' '/^/{print $2}'))
+
+	# "Sparkle" will always come before "url" because of "sort"
+LATEST_VERSION="$INFO[1]"
+URL="$INFO[2]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:\nINFO: $INFO"
+	exit 0
+fi
+
+
+if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+then
+	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+	exit 0
+fi
+
+autoload is-at-least
+
+is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+if [ "$?" = "0" ]
+then
+	echo "$NAME: Up-To-Date ($LATEST_VERSION)"
+	exit 0
+fi
+
+echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+FILENAME="$HOME/Downloads/Witch-${LATEST_VERSION}.dmg"
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+
+if [ -e "$INSTALL_TO" ]
+then
+		# Quit app, if running
+	pgrep -xq "witchdaemon" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "witchdaemon" to quit'
+
+		# move installed version to trash 
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/Witch.$INSTALLED_VERSION.prefPane"
+fi
+
+MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
+		| fgrep -A 1 '<key>mount-point</key>' \
+		| tail -1 \
+		| sed 's#</string>.*##g ; s#.*<string>##g')
+
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+fi
+
+ditto --noqtn -v "$MNTPNT/Witch.prefPane" "$INSTALL_TO" \
+&& diskutil eject "$MNTPNT"
+
+
+exit 0
+#EOF
