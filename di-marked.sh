6e4db2fc2b1892f59e887fbb1a4a852e49ce7925
TJ Luoma <luomat@m1.luo.ma>
2016-05-26 18:54:01 -0400

    replaces di-marked2

--- di-marked.sh
+++ di-marked.sh
@@ -3,7 +3,7 @@
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
-# Date:	2015-11-10
+# Date:	2016-03-30
 
 NAME="$0:t:r"
 
@@ -17,8 +17,6 @@ fi
 INSTALL_TO='/Applications/Marked 2.app'
 
 INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-
-# XML_FEED="http://abyss.designheresy.com/marked/marked.xml"
 
 XML_FEED="https://updates.marked2app.com/marked.xml"
 
@@ -40,29 +38,41 @@ then
 	exit 0
 fi
 
-FILENAME="$HOME/Downloads/Marked-${LATEST_VERSION}.zip"
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+then
+	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+	exit 0
+fi
 
 autoload is-at-least
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+if [ "$?" = "0" ]
+then
+	echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	exit 0
+fi
 
 echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+
+FILENAME="$HOME/Downloads/Marked-${LATEST_VERSION}.zip"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
 
 if [ -e "$INSTALL_TO" ]
 then
@@ -75,9 +85,24 @@ then
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/Marked 2.$INSTALLED_VERSION.app"
 fi
 
+
+
 echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
 
+	# Extract from the .zip file and install (this will leave the .zip file in place)
 ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+
+	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
+
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+fi
 
 
 
