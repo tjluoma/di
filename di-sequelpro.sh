1718157766adecee98481a45800304bc81d6d48f
a_galli <andrea.gallivanoni@gmail.com>
2016-05-29 16:07:38 +0200

    adds sequel pro script

---
+++ di-sequelpro.sh
@@ -0,0 +1,90 @@
+#!/bin/zsh -f
+# Purpose: 
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2015-11-14
+
+NAME="$0:t:r"
+APPNAME="Sequel Pro"
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+INSTALL_TO="/Applications/$APPNAME.app"
+echo $INSTALL_TO
+
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+BUILD_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo 600000`
+
+FEED_URL="https://www.sequelpro.com/appcast/app-releases.xml"
+
+INFO=($(curl -sfL "$FEED_URL" \
+| tr -s ' ' '\012' \
+| egrep 'sparkle:shortVersionString=|url=' \
+| head -2 \
+| awk -F'"' '/^/{print $2}'))
+
+URL="$INFO[1]"
+LATEST_VERSION="$INFO[2]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:\nINFO: $INFO"
+	exit 0
+fi
+
+ if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+ then
+ 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+ 	exit 0
+ fi
+
+autoload is-at-least
+
+ is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+ 
+ if [ "$?" = "0" ]
+ then
+ 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+ 	exit 0
+ fi
+
+echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+FILENAME="$HOME/Downloads/${APPNAME//[[:space:]]/}-$LATEST_VERSION.dmg"
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
+		| fgrep -A 1 '<key>mount-point</key>' \
+		| tail -1 \
+		| sed 's#</string>.*##g ; s#.*<string>##g')
+
+if [ -e "$INSTALL_TO" ]
+then
+		# Quit app, if running
+	pgrep -xq "$APPNAME" && pkill "$APPNAME"
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$APPNAME.$INSTALLED_VERSION.app"
+fi
+
+echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+
+# ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+
+ditto "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
+
+diskutil eject "$MNTPNT"
+
+
+exit 0
+#EOF
