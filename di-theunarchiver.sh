e3889b4a517e625a6159433ccd2e542bf01d94a9
a_galli <andrea.gallivanoni@gmail.com>
2016-05-29 13:01:42 +0200

    adds The Unarchiver

---
+++ di-theunarchiver.sh
@@ -0,0 +1,96 @@
+#!/bin/zsh -f
+# Purpose: 
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2016-01-19
+
+NAME="$0:t:r"
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+INSTALL_TO='/Applications/The Unarchiver.app'
+
+
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+BUILD_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo 600000`
+
+FEED_URL="https://unarchiver.c3.cx/updates.rss"
+
+# INFO=($(curl -sfL $FEED_URL \
+# | tr ' ' '\012' \
+# | egrep '^(url|sparkle:shortVersionString)=' \
+# | head -2 \
+# | awk -F'"' '//{print $2}'))
+
+INFO=($(curl -sfL $FEED_URL \
+| tr '[:space:]' '\012' \
+| egrep '^(url|sparkle:shortVersionString)=' \
+| tail -2 \
+| awk -F'"' '{print $2}' \
+))
+
+URL="$INFO[1]"
+
+LATEST_VERSION="$INFO[2]"
+
+
+if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+ then
+ 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+ 	exit 0
+fi
+
+autoload is-at-least
+
+is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+if [ "$?" = "0" ]
+ then
+ 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+ 	exit 0
+ fi
+ 
+ echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+
+FILENAME="$HOME/Downloads/Screens-${LATEST_VERSION}.zip"
+
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+
+echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+
+	# Extract from the .zip file and install (this will leave the .zip file in place)
+ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+	
+	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
+	
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+fi
+
+
+
+
+exit 0
+EOF
