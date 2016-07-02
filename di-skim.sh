fd895094d259db5b1049a2e6a14cbd6625b33164
a_galli <andrea.gallivanoni@gmail.com>
2016-07-02 14:32:52 +0200

    adds restart to Skim after installation

--- di-skim.sh
+++ di-skim.sh
@@ -68,11 +68,8 @@ MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 
 if [ -e "$INSTALL_TO" ]
 then
-		# Quit app, if running
-	pgrep -xq "$APPNAME" && pkill "$APPNAME"
-
-		# move installed version to trash
-	mv -vf "$INSTALL_TO" "$HOME/.Trash/$APPNAME.$INSTALLED_VERSION.app"
+	pgrep -qx "$APPNAME" && LAUNCH='yes' && killall "$APPNAME"
+	mv -f "$INSTALL_TO" "$HOME/.Trash/$APPNAME.$INSTALLED_VERSION.app"
 fi
 
 echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
@@ -80,6 +77,18 @@ echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
 # ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
 
 ditto "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+	
+	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
+	
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+fi
 
 diskutil eject "$MNTPNT"
 
