acff515ad9df879429c2291e428f61f5770334e1
TJ Luoma <github@tjluoma.com>
2018-08-02 16:02:03 -0400

    minor code cleanup and improvements

--- di-littlesnitch.sh
+++ di-littlesnitch.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of LittleSnitch
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2015-11-25
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Little Snitch Configuration.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -14,46 +16,60 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO='/Applications/Little Snitch Configuration.app'
+XML_FEED="https://sw-update.obdev.at/update-feeds/littlesnitch3.plist"
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '0'`
-
-INFO=($(curl -sfL https://sw-update.obdev.at/update-feeds/littlesnitch3.plist \
-| egrep -A1 'ReleaseLifecycle|BundleVersion|DownloadURL' \
-| fgrep '<string>' \
-| awk '{if (NR % 3) {printf("%s ", $0)} else printf("%s\n", $0) }' \
-| fgrep 'final' \
-| tr ' ' '\n' \
-| grep -v 'final' \
-| tail -2 \
-| sed 's#.*<string>##g; s#</string>##g'))
+INFO=($(curl -sfL "$XML_FEED" \
+		| egrep -A1 'BundleVersion|DownloadURL' \
+		| fgrep '<string>' \
+		| tail -2 \
+		| sed 's#.*<string>##g; s#</string>##g'))
 
 LATEST_VERSION="$INFO[1]"
+
 URL="$INFO[2]"
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
-autoload is-at-least
+	exit 1
+fi
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+if [[ -e "$INSTALL_TO" ]]
+then
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '0'`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
 
 FILENAME="$HOME/Downloads/LittleSnitch-$LATEST_VERSION.dmg"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
@@ -68,15 +84,15 @@ MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 if [[ "$MNTPNT" == "" ]]
 then
 	echo "$NAME: MNTPNT is empty"
-	exit 0
+	exit 1
 fi
 
 
-	# The installer requires user interaction, so we can't automate that part.
+	# Unfortunately, the installer _requires_ human interaction, so we can't automate that part.
 	# ¯\_(ツ)_/¯
 	# Fortunately it will handle moving the existing installation, etc
-	# so that's handy 
-	
+	# so that's handy
+
 open "$MNTPNT/Little Snitch Installer.app"
 
 exit 0
