d57e4c1959081a1af18b098377850df17b117f05
TJ Luoma <github@tjluoma.com>
2018-08-02 13:05:09 -0400

    Updated for changes in host XML_FEED, minor cleanup

--- di-macpilot.sh
+++ di-macpilot.sh
@@ -1,52 +1,97 @@
-#!/bin/zsh
-# Purpose:
+#!/bin/zsh -f
+# Purpose: Download and install the latest version of MacPilot @TODO - update this?
 #
 # From:	Tj Luo.ma
 # Mail:	luomat at gmail dot com
 # Web: 	http://RhymesWithDiploma.com
 # Date:	2015-10-27
 
+
 NAME="$0:t:r"
+
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
+fi
 
 INSTALL_TO='/Applications/MacPilot.app'
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo 0`
+	# The latest version is 10.1.1 so we want to fake at least version 10
+INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo 10`
 
 OS_VER=`sw_vers -productVersion`
 
-INFO=($(curl -sfL "http://www.koingosw.com/postback/versioncheck.php?appname=macpilot&appversion=${INSTALLED_VERSION}&sysplatform=Mac%20OS%20X&sysversion=Mac%20OS%20X%20${OS_VER}" \
-| tr -s ' ' '\012' \
-| egrep '^(url|sparkle:version)' \
-| awk -F'"' '//{print $2}'))
+XML_FEED="http://www.koingosw.com/postback/versioncheck.php?appname=macpilot&appversion=${INSTALLED_VERSION}&sysplatform=Mac%20OS%20X&sysversion=Mac%20OS%20X%20${OS_VER}"
+
+INFO=($(curl --silent --location "$XML_FEED" \
+		| fgrep -A3 '<macpilot>' \
+		| egrep '(<version>|<macpath>)' \
+		| sort \
+		| sed 's#.*<version>##g ; s#</version>##g; s#.*<macpath>##g; s#</macpath>##g; '
+	))
+
+## 2018-08-02 - this is old
+##
+# INFO=($(curl -sfL "$OS_URL" \
+# | tr -s ' ' '\012' \
+# | egrep '^(url|sparkle:version)' \
+# | awk -F'"' '//{print $2}'))
 
 URL="$INFO[1]"
 
 LATEST_VERSION="$INFO[2]"
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	OS_URL: $OS_URL
+	"
+
+	exit 1
+fi
+
+if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+then
+	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+	exit 0
+fi
 
 autoload is-at-least
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+if [ "$?" = "0" ]
+then
+	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+	exit 0
+fi
 
 echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
 
 FILENAME="$HOME/Downloads/MacPilot-$LATEST_VERSION.dmg"
 
-echo "$NAME: Downloading version $LATEST_VERSION to $FILENAME"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+echo "$NAME: Mounting $FILENAME:"
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 		| fgrep -A 1 '<key>mount-point</key>' \
@@ -69,23 +114,20 @@ then
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
 fi
 
+echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
 
-echo "$NAME: Installing $FILENAME to $INSTALL_TO"
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
 
-ditto --noqtn "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
+EXIT="$?"
 
-if (( $+commands[unmount.sh] ))
+if [[ "$EXIT" != "0" ]]
 then
+	echo "$NAME: ditto failed"
 
-	unmount.sh "$MNTPNT"
-
-else
-
-	diskutil eject "$MNTPNT"
-
+	exit 1
 fi
 
-
+diskutil eject "$MNTPNT"
 
 exit 0
 #
