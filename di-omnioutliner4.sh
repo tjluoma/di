a36ef6480c3b754bd5e955f61dd2491f29f2b831
TJ Luoma <github@tjluoma.com>
2018-08-02 13:18:31 -0400

    renamed from di-omnioutliner.sh (note v5 is available)

---
+++ di-omnioutliner4.sh
@@ -0,0 +1,116 @@
+#!/bin/zsh -f
+# Purpose: Download and install OmniOutliner 4 (note that version 5 is now available)
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2015-10-26
+
+NAME="$0:t:r"
+
+INSTALL_TO='/Applications/OmniOutliner.app'
+
+LAUNCH='no'
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
+fi
+
+## Note: Downloads are available in tbz2 and dmg but dmg has EULA so I use tbz2
+
+# Don't indent or you'll break sed
+INFO=($(curl -sfL "http://update.omnigroup.com/appcast/com.omnigroup.OmniOutliner4/" 2>&1 \
+| sed 's#<#\
+<#g' \
+| tr -s ' ' '\012' \
+| egrep '<omniappcast:marketingVersion>4|url=.*\.tbz2' \
+| head -2 \
+| tr -s '>|"' ' ' \
+| awk '{print $NF}'))
+
+LATEST_VERSION="$INFO[1]"
+
+URL="$INFO[2]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info"  CFBundleShortVersionString 2>/dev/null || echo '0'`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
+
+FILENAME="$HOME/Downloads/OmniOutliner-$LATEST_VERSION.tbz2"
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+
+
+if [ -e "$INSTALL_TO" ]
+then
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/OmniOutliner.$INSTALLED_VERSION.app"
+fi
+
+echo "$NAME: Installing $FILENAME to $INSTALL_TO"
+
+tar -x -C "$INSTALL_TO:h" -f "$FILENAME"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+
+	echo "$NAME: Installation of $INSTALL_TO successful"
+	exit 0
+
+else
+	echo "$NAME: tar failed (\$EXIT = $EXIT)"
+
+	exit 1
+fi
+
+exit 0
+
+#EOF
