97774df98a8164a7fbdaf6d4295c4ba8ff87930c
TJ Luoma <github@tjluoma.com>
2018-08-02 16:07:55 -0400

    improved error checking

--- di-namemangler.sh
+++ di-namemangler.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Download and install Name Mangler
+# Purpose: Download and install Name Mangler
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2015-09-28
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Name Mangler.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -14,71 +16,119 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
+XML_FEED='https://manytricks.com/namemangler/appcast/?version=3'
 
-INFO=($(curl -A Safari -sfL 'http://manytricks.com/namemangler/appcast.xml' | tr -s ' ' '\012' | egrep '^(url|sparkle:shortVersionString)=' | head -2 | awk -F'"' '//{print $2}'))
+UA='Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_7; en-us) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1'
+
+INFO=($(curl -A "$UA" -sfL "$XML_FEED" \
+	| tr -s ' ' '\012' \
+	| egrep '^(url|sparkle:shortVersionString)=' \
+	| head -2 \
+	| awk -F'"' '//{print $2}' ))
 
 URL="$INFO[1]"
 
 LATEST_VERSION="$INFO[2]"
 
-INSTALL_TO="/Applications/Name Mangler.app"
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+	exit 1
+fi
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
 
-autoload is-at-least
+if [[ -e "$INSTALL_TO" ]]
+then
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString`
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
 
 FILENAME="$HOME/Downloads/NameMangler-$LATEST_VERSION.dmg"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
-	# server doesn't like curl 
-curl -A Safari --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+	# server doesn't like curl
+curl -A "$UA" --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download failed (EXIT = $EXIT)" && exit 0
 
 if [ -e "$INSTALL_TO" ]
 then
-		# Quit app, if running
-	pgrep -xq "$INSTALL_TO:t:r" && osascript -e 'tell application "Name Mangler" to quit'
-
 		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
 fi
-
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 		| fgrep -A 1 '<key>mount-point</key>' \
 		| tail -1 \
 		| sed 's#</string>.*##g ; s#.*<string>##g')
 
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+fi
+
+if [ -e "$INSTALL_TO" ]
+then
+		# Quit app, if running
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
+fi
+
 echo "$NAME: Installing $MNTPNT/$INSTALL_TO:t to $INSTALL_TO"
 
-ditto -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+	exit 0
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	exit 1
+fi
 
 if (( $+commands[unmount.sh] ))
 then
-
 	unmount.sh "$MNTPNT"
 else
-	diskutil eject "$MNTPNT" 
+	diskutil eject "$MNTPNT"
 fi
-
-
 
 exit 0
 #
