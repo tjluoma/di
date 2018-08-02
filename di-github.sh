e49d9f5319c744ab0998b230b77c069f9d267a36
TJ Luoma <github@tjluoma.com>
2018-08-02 14:26:39 -0400

    minor improvements to error checking

--- di-github.sh
+++ di-github.sh
@@ -10,13 +10,21 @@ NAME="$0:t:r"
 
 INSTALL_TO='/Applications/GitHub Desktop.app'
 
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
+fi
+
 INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '0'`
 
 OS_VER=`sw_vers -productVersion || echo '10.11'`
 
 	# If you are up to date, you get an empty document with a 204 response code
 
-RESPONSE=`curl --head -sfL "https://central.github.com/api/mac/latest?version=${INSTALLED_VERSION}&os_version=${OS_VER}" | awk -F' ' '/^HTTP/{print $2}'`
+RESPONSE=`curl --head -sfL "https://central.github.com/api/mac/latest?version=${INSTALLED_VERSION}&os_version=${OS_VER}" \
+			| awk -F' ' '/^HTTP/{print $2}'`
 
 if [[ "$RESPONSE" == "204" ]]
 then
@@ -25,20 +33,32 @@ then
 fi
 
 INFO=($(curl -sfL "https://central.github.com/api/mac/latest?version=${INSTALLED_VERSION}&os_version=${OS_VER}" \
-| tr ',' '\012' \
-| egrep '"version"|"url"' \
-| head -2 \
-| awk -F'"' '//{print $4}'))
+		| tr ',' '\012' \
+		| egrep '"version"|"url"' \
+		| head -2 \
+		| awk -F'"' '//{print $4}'))
 
 LATEST_VERSION="$INFO[1]"
 
 URL="$INFO[2]"
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+then
+	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+	exit 0
+fi
 
 autoload is-at-least
 
@@ -56,8 +76,7 @@ FILENAME="$HOME/Downloads/GitHub-$LATEST_VERSION.zip"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
-
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 if [ -e "$INSTALL_TO" ]
 then
@@ -75,6 +94,21 @@ fi
 echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
 
 ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+
+	echo "$NAME: Update/installation of $INSTALL_TO was successful"
+
+else
+	echo "$NAME: ditto failed (\$EXIT = $EXIT)"
+
+	exit 1
+fi
+
 
 exit 0
 #
