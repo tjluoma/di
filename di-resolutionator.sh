bebc7b4201fd088f67ceffc90af64e8e9fe8a2bb
TJ Luoma <github@tjluoma.com>
2018-08-02 15:56:50 -0400

    error checking improvements, switched to https

--- di-resolutionator.sh
+++ di-resolutionator.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of Resolutionator
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2015-11-19
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Resolutionator.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -14,11 +16,9 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO='/Applications/Resolutionator.app'
+XML_FEED='https://manytricks.com/resolutionator/appcast.xml'
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-
-XML_FEED='http://manytricks.com/resolutionator/appcast.xml'
+## sparkle:version exists in feed but does not seem important
 
 INFO=($(curl -A Safari -sfL "$XML_FEED" \
 | tr -s ' ' '\012' \
@@ -34,27 +34,39 @@ URL="$INFO[2]"
 	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
 fi
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+if [[ -e "$INSTALL_TO" ]]
+then
 
-autoload is-at-least
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
 
 FILENAME="$HOME/Downloads/Resolutionator-${LATEST_VERSION}.dmg"
 
@@ -85,11 +97,27 @@ then
 	&& LAUNCH='yes' \
 	&& osascript -e 'tell application "Resolutionator" to quit'
 
-		# move installed version to trash 
+		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/Resolutionator.$INSTALLED_VERSION.app"
 fi
 
-ditto -v "$MNTPNT/Resolutionator.app" "$INSTALL_TO" 
+echo "$NAME: Installing \"$MNTPNT/Resolutionator.app\" to $INSTALL_TO"
+
+ditto --noqtn -v "$MNTPNT/Resolutionator.app" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+
+	[[ "$LAUNCH" = "yes" ]] && open -b com.manytricks.Resolutionator
+
+	exit 0
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	exit 1
+fi
 
 if (( $+commands[unmount.sh] ))
 then
