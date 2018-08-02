c10afd1366a02657697adf394f5cd4f778f3ac20
TJ Luoma <github@tjluoma.com>
2018-08-02 16:03:31 -0400

    code improvements, better error catching

--- di-vellum.sh
+++ di-vellum.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh -f
-# Purpose:
+# Purpose: Download and install the latest version of Vellum
 #
 # From:	Tj Luo.ma
 # Mail:	luomat at gmail dot com
@@ -12,72 +12,115 @@ DIR="$HOME/Downloads"
 
 INSTALL_TO='/Applications/Vellum.app'
 
-INSTALLED_VERSION=`defaults read ${INSTALL_TO}/Contents/Info CFBundleShortVersionString 2>/dev/null || echo 0`
+XML_FEED="https://get.180g.co/updates/vellum/"
 
-LATEST_VERSION=`curl -sfL "https://get.180g.co/updates/vellum/" | tr -s ' ' '\012' | awk -F'"' '/shortVersionString/{print $2}'`
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
+fi
 
-zmodload zsh/datetime
 
-TIME=$(strftime "%Y-%m-%d-at-%H.%M.%S" "$EPOCHSECONDS")
+INFO=($(curl -sfL "$XML_FEED" \
+	| tr ' ' '\012' \
+	| egrep '^(url|sparkle:shortVersionString|sparkle:version)=' \
+	| head -3 \
+	| sort \
+	| awk -F'"' '//{print $2}'))
 
-HOST=`hostname -s`
-HOST="$HOST:l"
+LATEST_VERSION="$INFO[1]"
 
-LOG="$HOME/Library/Logs/$NAME.$HOST.log"
+LATEST_BUILD="$INFO[2]"
 
-[[ -d "$LOG:h" ]] || mkdir -p "$LOG:h"
-[[ -e "$LOG" ]]   || touch "$LOG"
+URL="$INFO[3]"
 
-function timestamp { strftime "%Y-%m-%d at %H:%M:%S" "$EPOCHSECONDS" }
-function log { echo "$NAME [`timestamp`]: $@" | tee -a "$LOG" }
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Bad data from $XML_FEED
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	LATEST_BUILD: $LATEST_BUILD
+	URL: $URL
+	"
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+	exit 1
+fi
 
-autoload is-at-least
+if [[ -e "$INSTALL_TO" ]]
+then
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	INSTALLED_BUILD=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	if [ "$LATEST_VERSION" = "$INSTALLED_VERSION" -a "$LATEST_BUILD" = "$INSTALLED_BUILD" ]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION/$INSTALLED_BUILD)"
+		exit 0
+	fi
 
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	is-at-least "$LATEST_BUILD" "$INSTALLED_BUILD"
+
+	BUILD_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" -a "$BUILD_COMPARE" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION/$INSTALLED_BUILD) is ahead of official version $LATEST_VERSION/$LATEST_BUILD"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION/$INSTALLED_BUILD vs $LATEST_VERSION/$LATEST_BUILD"
+fi
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 #
 #		This is where we do the actual download
 #
 
-DL_URL=`curl -sfL "https://get.180g.co/updates/vellum/" | tr -s ' ' '\012' | awk -F'"' '/^url/{print $2}'`
+FILENAME="$HOME/Downloads/Vellum-${LATEST_VERSION}-${LATEST_BUILD}.zip"
 
-DL_URL=`echo $DL_URL | sed 's#https:/180g#https://180g#g'`
+	# Download the latest zip
+echo "$NAME: Downloading $URL to $FILENAME"
 
-FILENAME="$DIR/Vellum-$LATEST_VERSION.zip"
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
-echo "$NAME: Saving $DL_URL to $FILENAME"
+EXIT="$?"
 
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$DL_URL" 2>/dev/null
-
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
 [[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
 [[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && exit 0
 
-
-if [ -e "$INSTALL_TO" ]
+if [[ -e "$INSTALL_TO" ]]
 then
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/Vellum.$INSTALLED_VERSION.app"
 fi
 
-ditto --noqtn -v -xk "$FILENAME" "$INSTALL_TO:h"
+echo "$NAME: Installing $FILENAME to $INSTALL_TO:h"
 
-exit
+ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+	exit 0
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	exit 1
+fi
+
+exit 0
+
 #
 #EOF
