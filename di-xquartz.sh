371a3b08a71d7c6adaedfd679aba24eb199b9f99
TJ Luoma <github@tjluoma.com>
2018-08-02 15:50:06 -0400

    improved XML_FEED, updated code, cleanup, error checking

--- di-xquartz.sh
+++ di-xquartz.sh
@@ -1,39 +1,63 @@
 #!/bin/zsh -f
-# Download and install (or update) Xquartz
+# Purpose: Download and install (or update) Xquartz
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2014-06-10
 #
-## In case you need them:
-# GENERAL_URL='https://xquartz.macosforge.org/landing/'
-# DOWNLOAD_URL='http://xquartz.macosforge.org/trac/wiki/Releases'
-
-## 2016-05-22 - new XML_FEED is much faster, so no longer using cache 
+# Updated 2018-07-17 - updated some of the code, removed the 'cache' feature since it is no longer needed,
+# and update the XML_FEED to new URL thanks to the Homebrew Cask folks.
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Utilities/XQuartz.app'
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
+fi
 
 zmodload zsh/datetime
 
 function timestamp { strftime "%Y-%m-%d--%H.%M.%S" "$EPOCHSECONDS" }
 
-INSTALL_TO='/Applications/Utilities/XQuartz.app'
-
-INSTALLED_VERSION=`defaults read "${INSTALL_TO}/Contents/Info.plist" "CFBundleVersion" 2>/dev/null || echo '0'`
-
 ########################################################################################################################
 
-XML_FEED="https://www.xquartz.org/releases/sparkle/release.xml"
+	## This appears to be old
+	## XML_FEED='http://xquartz.macosforge.org/downloads/sparkle/release.xml'
+	# Found new appcast URL in "/usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask/Casks/xquartz.rb"
+	##
+	## The app seems to show 'https://www.xquartz.org/releases/sparkle/beta.xml' as the Sparkle URL
 
-INFO=($(curl -sfL "$XML_FEED" | tr -s ' ' '\012' | egrep 'sparkle:version=|url=' | head -2 | awk -F'"' '/^/{print $2}'))
+XML_FEED='https://www.xquartz.org/releases/sparkle/release.xml'
+
+# sparkle:shortVersionString exists in the feed, but sparkle:version seems to be the important number to check
+
+INFO=($(curl -sfL "$XML_FEED" \
+	| tr -s ' ' '\012' \
+	| egrep 'sparkle:version=|url=' \
+	| head -2 \
+	| awk -F'"' '/^/{print $2}'))
 
 URL="$INFO[1]"
 
 LATEST_VERSION="$INFO[2]"
 
-########################################################################################################################
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
-FILENAME="XQuartz-$LATEST_VERSION.dmg"
+	exit 1
+fi
+
+########################################################################################################################
 
 zmodload zsh/datetime
 
@@ -42,7 +66,7 @@ TIME=$(strftime "%Y-%m-%d-at-%H.%M.%S" "$EPOCHSECONDS")
 HOST=`hostname -s`
 HOST="$HOST:l"
 
-LOG="$HOME/Library/Logs/metalog/$NAME/$HOST/$TIME.txt"
+LOG="$HOME/Library/Logs/${NAME}.txt"
 
 [[ -d "$LOG:h" ]] || mkdir -p "$LOG:h"
 [[ -e "$LOG" ]]   || touch "$LOG"
@@ -74,39 +98,56 @@ function msgs {
 	fi
 }
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+########################################################################################################################
 
-autoload is-at-least
+if [[ -e "$INSTALL_TO" ]]
+then
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info.plist" "CFBundleVersion" 2>/dev/null)
 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	autoload is-at-least
 
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
-cd "$HOME/Downloads" || cd "$HOME/Desktop" || cd "$HOME" || cd /tmp
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
 
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
-echo "$NAME: Saving $URL to $PWD/$FILENAME"
-curl -C - --max-time 3600 --fail --location --referer ";auto" --progress-bar --output "${FILENAME}" "${URL}"
+fi
 
+########################################################################################################################
+
+FILENAME="$HOME/Downloads/XQuartz-$LATEST_VERSION.dmg"
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+curl --continue-at -  --max-time 3600 --fail --location --referer ";auto" --progress-bar --output "${FILENAME}" "${URL}"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
 [[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
-[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && exit 0
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
 
-
-MNTPNT=$(echo -n "Y" | hdid -plist "$FILENAME" 2>/dev/null | fgrep -A 1 '<key>mount-point</key>' | tail -1 | sed 's#</string>.*##g ; s#.*<string>##g')
+MNTPNT=$(echo -n "Y" \
+	| hdid -plist "$FILENAME" 2>/dev/null \
+	| fgrep -A 1 '<key>mount-point</key>' \
+	| tail -1 \
+	| sed 's#</string>.*##g ; s#.*<string>##g')
 
 if [ "$MNTPNT" = "" ]
 then
@@ -122,9 +163,14 @@ then
 	exit 0
 fi
 
-msgs "Installing $PKG (this will take awhile...)"
+msgs "Installing $PKG (this may take awhile...)"
 
-sudo installer -verbose -pkg "$PKG" -target / -lang en 2>&1 | tee -a "$LOG"
+if (( $+commands[pkginstall.sh] ))
+then
+	pkginstall.sh "$FILENAME"
+else
+	sudo /usr/sbin/installer -verbose -pkg "$PKG" -target / -lang en 2>&1 | tee -a "$LOG"
+fi
 
 EXIT="$?"
 
