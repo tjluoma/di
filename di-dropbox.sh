5a8b4ee27061dfdd7091a8d1a7b1ab576719dcce
TJ Luoma <github@tjluoma.com>
2018-08-02 15:35:07 -0400

    significant update due to Dropbox.com changes

--- di-dropbox.sh
+++ di-dropbox.sh
@@ -1,32 +1,58 @@
 #!/bin/zsh -f
+# Purpose: Download and install the latest version of Dropbox
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	Sat, Jul 14, 2018 4:11
 
 NAME="$0:t:r"
 
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
+fi
+
 function msg {
-		echo "$NAME: $@"
+	echo "$NAME: $@"
 }
 
-
-die ()
-{
+function die {
 	msg "[die] $@"
-	exit 0
+	po.sh "[die] $@"
+	exit 1
 }
 
+## I found this URL via https://github.com/Homebrew/homebrew-cask/blob/master/Casks/dropbox.rb
+
+DL_URL='https://www.dropbox.com/download?plat=mac&full=1'
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 #
 #		Check the website for the latest version number
 #
 
-LATEST_VERSION=`curl -A "$UA" -sL 'https://www.dropbox.com/install' |\
-				fgrep '<span id="version_str">' |\
-				sed 's#.*<span id="version_str">##g ; s# .*##g'`
+## Note: Do not use Safari UA here, it will fail
+## This gives the actual URL to download, which should look something like this:
+#
+# https://clientupdates.dropboxstatic.com/dbx-releng/client/Dropbox%2053.4.67.dmg
+
+URL=$(curl -sfL --head "$DL_URL" | awk -F' ' '/^.ocation: /{print $2}' | tail -1 | tr -d '\r')
+
+if [[ "$URL" == "" ]]
+then
+	die "$NAME: URL is empty. Cannot continue."
+	exit 1
+fi
+
+# This looks at the URL and extracts the version number from it
+LATEST_VERSION=$(echo "$URL:t:r" | sed 's#Dropbox%20##g')
 
 if [[ "$LATEST_VERSION" == "" ]]
 then
- 	die "$NAME: LATEST_VERSION is empty. Cannot continue."
-    exit 0
+	die "$NAME: LATEST_VERSION is empty. Cannot continue."
+	exit 1
 fi
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
@@ -34,43 +60,32 @@ fi
 #		Get the version number for the installed version of Dropbox
 #
 
-INSTALLED_VERSION=`defaults read /Applications/Dropbox.app/Contents/Info CFBundleShortVersionString 2>/dev/null || echo 0`
+INSTALL_TO='/Applications/Dropbox.app'
 
-####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
-#
-#		Compare Versions
-#
-
-# function version { echo "$@" | awk -F. '{ printf("08%03d%03d%03d\n", $1,$2,$3,$4); }'; }
-# 
-# if [ $(version ${LATEST_VERSION}) -gt $(version ${INSTALLED_VERSION}) ]
-# then
-#     msg "Dropbox is out of date: $INSTALLED_VERSION vs $LATEST_VERSION"
-# else
-# 
-#     msg "Dropbox is up to date! [$INSTALLED_VERSION = $LATEST_VERSION]"
-#     exit 0
-# fi
-
-
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
-
-autoload is-at-least
-
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
-
-if [ "$?" = "0" ]
+if [[ -e "$INSTALL_TO" ]]
 then
-	msg "Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-	exit 0
+
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo 0`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		msg "Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		msg "Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+		exit 0
+	fi
+
+	msg  "Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
 fi
-
-msg  "Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 #
@@ -79,20 +94,19 @@ msg  "Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
 FILENAME="$HOME/Downloads/Dropbox-${LATEST_VERSION}.dmg"
 
-
+echo "$NAME: Downloading $URL to $FILENAME:"
 
 curl --fail \
-    --progress-bar \
-    --continue-at - \
-    --location \
-    --output "$FILENAME" \
-    "https://dl.dropboxusercontent.com/u/17/Dropbox%20${LATEST_VERSION}.dmg"
+   --progress-bar \
+   --continue-at - \
+   --location \
+   --output "$FILENAME" \
+	"$URL"
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 #
 #		Mount DMG
 #
-
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 		| fgrep -A 1 '<key>mount-point</key>' \
@@ -101,17 +115,22 @@ MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 #
-#		Open app (which is an installer)
+#		Open app (which is an installer,
+#		so unlike most DMGs, we don't have to do anything else)
 #
 
 if [ -e "$MNTPNT/Dropbox.app" ]
 then
-    open "$MNTPNT/Dropbox.app"
+   open "$MNTPNT/Dropbox.app"
 else
 	die "Did not find anything at $MNTPNT/Dropbox.app"
-	exit 0
+	exit 1
 
 fi
 
-#EOF
+exit 0
 
+## 2018-07-14 - This is old, but it used to be the last part of the 'curl' command
+#   "https://dl.dropboxusercontent.com/u/17/Dropbox%20${LATEST_VERSION}.dmg"
+
+#EOF
