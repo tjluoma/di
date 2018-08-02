5928d725889735d73df8234567005fe39e9eaf4c
TJ Luoma <github@tjluoma.com>
2018-08-02 11:32:41 -0400

    new appcast, better error-checking

--- di-cocoapacketanalyzer.sh
+++ di-cocoapacketanalyzer.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of CocaPacketAnalyzer
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2016-03-19
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/CocoaPacketAnalyzer.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -14,20 +16,19 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO='/Applications/CocoaPacketAnalyzer.app'
-
 INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
+# OLD (but identical?)
 # XML_FEED="http://www.tastycocoabytes.com/cpa/updates/appcasting.php"
 
-XML_FEED="http://www.tastycocoabytes.com/cpa/updates/appcast.xml"
+XML_FEED='http://www.tastycocoabytes.com/cpa/updates/appcast.xml'
 
 INFO=($(curl -sfL "$XML_FEED" \
-| tr -s ' ' '\012' \
-| egrep 'sparkle:version=|url=' \
-| tail -2 \
-| sort \
-| awk -F'"' '/^/{print $2}'))
+		| tr -s ' ' '\012' \
+		| egrep 'sparkle:version=|url=' \
+		| tail -2 \
+		| sort \
+		| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
 LATEST_VERSION="$INFO[1]"
@@ -36,14 +37,18 @@ URL="$INFO[2]"
 	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
-fi
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
+	exit 1
+fi
 
 if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
 then
-	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+	echo "$NAME: Up-To-Date (Installed/Latest Version = $INSTALLED_VERSION)"
 	exit 0
 fi
 
@@ -59,10 +64,9 @@ fi
 
 echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
-
 FILENAME="$HOME/Downloads/CocoaPacketAnalyzer-${LATEST_VERSION}.dmg"
 
-echo "$NAME: Downloading $URL to $FILENAME"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
@@ -70,6 +74,12 @@ EXIT="$?"
 
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+echo "$NAME: Mounting $FILENAME:"
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 		| fgrep -A 1 '<key>mount-point</key>' \
@@ -83,6 +93,7 @@ then
 fi
 
 ditto --noqtn -v "$MNTPNT/CocoaPacketAnalyzer.app" "$INSTALL_TO" \
+&& echo "$NAME: Installation success. Ejecting $MNTPNT:" \
 && diskutil eject "$MNTPNT"
 
 
