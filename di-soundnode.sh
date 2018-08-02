231b9d001ab2237c59b9378ac54f6173f13b228e
TJ Luoma <github@tjluoma.com>
2018-08-02 16:48:13 -0400

    working around weird information in feed.

--- di-soundnode.sh
+++ di-soundnode.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download the latest version of Soundnode
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
@@ -14,14 +14,28 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO="/Applications/Soundnode.app" 
+INSTALL_TO="/Applications/Soundnode.app"
 
 INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '0'`
 
-URL='http://www.soundnodeapp.com/downloads/mac/Soundnode-App.zip'
+URL='http://www.soundnodeapp.com/downloads/mac/Soundnode.zip'
 
-LATEST_VERSION=`curl -sfL https://api.github.com/repos/Soundnode/soundnode-app/releases | fgrep tag_name | head -1 | tr -dc '[0-9].'`
+## 2018-08-02 - ok, for some reason there's a '7.0.0' tag_name, when all the rest are like this:
+#     "tag_name": "0.6.5",
+#     "tag_name": "0.6.4",
+#     "tag_name": "0.6.3",
+#     "tag_name": "0.6.2",
+#     "tag_name": "0.6.1",
+#     "tag_name": "0.6.0",
+#     "tag_name": "0.5.9",
+## So I'm just excluding that one for 'latest-version' checking purposes, because otherwise this will constantly run
+## and replace itself with new versions that aren't really new. Not sure what else to do.
 
+LATEST_VERSION=`curl -sfL https://api.github.com/repos/Soundnode/soundnode-app/releases \
+				| fgrep tag_name \
+				| fgrep -v '"tag_name": "7.0.0",' \
+				| head -1 \
+				| tr -dc '[0-9].'`
 
 if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
 then
@@ -63,7 +77,7 @@ EXIT="$?"
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 #
-#		Here’s the 'move the old version aside' section 
+#		Here’s the 'move the old version aside' section
 #
 
 
@@ -74,7 +88,7 @@ then
 	&& LAUNCH='yes' \
 	&& osascript -e 'tell application "Soundnode" to quit'
 
-		# move installed version to trash 
+		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/Soundnode.$INSTALLED_VERSION.app"
 fi
 
@@ -95,9 +109,9 @@ EXIT="$?"
 if [ "$EXIT" = "0" ]
 then
 	echo "$NAME: Installation of $INSTALL_TO was successful."
-	
+
 	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-	
+
 else
 	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
 fi
