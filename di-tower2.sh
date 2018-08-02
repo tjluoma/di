8061ce47c0dbded3e1398fb70c995e34671caf43
TJ Luoma <github@tjluoma.com>
2018-08-02 15:44:40 -0400

    code cleanup, better error-checking

--- di-tower2.sh
+++ di-tower2.sh
@@ -1,14 +1,14 @@
 #!/bin/zsh -f
-# Purpose: Download and install Tower 2
+# Purpose: Download and install Tower 2 (note that Tower 3 is available, but requires an annual subscription)
 #
 # From:	Tj Luo.ma
 # Mail:	luomat at gmail dot com
 # Web: 	http://RhymesWithDiploma.com
 # Date:	2015-02-02
 
-# @NOTDONE
-
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Tower.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -19,43 +19,70 @@ fi
 
 OS_VER=`sw_vers -productVersion`
 
+# sparkle:version= exists in feed and _may_ be used.. but Tower 2 is EOL so it's probably not worth worrying about.
+
 INFO=($(curl -sfL "https://updates.fournova.com/updates/tower2-mac/stable/?os_version=$OS_VER" \
-| tr -s ' ' '\012' \
-| egrep '^(url|sparkle:shortVersionString)=' \
-| head -2 \
-| awk -F'"' '//{print $2}'))
+		| tr -s ' ' '\012' \
+		| egrep '^(url|sparkle:shortVersionString)=' \
+		| head -2 \
+		| awk -F'"' '//{print $2}'))
 
 URL="$INFO[1]"
 
 LATEST_VERSION="$INFO[2]"
 
-INSTALL_TO="/Applications/Tower.app"
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
-INSTALLED_VERSION=`defaults read $INSTALL_TO/Contents/Info CFBundleShortVersionString 2>/dev/null || echo '0'`
+	exit 1
+fi
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+if [[ -e "$INSTALL_TO" ]]
+then
 
-autoload is-at-least
+	INSTALLED_VERSION=`defaults read $INSTALL_TO/Contents/Info CFBundleShortVersionString 2>/dev/null || echo '0'`
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	 if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	 then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	 fi
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
 
 FILENAME="$HOME/Downloads/Tower-${LATEST_VERSION}.zip"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
 
 if [[ -e "$INSTALL_TO" ]]
 then
@@ -66,6 +93,19 @@ echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
 
 ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
 
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+
+	echo "$NAME: Installation/Update of $INSTALL_TO succeeded."
+	exit 0
+
+else
+	echo "$NAME: ditto failed (\$EXIT = $EXIT)"
+
+	exit 1
+fi
 
 exit 0
 
