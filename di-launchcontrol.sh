774bfd767eb02b3e745001385ef0f4bfa5c797b5
TJ Luoma <github@tjluoma.com>
2018-08-02 16:05:09 -0400

    new XML_Feed and various improvements

--- di-launchcontrol.sh
+++ di-launchcontrol.sh
@@ -1,5 +1,5 @@
-#!/bin/zsh
-# Download and install latest LaunchControl
+#!/bin/zsh -f
+# Purpose: Download and install latest LaunchControl
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
@@ -7,107 +7,105 @@
 
 NAME="$0:t:r"
 
-zmodload zsh/datetime
+INSTALL_TO='/Applications/LaunchControl.app'
 
-TIME=$(strftime "%Y-%m-%d-at-%H.%M.%S" "$EPOCHSECONDS")
-
-HOST=`hostname -s`
-HOST="$HOST:l"
-
-LOG="$HOME/Library/Logs/metalog/$NAME/$HOST/$TIME.log"
-
-[[ -d "$LOG:h" ]] || mkdir -p "$LOG:h"
-[[ -e "$LOG" ]]   || touch "$LOG"
-
-function timestamp { strftime "%Y-%m-%d at %H:%M:%S" "$EPOCHSECONDS" }
-function log { echo "$NAME [`timestamp`]: $@" | tee -a "$LOG" }
-
-XML_URL='http://www.soma-zone.com/LaunchControl/a/appcast_update.xml'
-
-DOWNLOAD_URL=`curl -sfL "$XML_URL" \
-| tr '[:blank:]' '\012' \
-| egrep '^url' \
-| tail -1 \
-| sed 's#url="##g ; s#"$##g'`
-
-DL_SIZE=`curl -sfL "$XML_URL" \
-| tr '[:blank:]' '\012' \
-| egrep '^length' \
-| tail -1 \
-| tr -dc '[0-9]'`
-
-LATEST_VERSION=`curl -sfL "$XML_URL" \
-| tr '[:blank:]' '\012' \
-| egrep '^sparkle:shortVersionString=' \
-| tail -1 \
-| tr -dc '[0-9].'`
-
-INSTALLED_VERSION=`defaults read /Applications/LaunchControl.app/Contents/Info CFBundleShortVersionString 2>/dev/null || echo '0'`
-
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
-
-autoload is-at-least
-
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
-
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-
-
-log "Update needed $LATEST_VERSION vs $INSTALLED_VERSION"
-
-cd "$HOME/Sites/iusethis.luo.ma/launchcontrol" 2>/dev/null \
-	|| cd '/Volumes/Drobo2TB/BitTorrent Sync/iusethis.luo.ma/launchcontrol' 2>/dev/null \
-	|| cd "$HOME/BitTorrent Sync/iusethis.luo.ma/launchcontrol" 2>/dev/null \
-	|| cd "$HOME/Downloads/" 2>/dev/null \
-	|| cd "$HOME/"
-
-if [ -d old ]
+if [ -e "$HOME/.path" ]
 then
-	mv *.tar.bz2 old/
+	source "$HOME/.path"
+else
+	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
+fi
+
+	# 2018-07-17 this was the old URL I was using:
+	# XML_URL='http://www.soma-zone.com/LaunchControl/a/appcast_update.xml'
+XML_FEED='https://somazonecom.ipage.com/soma-zone.com/LaunchControl/a/appcast_update.xml'
+
+INFO=($(curl -sfL "$XML_FEED" \
+		| tr '[:blank:]' '\012' \
+		| egrep '^(url|sparkle:shortVersionString=)' \
+		| tail -2 \
+		| sort ))
+
+XML_FEED='https://somazonecom.ipage.com/soma-zone.com/LaunchControl/a/appcast_update.xml'
+
+INFO=($(curl -sfL "$XML_FEED" \
+| tr -s ' ' '\012' \
+| egrep 'sparkle:shortVersionString=|url=' \
+| tail -2 \
+| sort \
+| awk -F'"' '/^/{print $2}'))
+
+URL="$INFO[1]"
+
+LATEST_VERSION="$INFO[2]"
+
+if [ "$URL" = "" -o "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Bad data from $XML_FEED"
+	echo "
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=`defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString 2>/dev/null`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
 fi
 
 FILENAME="$HOME/Downloads/LaunchControl-$LATEST_VERSION.tar.bz2"
 
-zmodload zsh/stat
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
-SIZE=$(zstat -L +size "$FILENAME" || echo '0')
+EXIT="$?"
 
-if [ "$SIZE" -lt "$DL_SIZE" ]
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+if [[ -e "$INSTALL_TO" ]]
 then
-	curl -fL --progress-bar --output "$FILENAME" "$DOWNLOAD_URL"
-fi
-
-SIZE=$(zstat -L +size "$FILENAME" 2>/dev/null || echo '0')
-
-if [ "$SIZE" -lt "$DL_SIZE" ]
-then
-	log "Size mismatch: Actual $SIZE vs expected $DL_SIZE"
-	exit 0
-fi
-
-if [ -e /Applications/LaunchControl.app ]
-then
-	mv -vn /Applications/LaunchControl.app "$HOME/.Trash/LaunchControl-$INSTALLED_VERSION.app"
-fi
-
-if [ -e /Applications/LaunchControl.app ]
-then
-	log "Failed to remove old /Applications/LaunchControl.app"
-	exit 0
+	mv -vn "$INSTALL_TO" "$HOME/.Trash/LaunchControl-$INSTALLED_VERSION.app"
 fi
 
 	# Unpack and Install the .tar.bz2 file to /Applications/
+
+echo "$NAME: Installing $FILENAME to $INSTALL_TO..."
+
 tar -C "/Applications/" -j -x -f "$FILENAME"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+	exit 0
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	exit 1
+fi
 
 exit 0
 #
