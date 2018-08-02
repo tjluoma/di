815e69161e6a1b570b267df307e86ff73db28af4
TJ Luoma <github@tjluoma.com>
2018-08-02 15:53:39 -0400

    improved error checking

--- di-desktopcurtain.sh
+++ di-desktopcurtain.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of Desktop Curtain
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2016-02-01
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Desktop Curtain.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -16,45 +18,53 @@ fi
 
 XML_FEED='https://manytricks.com/desktopcurtain/appcast.xml'
 
-INSTALL_TO='/Applications/Desktop Curtain.app'
-
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
 INFO=($(curl -A Safari -sfL "$XML_FEED" \
-| tr -s ' ' '\012' \
-| egrep 'sparkle:shortVersionString=|url=' \
-| head -2 \
-| sort \
-| awk -F'"' '/^/{print $2}'))
+		| tr -s ' ' '\012' \
+		| egrep 'sparkle:shortVersionString=|url=' \
+		| head -2 \
+		| sort \
+		| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
 LATEST_VERSION="$INFO[1]"
 URL="$INFO[2]"
 
-	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
 fi
 
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+if [[ -e "$INSTALL_TO" ]]
 then
-	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
-	exit 0
+
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
 fi
-
-autoload is-at-least
-
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
-
-if [ "$?" = "0" ]
-then
-	echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-	exit 0
-fi
-
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
 FILENAME="$HOME/Downloads/DesktopCurtain-${LATEST_VERSION}.dmg"
 
@@ -89,17 +99,28 @@ then
 	&& LAUNCH='yes' \
 	&& osascript -e 'tell application "Desktop Curtain" to quit'
 
-		# move installed version to trash 
+		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/Desktop Curtain.$INSTALLED_VERSION.app"
 fi
 
 ########################################################################################################################
 
-ditto --noqtn -v "$MNTPNT/Desktop Curtain.app" "$INSTALL_TO" \
-&& diskutil eject "$MNTPNT"
+ditto --noqtn -v "$MNTPNT/Desktop Curtain.app" "$INSTALL_TO"
 
+EXIT="$?"
 
+if [ "$EXIT" = "0" ]
+then
 
+	echo "$NAME: Installed/updated $INSTALL_TO"
+
+else
+	echo "$NAME: 'ditto' failed (\$EXIT = $EXIT)"
+
+	exit 1
+fi
+
+diskutil eject "$MNTPNT"
 
 exit 0
 #EOF
