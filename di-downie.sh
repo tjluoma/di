d7bad06cc0c9978f27709aef6add7788e8b10090
TJ Luoma <github@tjluoma.com>
2018-08-02 11:42:34 -0400

    code cleanup, better debugging

--- di-downie.sh
+++ di-downie.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of Downie
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2016-05-07
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Downie.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -14,62 +16,66 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO='/Applications/Downie.app'
-
-APPNAME="$INSTALL_TO:t:r"
-
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 
 XML_FEED="http://trial.charliemonroe.net/downie/updates_2.3.xml"
 
 INFO=($(curl -sfL "$XML_FEED" \
-| tr -s ' ' '\012' \
-| egrep 'sparkle:shortVersionString=|url=' \
-| head -2 \
-| sort \
-| awk -F'"' '/^/{print $2}'))
+		| tr -s ' ' '\012' \
+		| egrep 'sparkle:shortVersionString=|url=' \
+		| head -2 \
+		| sort \
+		| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
 LATEST_VERSION="$INFO[1]"
 URL="$INFO[2]"
 
-	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
 fi
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+if [[ -e "$INSTALL_TO" ]]
 then
-	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
-	exit 0
+
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date (Installed/Latest Version = $INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
 fi
-
-autoload is-at-least
-
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
-
-if [ "$?" = "0" ]
-then
-	echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-	exit 0
-fi
-
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-
-FILENAME="$HOME/Downloads/$APPNAME-${LATEST_VERSION}.zip"
-
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 
-echo "$NAME: Downloading $URL to $FILENAME"
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.zip"
 
-curl --continue-at - --fail --location --output "$FILENAME" "$URL"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
@@ -82,7 +88,6 @@ EXIT="$?"
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 
-
 if [ -e "$INSTALL_TO" ]
 then
 		# Quit app, if running
@@ -90,7 +95,7 @@ then
 	&& LAUNCH='yes' \
 	&& osascript -e 'tell application "Downie" to quit'
 
-		# move installed version to trash 
+		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/Downie.$INSTALLED_VERSION.app"
 fi
 
@@ -106,19 +111,14 @@ EXIT="$?"
 if [ "$EXIT" = "0" ]
 then
 	echo "$NAME: Installation of $INSTALL_TO was successful."
-	
+
 	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-	
+
 else
-	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	echo "$NAME: Installation of $INSTALL_TO failed (ditto \$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+
+	exit 1
 fi
-
-
-####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
-
-
-####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
-
 
 ####|####|####|####|####|####|####|####|####|####|####|####|####|####|####
 
