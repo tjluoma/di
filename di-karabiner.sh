2291ca81d6f22bd7009618808bc1f960511bdb1a
TJ Luoma <github@tjluoma.com>
2018-08-02 14:24:16 -0400

    better debugging, code cleanup

--- di-karabiner.sh
+++ di-karabiner.sh
@@ -1,10 +1,14 @@
 #!/bin/zsh -f
-# Purpose: download and install latest Karabiner
+# Purpose: download and install latest Karabiner -- not to be confused with 'Karabiner-Elements'
 #
 # From:	Tj Luo.ma
 # Mail:	luomat at gmail dot com
 # Web: 	http://RhymesWithDiploma.com
 # Date:	2015-01-14
+
+NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Karabiner.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -13,20 +17,14 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-NAME="$0:t:r"
-
-INSTALL_TO='/Applications/Karabiner.app'
-
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-
 XML_FEED='https://pqrs.org/osx/karabiner/files/appcast.xml'
 
 INFO=($(curl -sfL "$XML_FEED" \
-| tr -s ' ' '\012' \
-| egrep 'sparkle:version=|url=' \
-| head -2 \
-| sort \
-| awk -F'"' '/^/{print $2}'))
+		| tr -s ' ' '\012' \
+		| egrep 'sparkle:version=|url=' \
+		| head -2 \
+		| sort \
+		| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
 LATEST_VERSION="$INFO[1]"
@@ -35,27 +33,39 @@ URL="$INFO[2]"
 	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
 fi
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+if [[ -e "$INSTALL_TO" ]]
+then
 
-autoload is-at-least
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
 
 FILENAME="$HOME/Downloads/Karabiner-${LATEST_VERSION}.dmg"
 
@@ -88,7 +98,7 @@ then
 	&& osascript -e 'tell application "Karabiner" to quit'
 
 	# The package installer will take care of moving old version
-	
+
 fi
 
 PKG=`find "$MNTPNT" -type f -iname \*.pkg -maxdepth 1 -print`
@@ -97,32 +107,14 @@ if [[ "$PKG" == "" ]]
 then
 	echo "$NAME [failed]: PKG is empty" \
 	tee -a "$HOME/Desktop/$NAME.Failed.log"
-	
+
 	exit 0
-fi 	
-
-if (( $+commands[pkginstall.sh] ))
-then
-
-	pkginstall.sh "$PKG" \
-	&& diskutil eject "$MNTPNT"
-
-else
-
-	if [ "$EUID" = "0" ]
-	then
-		installer -pkg "$PKG" -target / -lang en \
-		&& diskutil eject "$MNTPNT"
-
-	else
-			# Try sudo but if it fails, open pkg in Finder
-		sudo installer -pkg "$PKG" -target / -lang en \
-		&& diskutil eject "$MNTPNT" \
-		|| open -R "$PKG"
-	fi 	
 fi
 
-open -a Karabiner
+	# Try 'sudo installer', but if it fails, open pkg in Finder
+sudo /usr/sbin/installer -pkg "$PKG" -target / -lang en \
+	&& diskutil eject "$MNTPNT" \
+		|| open -R "$PKG"
 
 exit 0
 
