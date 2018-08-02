844b93e939bd11bf6352c8e34a9ffc42c881fc22
TJ Luoma <github@tjluoma.com>
2018-08-02 10:32:27 -0400

    added for people using this version of CCC

---
+++ di-carboncopycloner3.sh
@@ -0,0 +1,117 @@
+#!/bin/zsh -f
+# Purpose: Download and install latest version of Carbon Copy Cloner 3 (there is also a version 5! too)
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2016-05-02
+
+NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Carbon Copy Cloner.app'
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+
+# @TODO - This feed seems to stop with version 3.4.7 but there is a version 5. Need new feed URL?
+
+XML_FEED='https://bombich.com/software/updates/ccc.php'
+
+# sparkle:version is the only version info in the feed
+
+INFO=($(curl -sfL "$XML_FEED" \
+| gunzip \
+| tr -s ' ' '\012' \
+| egrep 'sparkle:version=|url=' \
+| head -2 \
+| sort \
+| awk -F'"' '/^/{print $2}'))
+
+	# "Sparkle" will always come before "url" because of "sort"
+LATEST_VERSION="$INFO[1]"
+URL="$INFO[2]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+echo "$INFO"
+
+exit 0
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null | tr -dc '[0-9]\.'`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date (Installed/Latest Version = $INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+
+fi
+
+
+FILENAME="$HOME/Downloads/CarbonCopyCloner-${LATEST_VERSION}.dmg"
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+
+MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
+		| fgrep -A 1 '<key>mount-point</key>' \
+		| tail -1 \
+		| sed 's#</string>.*##g ; s#.*<string>##g')
+
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+fi
+
+echo "$NAME: Installing MNTPNT/Carbon Copy Cloner.app to $INSTALL_TO"
+
+ditto --noqtn -v "$MNTPNT/Carbon Copy Cloner.app" "$INSTALL_TO" \
+&& diskutil eject "$MNTPNT"
+
+
+
+
+exit 0
+#EOF
