b3ea1601bcf39984faeff10241c9573621bae3ec
TJ Luoma <github@tjluoma.com>
2018-08-02 16:01:31 -0400

    new XML_FEED, other minor improvements

--- di-textbar.sh
+++ di-textbar.sh
@@ -8,6 +8,8 @@
 
 NAME="$0:t:r"
 
+INSTALL_TO='/Applications/TextBar.app'
+
 if [ -e "$HOME/.path" ]
 then
 	source "$HOME/.path"
@@ -15,75 +17,85 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-	# Where is the XML sparkle feed
-XML_URL='http://www.richsomerfield.com/apps/textbar/sparkle_textbar.xml'
+## 2018-07-17 - this was the old XML_FEED that I was using
+# XML_FEED='http://www.richsomerfield.com/apps/textbar/sparkle_textbar.xml'
 
-	# The most recent version in the XML feed
-LATEST_VERSION=`curl -sL "$XML_URL" \
-| tr ' ' '\012' \
-| fgrep 'sparkle:version="' \
-| head -1 \
-| tr -dc '[0-9].'`
+XML_FEED='https://raw.githubusercontent.com/richie5um/richie5um.github.io/master/apps/textbar/sparkle_textbar.xml'
 
-	# The URL of the actual latest zip file to download
-DOWNLOAD_ACTUAL=`curl -sL "$XML_URL" \
-| tr '"' '\012' \
-| egrep "http.*\.zip" \
-| head -1`
+# sparkle:version is the ONLY field available in XML_FEED as far as version numbers go
 
-	# The size of the zip file in the XML feed
-REMOTE_SIZE=`curl -sfL --head "$DOWNLOAD_ACTUAL" \
-| awk -F' ' '/^Content-Length/{print $NF}' \
-| tr -dc '[0-9]'`
+INFO=($(curl -sfL "$XML_FEED" \
+		| tr ' ' '\012' \
+		| egrep '^(url|sparkle:version)=' \
+		| head -2 \
+		| sort \
+		| awk -F'"' '//{print $2}'))
 
-	# Where should the app be installed to
-INSTALL_TO="/Applications/TextBar.app"
+LATEST_VERSION="$INFO[1]"
 
-	# Get the currently install version number, if any
-	# if none, set to 0
-INSTALLED_VERSION=`defaults read ${INSTALL_TO}/Contents/Info CFBundleShortVersionString 2>/dev/null || echo 0`
+URL="$INFO[2]"
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
-autoload is-at-least
-
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
-
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	exit 1
+fi
 
 
-DIR="$HOME/Downloads"
+if [[ -e "$INSTALL_TO" ]]
+then
 
-	# chdir to the appropriate dir
-cd "$DIR"
+		# Get the currently install version number
+	INSTALLED_VERSION=`defaults read ${INSTALL_TO}/Contents/Info CFBundleShortVersionString`
 
-	# Make sure the file size that we have matches what we should have
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
+
+
 FILENAME="$HOME/Downloads/TextBar-$LATEST_VERSION.zip"
 
 	# Download the latest zip
-echo "$NAME: Downloading $DOWNLOAD_ACTUAL to $PWD"
+echo "$NAME: Downloading $URL to $FILENAME"
 
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
-curl -fL --output "$FILENAME" --progress-bar "$DOWNLOAD_ACTUAL"
+EXIT="$?"
 
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
-	# Quit the app, if running, otherwise harmless
-pkill -x TextBar
+# if running, quit
+pgrep -xq TextBar \
+&& osascript -e 'tell application "TextBar" to quit' \
+&& LAUNCH='yes'
 
-	# Move the old version to the trash, if there is one
-	# put its version number in the filename in case we want to go back to it
-[[ -d "$INSTALL_TO" ]] \
-&& mv -vn "$INSTALL_TO" "$HOME/.Trash/TextBar.$INSTALLED_VERSION.app" 2>/dev/null
+if [[ -e "$INSTALL_TO" ]]
+then
+	mv -f "$INSTALL_TO" "$HOME/.Trash/TextBar.$INSTALLED_VERSION.app"
+fi
 
 	# unzip the file we downloaded into /Applications/ aka the 'head' folder
 	# of $INSTALL_TO
@@ -91,6 +103,17 @@ pkill -x TextBar
 echo "$NAME: Installing $FILENAME to $INSTALL_TO:h"
 ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
 
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+
+	[[ "$LAUNCH" = "yes" ]] && open -a TextBar
+
+	exit 0
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	exit 1
+fi
 
 exit 0
 #
