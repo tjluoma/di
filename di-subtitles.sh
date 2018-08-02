a6054a8851af5e0bfbb7a53bbe2fd63104f94427
TJ Luoma <github@tjluoma.com>
2018-08-02 16:14:01 -0400

    better error checking

--- di-subtitles.sh
+++ di-subtitles.sh
@@ -8,6 +8,8 @@
 
 NAME="$0:t:r"
 
+INSTALL_TO='/Applications/Subtitles.app'
+
 if [ -e "$HOME/.path" ]
 then
 	source "$HOME/.path"
@@ -15,29 +17,31 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-INSTALL_TO='/Applications/Subtitles.app'
+XML_FEED='https://subtitlesapp.com/updates.xml'
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo 0`
+	# this XML file is... unusual. It's a huge blob of XML, most of which is XML-ified HTML, so
+	# we use 'tidy' to force it into something more easily readable
 
-	# XML file is... unusual.
-INFO=($(curl -sfL 'http://subtitlesapp.com/updates.xml' \
-| tidy --input-xml yes --output-xml yes --show-warnings no --force-output yes --quiet yes --wrap 0 \
-| sed 's#&lt;#<#g ; s#&gt;#>#g ' \
-| fgrep 'sparkle:version=' \
-| head -1 \
-| tr -s ' ' '\012' \
-| sort \
-| egrep 'sparkle:version=|url=' \
-| awk -F'"' '/^/{print $2}'))
+	# CFBundleShortVersionString and CFBundleVersion: 3.2.11 are identical, so no need to check both
+
+INFO=($(curl -sfL "$XML_FEED" \
+	| tidy --input-xml yes --output-xml yes --show-warnings no --force-output yes --quiet yes --wrap 0 \
+	| sed 's#&lt;#<#g ; s#&gt;#>#g ' \
+	| fgrep 'sparkle:version=' \
+	| head -1 \
+	| tr -s ' ' '\012' \
+	| sort \
+	| egrep 'sparkle:version=|url=' \
+	| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
 LATEST_VERSION="$INFO[1]"
+
 URL="$INFO[2]"
 
-	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-		echo
+	echo "$NAME: Trying backup method to find URL and LATEST_VERSION"
 
 	URL=`curl -sfL --head 'http://subtitlesapp.com/download/' | awk -F' ' '/^Location:/{print $2}' | tail -1 | tr -d '\r'`
 
@@ -45,48 +49,74 @@ then
 
 fi
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+if [ "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
 
-autoload is-at-least
+	echo "$NAME: Cannot continue. Bad data from $XML_FEED:
+	INFO: $INFO
+	URL: $URL
+	LATEST_VERSION: LATEST_VERSION
+	"
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	exit 1
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+fi
 
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
 
 FILENAME="$HOME/Downloads/Subtitles-$LATEST_VERSION.zip"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
-
-
-# if running, quit
-pgrep -xq Subtitles \
-&& osascript -e 'tell application "Subtitles" to quit'
-
 if [[ -e "$INSTALL_TO" ]]
 then
 	mv -f "$INSTALL_TO" "$HOME/.Trash/Subtitles.$INSTALLED_VERSION.app"
 fi
 
+echo "$NAME: Installing $FILENAME to $INSTALL_TO"
+
 ditto -v --noqtn -xk "$FILENAME" "$INSTALL_TO:h"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+	exit 0
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	exit 1
+fi
+
 
 exit 0
 #
