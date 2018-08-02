1c97e4605ea26f9096ded34f3667869d111fe5ce
TJ Luoma <github@tjluoma.com>
2018-08-02 15:40:43 -0400

    changed INSTALL_TO to avoid conflict with Logos 7

--- di-logos6.sh
+++ di-logos6.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: Download and install (or update) the latest version of Logos.com for Mac
+# Purpose: Download and install (or update) the latest version of Logos.com for Mac (note: this is for version 6. Version 7 is also available)
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2015-11-19
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Logos 6.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -20,7 +22,17 @@ XML_FEED='https://clientservices.logos.com/update/v1/feed/logos6-mac/stable.xml'
 #<logos:version>6.7.0.0044</logos:version>
 
 INFO=($(curl -sfL "$XML_FEED" \
-| tidy --char-encoding utf8 --force-output yes --input-xml yes --markup yes --output-xhtml no --output-xml yes --quiet yes --show-errors 0 --show-warnings no --wrap 0 \
+| tidy \
+	--char-encoding utf8 \
+	--force-output yes \
+	--input-xml yes \
+	--markup yes \
+	--output-xhtml no \
+	--output-xml yes \
+	--quiet yes \
+	--show-errors 0 \
+	--show-warnings no \
+	--wrap 0 \
 | egrep 'link href|logos:version' \
 | head -2 \
 | sed 's#<logos:version>##g ; s#</logos:version>##g ; s#<link href="##g; s#" .*##g'))
@@ -31,39 +43,53 @@ URL="$INFO[1]"
 
 LATEST_VERSION="$INFO[2]"
 
-INSTALL_TO="/Applications/Logos.app"
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+	exit 1
+fi
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+if [[ -e "$INSTALL_TO" ]]
+then
 
-autoload is-at-least
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
 
 FILENAME="$HOME/Downloads/Logos-$LATEST_VERSION.dmg"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download failed (EXIT = $EXIT)" && exit 0
-
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 	| fgrep -A 1 '<key>mount-point</key>' \
@@ -90,11 +116,24 @@ fi
 
 echo "$NAME: Installing $MNTPNT/Logos.app to $INSTALL_TO"
 
-ditto -v "$MNTPNT/Logos.app" "$INSTALL_TO"
+ditto --noqtn -v "$MNTPNT/Logos.app" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+
+	echo "$NAME: Installed $LATEST_VERSION to $INSTALL_TO"
+
+else
+	echo "$NAME: ditto --noqtn failed (\$EXIT = $EXIT)"
+
+	exit 1
+fi
 
 diskutil eject "$MNTPNT"
 
-open "$INSTALL_TO"
+[[ "$LAUNCH" == "yes" ]] && open "$INSTALL_TO"
 
 exit 0
 #EOF
