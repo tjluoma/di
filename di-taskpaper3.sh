20207354972a0c6d646c636d07dfda4b95ede3c5
TJ Luoma <github@tjluoma.com>
2018-08-02 15:57:56 -0400

    updated XML_FEED and other minor improvements

--- di-taskpaper3.sh
+++ di-taskpaper3.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose:
+# Purpose: Download and install the latest version of TaskPaper
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2015-11-18
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/TaskPaper.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -14,53 +16,73 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO='/Applications/TaskPaper.app'
-
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-
-XML_FEED='https://taskpaper.s3.amazonaws.com/TaskPaper.rss'
+	# via Cask folks
+XML_FEED='https://www.taskpaper.com/assets/app/TaskPaper.rss'
 
 INFO=($(curl -sfL "$XML_FEED" \
-| tr -s ' ' '\012' \
-| egrep 'sparkle:shortVersionString=|url=' \
-| head -2 \
-| sort \
-| awk -F'"' '/^/{print $2}'))
+	| tr -s ' ' '\012' \
+	| egrep 'sparkle:shortVersionString=|url=' \
+	| head -2 \
+	| sort \
+	| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
 LATEST_VERSION="$INFO[1]"
+
 URL="$INFO[2]"
 
 	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
 fi
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+if [[ -e "$INSTALL_TO" ]]
+then
 
-autoload is-at-least
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString`
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	autoload is-at-least
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
-FILENAME="$HOME/Downloads/TaskPaper3-$LATEST_VERSION.dmg"
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
+
+FILENAME="$HOME/Downloads/TaskPaper-$LATEST_VERSION.dmg"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
 		| fgrep -A 1 '<key>mount-point</key>' \
@@ -73,24 +95,33 @@ then
 	exit 1
 fi
 
-if [ -e "$INSTALL_TO" ]
+if [[ -e "$INSTALL_TO" ]]
 then
-		# Quit app, if running
-	pgrep -xq "TaskPaper" \
-	&& LAUNCH='yes' \
-	&& osascript -e 'tell application "TaskPaper" to quit'
-
 		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/TaskPaper.$INSTALLED_VERSION.app"
 fi
 
 echo "$NAME: Installing $MNTPNT/TaskPaper.app to $INSTALL_TO"
 
-ditto -v "$MNTPNT/TaskPaper.app" "$INSTALL_TO" \
-&& diskutil eject "$MNTPNT" \
-&& rm -f "$FILENAME"
-## These are betas and they are coming out very often, so there's no need to keep them
+ditto --noqtn -v "$MNTPNT/TaskPaper.app" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+
+	echo "$NAME: Installation/Update successful"
+
+else
+	echo "$NAME: failed (\$EXIT = $EXIT)"
+
+	exit 1
+fi
+
+diskutil eject "$MNTPNT"
 
 
 exit 0
 #EOF
+		## This seems to give an outdated version because the XML_FEED only goes to 3.3.2 but the current version is 3.7.5"
+		## XML_FEED='https://taskpaper.s3.amazonaws.com/TaskPaper.rss'
