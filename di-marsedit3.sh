b198667e935b6b69475ebc64568d123f84274908
TJ Luoma <github@tjluoma.com>
2018-08-02 16:15:17 -0400

    updated error checking

--- di-marsedit3.sh
+++ di-marsedit3.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: Download and Install latest MarsEdit
+# Purpose: Download and Install latest version of MarsEdit 3 (note that MarsEdit 4 is now available too)
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2015-11-19
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/MarsEdit.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -21,59 +23,66 @@ fi
 	# relaunched after upgrade
 LAUNCH='no'
 
-	# Where should it be installed to?
-INSTALL_TO='/Applications/MarsEdit.app'
-
-	# If MarsEdit is already installed, check to see if it is up to date
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-
 	# This is the Sparkle feed for MarsEdit updates
-XML_FEED='http://www.red-sweater.com/marsedit/appcast3.php'
+XML_FEED='https://www.red-sweater.com/marsedit/appcast3.php'
 
 	# Parse the Sparkle feed for information about the latest release
 INFO=($(curl -sfL "$XML_FEED" \
-| tr -s ' ' '\012' \
-| egrep 'sparkle:shortVersionString=|url=' \
-| head -2 \
-| sort \
-| awk -F'"' '/^/{print $2}'))
+		| tr -s ' ' '\012' \
+		| egrep 'sparkle:shortVersionString=|url=' \
+		| head -2 \
+		| sort \
+		| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
 LATEST_VERSION="$INFO[1]"
+
 URL="$INFO[2]"
 
 	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
 fi
 
- if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
- fi
+if [[ -e "$INSTALL_TO" ]]
+then
+		# If MarsEdit is already installed, check to see if it is up to date
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null`
 
-autoload is-at-least
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
 
- is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
- 
- if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
+	autoload is-at-least
 
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
 
 	# This is where the latest version will be saved to
 FILENAME="$HOME/Downloads/MarsEdit-${LATEST_VERSION}.zip"
 
-echo "$NAME: Out of Date: $INSTALLED_VERSION vs $LATEST_VERSION\nDownloading $URL to $FILENAME"
+echo "$NAME: Downloading $URL to $FILENAME"
 
 	# Here is where we do the actual downloading
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL" 2>/dev/null
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL" 2>/dev/null
 
 EXIT="$?"
 
@@ -84,9 +93,9 @@ EXIT="$?"
 if [ -e "$INSTALL_TO" ]
 then
 		# if it is currently running, quit it (nicely!) and then move it out of the way
-	pgrep -xq "MarsEdit" \
-	&& LAUNCH='yes' \
-	&& osascript -e 'tell application "MarsEdit" to quit'
+# 	pgrep -xq "MarsEdit" \
+# 	&& LAUNCH='yes' \
+# 	&& osascript -e 'tell application "MarsEdit" to quit'
 
 		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/MarsEdit.$INSTALLED_VERSION.app"
@@ -113,6 +122,9 @@ else
 	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
 fi
 
+exit 0
+
+
 # is MarsEdit Registered?
 
 if [ "`defaults read com.red-sweater.marsedit ME3RegistrationSerialNumber 2>/dev/null`" = "" ]
@@ -124,7 +136,7 @@ fi
 ########################################################################################################################
 ##                                        NERD NOTE:                                                                ####
 ##
-## If you want to automate registration of MarsEdit, you can do it by uncommenting the two' defaults write' lines 
+## If you want to automate registration of MarsEdit, you can do it by uncommenting the two' defaults write' lines
 ## and adding in the relevant information between the 'quote marks'
 #
 # defaults write com.red-sweater.marsedit ME3RegistrationUserName 'YourNameHere'
