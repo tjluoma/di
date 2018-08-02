abdbdc8e338cc345933001dd033da019b2d30d2d
TJ Luoma <github@tjluoma.com>
2018-08-02 12:49:36 -0400

    minor updates and improvements

--- di-liteicon.sh
+++ di-liteicon.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of LiteIcon
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2016-05-10
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/LiteIcon.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -15,10 +17,6 @@ else
 fi
 
 XML_FEED='http://www.freemacsoft.net/liteicon/updates.xml'
-
-INSTALL_TO='/Applications/LiteIcon.app'
-
-APPNAME="$INSTALL_TO:t:r"
 
 INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
 
@@ -34,17 +32,22 @@ INFO=($(curl -sfL "$XML_FEED" \
 LATEST_VERSION="$INFO[1]"
 URL="$INFO[2]"
 
+
 	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
-fi
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
+	exit 1
+fi
 
 if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
 then
-	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+	echo "$NAME: Up-To-Date (Installed/Latest Version = $INSTALLED_VERSION)"
 	exit 0
 fi
 
@@ -60,11 +63,11 @@ fi
 
 echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
-FILENAME="$HOME/Downloads/$APPNAME-${LATEST_VERSION}.zip"
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.zip"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
-curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+ curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
@@ -86,9 +89,9 @@ EXIT="$?"
 if [ "$EXIT" = "0" ]
 then
 	echo "$NAME: Installation of $INSTALL_TO was successful."
-	
+
 	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-	
+
 else
 	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
 fi
