7384877628449da9f58797c2adb2a60596783a6b
TJ Luoma <github@tjluoma.com>
2018-08-02 11:36:03 -0400

    better debugging info

--- di-daisydisk.sh
+++ di-daisydisk.sh
@@ -7,6 +7,8 @@
 
 NAME="$0:t:r"
 
+INSTALL_TO='/Applications/DaisyDisk.app'
+
 if [ -e "$HOME/.path" ]
 then
 	source "$HOME/.path"
@@ -14,17 +16,12 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO='/Applications/DaisyDisk.app'
-
 OS_VER=`sw_vers -productVersion`
 
+	# If we don't tell it we are using at least version 4, we get an empty feed
 INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '4'`
 
-# XML_FEED="http://www.daisydiskapp.com/downloads/appcastFeed.php?osVersion=${OS_VER}&appVersion=${INSTALLED_VERSION}&appEdition=Standard"
-
-## 2016-05-22 - we intentionally tell it we are using 4.0 so that the feed will show us the most recent version
-##				because that is what we need to compare against what we have. Otherwise we get an empty feed back.
-XML_FEED="http://www.daisydiskapp.com/downloads/appcastFeed.php?osVersion=${OS_VER}&appVersion=4.0&appEdition=Standard"
+XML_FEED="http://www.daisydiskapp.com/downloads/appcastFeed.php?osVersion=${OS_VER}&appVersion=${INSTALLED_VERSION}&appEdition=Standard"
 
 INFO=($(curl -sfL "$XML_FEED" \
 | tr -s ' ' '\012' \
@@ -40,9 +37,15 @@ URL="$INFO[2]"
 	# If any of these are blank, we should not continue
 if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO"
-	exit 0
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
 fi
+
 
 autoload is-at-least
 
@@ -50,7 +53,7 @@ is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
 if [ "$?" = "0" ]
 then
-	echo "$NAME: Up-To-Date ($LATEST_VERSION)"
+	echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 	exit 0
 fi
 
@@ -58,14 +61,19 @@ echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSIO
 
 FILENAME="$HOME/Downloads/DaisyDisk-${LATEST_VERSION}.zip"
 
-echo "$NAME: Downloading $URL to $FILENAME"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
 	## exit 22 means 'the file was already fully downloaded'
-[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download failed (EXIT = $EXIT)" && exit 0
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
 
 if [ -e "$INSTALL_TO" ]
 then
@@ -74,7 +82,7 @@ then
 	&& LAUNCH='yes' \
 	&& osascript -e 'tell application "DaisyDisk" to quit'
 
-		# move installed version to trash 
+		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/DaisyDisk.$INSTALLED_VERSION.app"
 fi
 
@@ -88,9 +96,9 @@ EXIT="$?"
 if [ "$EXIT" = "0" ]
 then
 	echo "$NAME: Installation of $INSTALL_TO was successful."
-	
+
 	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-	
+
 else
 	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
 fi
