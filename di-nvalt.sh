f1008fced8025723972b42a3cfb9aff487c8aabe
TJ Luoma <github@tjluoma.com>
2018-08-04 09:12:54 -0400

    updated variable names to be more consistent

--- di-nvalt.sh
+++ di-nvalt.sh
@@ -31,51 +31,50 @@ INFO=($(curl -sfL "$XML_FEED" \
 		| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
-MAJOR_VERSION="$INFO[1]"
-LATEST_VERSION="$INFO[2]"
+LATEST_VERSION="$INFO[1]"
+LATEST_BUILD="$INFO[2]"
 URL="$INFO[3]"
 
 
 	# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
 then
 	echo "$NAME: Error: bad data received:
 	INFO: $INFO
 	LATEST_VERSION: $LATEST_VERSION
+	LATEST_BUILD: $LATEST_BUILD
 	URL: $URL
 	"
 
 	exit 1
 fi
 
-
-
 if [[ -e "$INSTALL_TO" ]]
 then
 
 	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null`
 
-	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	if [[ "$LATEST_BUILD" == "$INSTALLED_VERSION" ]]
 	then
-		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		echo "$NAME: Up-To-Date ($LATEST_VERSION/$INSTALLED_VERSION)"
 		exit 0
 	fi
 
 	autoload is-at-least
 
-	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+	is-at-least "$LATEST_BUILD" "$INSTALLED_VERSION"
 
 	if [ "$?" = "0" ]
 	then
-		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_BUILD"
 		exit 0
 	fi
 
-	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_BUILD)"
 
 fi
 
-FILENAME="$HOME/Downloads/nvALT-${MAJOR_VERSION}-${LATEST_VERSION}.zip"
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}-${LATEST_BUILD}.zip"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
@@ -89,12 +88,12 @@ EXIT="$?"
 if [ -e "$INSTALL_TO" ]
 then
 		# Quit app, if running
-	pgrep -xq "nvALT" \
+	pgrep -xq "$INSTALL_TO:t:r" \
 	&& LAUNCH='yes' \
-	&& osascript -e 'tell application "nvALT" to quit'
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
 
 		# move installed version to trash
-	mv -vf "$INSTALL_TO" "$HOME/.Trash/nvALT.$INSTALLED_VERSION.app"
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
 fi
 
 echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
