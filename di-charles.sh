04a32f01f7d468c6df79bde7a8153736467f70a0
TJ Luoma <github@tjluoma.com>
2018-08-04 04:55:47 -0400

    minor code cleanup, clarification of major version

--- di-charles.sh
+++ di-charles.sh
@@ -1,11 +1,13 @@
 #!/bin/zsh -f
-# Purpose: Download and install the latest version of Charles
+# Purpose: Download and install the latest version of Charles (now at version 4)
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2015-12-02
 
 NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Charles.app'
 
 if [ -e "$HOME/.path" ]
 then
@@ -14,44 +16,44 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO='/Applications/Charles.app'
-
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-
 	# This returns just the version number
 LATEST_VERSION=`curl -sfL http://www.charlesproxy.com/latest.do`
 
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+if [[ -e "$INSTALL_TO" ]]
 then
-	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
-	exit 0
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 fi
-
-autoload is-at-least
-
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
-
-if [ "$?" = "0" ]
-then
-	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
-	exit 0
-fi
-
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
 URL="https://www.charlesproxy.com/assets/release/$LATEST_VERSION/charles-proxy-$LATEST_VERSION.dmg"
 
 FILENAME="$HOME/Downloads/Charles-$LATEST_VERSION.dmg"
 
-echo "$NAME: Downloading $URL to $FILENAME"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
- curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
-
 
 if [ -e "$INSTALL_TO" ]
 then
@@ -64,6 +66,8 @@ then
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/Charles.$INSTALLED_VERSION.app"
 fi
 
+echo "$NAME: Accepting EULA and mounting '$FILENAME':"
+
 	# Note that this will automatically accept the EULA without reading it
 	# just like you would have done :-)
 MNTPNT=$(echo -n "Y" | hdid -plist "$FILENAME" 2>/dev/null | fgrep '/Volumes/' | sed 's#</string>##g ; s#.*<string>##g')
@@ -74,7 +78,7 @@ then
 	exit 1
 fi
 
-echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+echo "$NAME: Installing '$MNTPNT/Charles.app' to '$INSTALL_TO:h/':"
 
 	# Extract from the .zip file and install (this will leave the .zip file in place)
 ditto --noqtn -v "$MNTPNT/Charles.app" "$INSTALL_TO"
@@ -92,7 +96,6 @@ else
 fi
 
 diskutil eject "$MNTPNT"
-
 
 exit 0
 #EOF
