86dae1cc8b71500f6bc6efae22f7bd002ff59ade
TJ Luoma <github@tjluoma.com>
2018-08-04 12:41:16 -0400

    Added more error checking, better unzip

--- di-uebersicht.sh
+++ di-uebersicht.sh
@@ -1,14 +1,16 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of Übersicht from <https://github.com/felixhageloh/uebersicht>
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2016-01-19
 
 NAME="$0:t:r"
-APPNAME="Übersicht"
-CONVERTED_APPNAME="$(iconv -t MAC <<< $APPNAME)"
+INSTALL_TO="/Applications/Übersicht.app"
+
 LAUNCH='no'
+
+# CONVERTED_APPNAME="$(iconv -t MAC <<< $INSTALL_TO:t:r)"		# See note below
 
 if [ -e "$HOME/.path" ]
 then
@@ -17,55 +19,62 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO="/Applications/$APPNAME.app"
+# Note that /Applications/Übersicht.app reports its version info like this:
+# 	CFBundleShortVersionString: 1.2
+# 	CFBundleVersion: 53
+#
+# but in the XML_FEED we get:
+#
+# 	sparkle:version="53"
+# 	sparkle:shortVersionString="1.2.53"
 
-# https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/600008
-# https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/601003
-# where '600008' = CFBundleVersion
+XML_FEED="https://raw.githubusercontent.com/felixhageloh/uebersicht/gh-pages/updates.xml.rss"
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-BUILD_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo 600000`
-INSTALLED_VERSION="$INSTALLED_VERSION.$BUILD_NUMBER"
-# echo $INSTALLED_VERSION
-# echo $BUILD_NUMBER
+INFO=($(curl -sfL $XML_FEED \
+		| tr ' ' '\012' \
+		| egrep '^(url|sparkle:shortVersionString)=' \
+		| head -2 \
+		| sort \
+		| awk -F'"' '//{print $2}'))
 
-FEED_URL="https://raw.githubusercontent.com/felixhageloh/uebersicht/gh-pages/updates.xml.rss"
+LATEST_VERSION="$INFO[1]"
 
-INFO=($(curl -sfL $FEED_URL \
-| tr ' ' '\012' \
-| egrep '^(url|sparkle:shortVersionString)=' \
-| head -2 \
-| awk -F'"' '//{print $2}'))
+URL="$INFO[2]"
 
-URL="$INFO[1]"
-# echo $URL
-LATEST_VERSION="$INFO[2]"
-# echo $LATEST_VERSION
+if [[ -e "$INSTALL_TO" ]]
+then
+		# We need to get both 'INSTALLED_VERSION' and 'INSTALLED_BUILD' and put them together
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString`
 
+	INSTALLED_BUILD=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion`
 
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
+		# now we meld them into one:
+	INSTALLED_VERSION="$INSTALLED_VERSION.$INSTALLED_BUILD"
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up To Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
 fi
 
-autoload is-at-least
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.zip"
 
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
-
-if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
- 
- echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-
-
-FILENAME="$HOME/Downloads/${APPNAME//[[:space:]]/}-${LATEST_VERSION}.zip"
-
-
-echo "$NAME: Downloading $URL to $FILENAME"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
@@ -74,31 +83,81 @@ EXIT="$?"
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
-if [ -e "$INSTALL_TO" ]
-then
-	pgrep -qx "$CONVERTED_APPNAME" && LAUNCH='yes' && killall "$CONVERTED_APPNAME"
-	mv -f "$INSTALL_TO" "$HOME/.Trash/$APPNAME.$INSTALLED_VERSION.app"
-fi
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
-echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
-	# Extract from the .zip file and install (this will leave the .zip file in place)
-ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
+
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+
+ditto -xk --noqtn "${FILENAME}" "${UNZIP_TO}"
 
 EXIT="$?"
 
-if [ "$EXIT" = "0" ]
+if [[ "$EXIT" == "0" ]]
 then
-	echo "$NAME: Installation of $INSTALL_TO was successful."
-	
-	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-	
+	echo "$NAME: Unzip successful"
 else
-	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+		# failed
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+
+	exit 1
 fi
 
+if [[ -e "$INSTALL_TO" ]]
+then
+	echo "$NAME: Moving existing (old) \"$INSTALL_TO\" to \"$HOME/.Trash/\"."
 
+	## I couldn't get this to work, but the 'osascript' line does quit the app,
+	## so I'm suggesting that instead.
+	# pgrep -qx "$CONVERTED_APPNAME" && LAUNCH='yes' && killall "$CONVERTED_APPNAME"
 
+	IS_RUNNING=`ps auxwww | egrep '.bersicht' | fgrep -v egrep`
+
+	if [[ "$IS_RUNNING" != "" ]]
+	then
+		echo "$NAME: Quitting Übersicht"
+
+		osascript -e 'tell application "Übersicht" to quit'
+
+		sleep 5 # give it a chance to clean up
+
+		LAUNCH='yes'
+	fi
+
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.$RANDOM.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
+
+		exit 1
+	fi
+fi
+
+echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
+
+	# Move the file out of the folder
+mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" = "0" ]]
+then
+
+	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO" && echo "$NAME: launching ${INSTALL_TO}."
+
+else
+	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+	exit 1
+fi
 
 exit 0
 EOF
