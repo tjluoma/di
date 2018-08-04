4819e1412d464e8fea16e09b2f96925dc3d0d1f5
TJ Luoma <github@tjluoma.com>
2018-08-04 15:22:02 -0400

    run all of the 'di-' scripts except 'di-all.sh'

---
+++ di-all.sh
@@ -0,0 +1,58 @@
+#!/bin/zsh -f
+# Purpose: Runs all di- scripts found in the same directory
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2016-05-13
+
+NAME="$0:t:r"
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+zmodload zsh/datetime
+
+LOG="$HOME/Library/Logs/$NAME.log"
+
+	# Delete (old) log if it exists already
+[[ -e "$LOG" ]] 	|| rm -f "$LOG"
+
+function timestamp { strftime "%Y-%m-%d at %-l:%M:%S %p" "$EPOCHSECONDS" }
+
+function log { echo "$NAME [`timestamp`]: $@" | tee -a "$LOG" }
+
+# chdir to the directory where this script is found
+cd "$0:h"
+
+COUNT='0'
+
+log "---------- STARTING AT `timestamp` ---------- "
+
+
+##
+## Get a list of all of the 'di-*.sh' files (be sure to exclude this script (di-all.sh) or else it'll go on forever)
+##
+
+command ls -1 di-*sh \
+| fgrep -v 'di-all.sh' \
+| while read line
+do
+
+	log "Running $line"
+
+	((COUNT++))
+
+	"${line}"  2>&1 | tee -a "$LOG"
+
+done
+
+log "---------- FINISHED AT `timestamp` after checking $COUNT apps ---------- "
+
+echo "$NAME last ran: `timestamp`. ${COUNT} apps were checked." >| "$HOME/.$NAME.lastrun.log"
+
+exit 0
+#EOF
