1047babd02009531863fb1c35f7be24548e5d4e0
TJ Luoma <github@tjluoma.com>
2018-08-04 11:38:07 -0400

    code cleanup, better error checking

--- di-transporterdesktop.sh
+++ di-transporterdesktop.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of Transporter Desktop
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2016-06-02
 
 NAME="$0:t:r"
-APPNAME="Transporter Desktop"
+INSTALL_TO="/Applications/Transporter Desktop.app"
+XML_FEED="http://appsoftware.connecteddata.com/mac/2.5/Appcast.xml"
 
 if [ -e "$HOME/.path" ]
 then
@@ -15,51 +16,60 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO="/Applications/$APPNAME.app"
-# echo $INSTALL_TO
-# https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/600008
-# https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/601003
-# where '600008' = CFBundleVersion
+	# CFBundleShortVersionString and CFBundleVersion are identical in the app
+INFO=($(curl -sfL $XML_FEED \
+		| tr ' ' '\012' \
+		| egrep '^(url|sparkle:version)=' \
+		| tail -2 \
+		| sort \
+		| awk -F'"' '//{print $2}'))
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-BUILD_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo 600000`
-# echo $INSTALLED_VERSION
-# echo $BUILD_NUMBER
-FEED_URL="http://appsoftware.connecteddata.com/mac/2.5/Appcast.xml"
+LATEST_VERSION="$INFO[1]"
 
-INFO=($(curl -sfL $FEED_URL \
-| tr ' ' '\012' \
-| egrep '^(url|sparkle:shortVersionString|sparkle:version)=' \
-| tail -2 \
-| awk -F'"' '//{print $2}'))
+URL="$INFO[2]"
 
-URL="$INFO[1]"
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
 
-LATEST_VERSION="$INFO[2]"
-
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
+	exit 1
 fi
 
-autoload is-at-least
+if [[ -e "$INSTALL_TO" ]]
+then
 
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
 
-if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
- 
- echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	autoload is-at-least
 
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
-FILENAME="$HOME/Downloads/${APPNAME//[[:space:]]/}-${LATEST_VERSION}.zip"
+	VERSION_COMPARE="$?"
 
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up To Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
 
-echo "$NAME: Downloading $URL to $FILENAME"
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+	# Hard-coding appname to remove space in appname
+FILENAME="$HOME/Downloads/TransporterDesktop-${LATEST_VERSION}.zip"
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
@@ -68,32 +78,64 @@ EXIT="$?"
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
-if [ -e "$INSTALL_TO" ]
-then
-	pgrep -qx "$APPNAME" && LAUNCH='yes' && killall -9 "$APPNAME"
-# 	pgrep -qx "$APPNAME" && LAUNCH='yes' && osascript -e 'tell application "Alfred 3" to quit'
-	mv -f "$INSTALL_TO" "$HOME/.Trash/$APPNAME.$INSTALLED_VERSION.app"
-fi
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
-echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
-	# Extract from the .zip file and install (this will leave the .zip file in place)
-ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
+
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+
+ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
 
 EXIT="$?"
 
-if [ "$EXIT" = "0" ]
+if [[ "$EXIT" == "0" ]]
 then
-	echo "$NAME: Installation of $INSTALL_TO was successful."
-	
-	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-	
+	echo "$NAME: Unzip successful"
 else
-	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+		# failed
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+
+	exit 1
 fi
 
+if [[ -e "$INSTALL_TO" ]]
+then
+	echo "$NAME: Moving existing (old) \"$INSTALL_TO\" to \"$HOME/.Trash/\"."
 
+ 	pgrep -qx "$INSTALL_TO:t:r" && LAUNCH='yes' && osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
 
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
+
+		exit 1
+	fi
+fi
+
+echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
+
+	# Move the file out of the folder
+mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" = "0" ]]
+then
+
+	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+else
+	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+	exit 1
+fi
 
 exit 0
 EOF
