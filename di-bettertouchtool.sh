07372828dcb12dcc0a818c1ee122f160b73ad9f6
TJ Luoma <github@tjluoma.com>
2018-08-04 08:20:30 -0400

    Major update, using new URL, replaces old appcast

--- di-bettertouchtool.sh
+++ di-bettertouchtool.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of BetterTouchTool
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2016-01-19
 
 NAME="$0:t:r"
-APPNAME="BetterTouchTool"
+INSTALL_TO="/Applications/BetterTouchTool.app"
 
 if [ -e "$HOME/.path" ]
 then
@@ -15,46 +15,46 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO="/Applications/$APPNAME.app"
-# echo $INSTALL_TO
+URL=$(curl -sfL "https://updates.bettertouchtool.net/bettertouchtool_release_notes.html" \
+	| awk -F'"' '/http.*\.zip/{print $2}' \
+	| head -1)
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-BUILD_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo 600000`
-# echo $INSTALLED_VERSION
-# echo $BUILD_NUMBER
-FEED_URL="https://updates.bettertouchtool.net/appcast.xml"
+LATEST_VERSION=`echo "$URL:t:r" | tr -dc '[0-9]\.'`
 
-INFO=($(curl -sfL $FEED_URL \
-| tr ' ' '\012' \
-| egrep '^(url|sparkle:shortVersionString)=' \
-| head -2 \
-| awk -F'"' '//{print $2}'))
-# echo $INFO
-
-URL="$INFO[1]"
-LATEST_VERSION="$INFO[2]"
-
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
+	# If any of these are blank, we should not continue
+if [ "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:\nLATEST_VERSION: ${LATEST_VERSION}\nURL: ${URL}"
+	exit 1
 fi
 
-autoload is-at-least
+if [[ -e "$INSTALL_TO" ]]
+then
 
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
 
-if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
- 
- echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	autoload is-at-least
 
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
-FILENAME="$HOME/Downloads/${APPNAME//[[:space:]]/}-${LATEST_VERSION}.zip"
+	VERSION_COMPARE="$?"
 
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up To Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.zip"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
@@ -67,29 +67,75 @@ EXIT="$?"
 
 if [ -e "$INSTALL_TO" ]
 then
-	pgrep -qx "$APPNAME" && LAUNCH='yes' && killall "$APPNAME"
-	mv -f "$INSTALL_TO" "$HOME/.Trash/$APPNAME.$INSTALLED_VERSION.app"
+	pgrep -qx "BetterTouchTool" && LAUNCH='yes' && killall "BetterTouchTool"
+	mv -f "$INSTALL_TO" "$HOME/.Trash/BetterTouchTool.$INSTALLED_VERSION.app"
 fi
 
-echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
 
-	# Extract from the .zip file and install (this will leave the .zip file in place)
-ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+
+ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
 
 EXIT="$?"
 
-if [ "$EXIT" = "0" ]
+if [[ "$EXIT" == "0" ]]
 then
-	echo "$NAME: Installation of $INSTALL_TO was successful."
-	
-	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-	
+	echo "$NAME: Unzip successful"
 else
-	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+		# failed
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+
+	exit 1
 fi
 
+if [[ -e "$INSTALL_TO" ]]
+then
+	echo "$NAME: Moving existing (old) \"$INSTALL_TO\" to \"$HOME/.Trash/\"."
 
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
 
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
+
+		exit 1
+	fi
+fi
+
+echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
+
+	# Move the file out of the folder
+mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" = "0" ]]
+then
+
+	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+else
+	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+	exit 1
+fi
 
 exit 0
 EOF
+
+
+##
+## 2018-08-04 - this feed is outdated.
+## https://updates.bettertouchtool.net/bettertouchtool_release_notes.html is
+## not an RSS/Sparkle feed, but it's the best place to check for updates.
+##
+# XML_FEED="https://updates.bettertouchtool.net/appcast.xml"
+#
+# url="https://bettertouchtool.net/releases/btt2.427_recovery4.zip"
+# sparkle:version="787"
+# sparkle:shortVersionString="2.427"
+
