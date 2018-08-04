901167572101832000ae5eb17f33efc9553a2900
TJ Luoma <github@tjluoma.com>
2018-08-04 04:51:16 -0400

    For those still using Charles 3 instead of 4
    This probably won't get any updates, of course, but it's still handy if you need to install it on a new Mac.

---
+++ di-charles3.sh
@@ -0,0 +1,101 @@
+#!/bin/zsh -f
+# Purpose: Download and install the latest (last?) version of Charles Proxy v3 (v4 is now available)
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2018-08-04
+
+NAME="$0:t:r"
+
+if [[ -e "$HOME/.path" ]]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+URL="https://www.charlesproxy.com/assets/release/3.12.3/charles-proxy-3.12.3.dmg"
+
+LATEST_VERSION=`echo "$URL:t:r" | tr -dc '[0-9]\.'`
+
+FILENAME="$HOME/Downloads/Charles-${LATEST_VERSION}.dmg"
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up To Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+echo "$NAME: Downloading $URL to $FILENAME"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+if [ -e "$INSTALL_TO" ]
+then
+		# Quit app, if running
+	pgrep -xq "Charles" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "Charles" to quit'
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/Charles.$INSTALLED_VERSION.app"
+fi
+
+	# Note that this will automatically accept the EULA without reading it
+	# just like you would have done :-)
+MNTPNT=$(echo -n "Y" | hdid -plist "$FILENAME" 2>/dev/null | fgrep '/Volumes/' | sed 's#</string>##g ; s#.*<string>##g')
+
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+fi
+
+echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+
+	# Extract from the .zip file and install (this will leave the .zip file in place)
+ditto --noqtn -v "$MNTPNT/Charles.app" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+	echo "$NAME: Installation of $INSTALL_TO was successful."
+
+	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
+
+else
+	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+
+	exit 1
+fi
+
+diskutil eject "$MNTPNT"
+
+exit 0
+#EOF
