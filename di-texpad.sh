8eca0da2a50211dc8986014abae1d295a8cca401
TJ Luoma <github@tjluoma.com>
2018-08-04 10:22:41 -0400

    Major rewrite to deal with tricky XML_FEED format

--- di-texpad.sh
+++ di-texpad.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh -f
-# Purpose: 
+# Purpose: Download and install the latest version of Texpad
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2016-01-19
 
 NAME="$0:t:r"
-APPNAME="Texpad"
+INSTALL_TO="/Applications/Texpad.app"
+XML_FEED="https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml"
 
 if [ -e "$HOME/.path" ]
 then
@@ -15,55 +16,81 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO="/Applications/$APPNAME.app"
+# NOTE: This is a very unusual case. Rather than including the CFBundleShortVersionString in a 'sparkle:shortVersionString' field
+# in the RSS feed, the LATEST_VERSION information appears to be shown only in the '<title>' field of the appropriate entry
+# AND we need to skip the _first_ '<title>' because it is the <title> of the feed itself, whereas we want the
+# <title> of the first _entry_ in the feed
 
-# https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/600008
-# https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/601003
-# where '600008' = CFBundleVersion
+IFS=$'\n' INFO=($(curl -sfL "$XML_FEED" \
+		| fgrep -vi '<title>Texpad</title>' \
+		| egrep '<title>.*</title>|sparkle:version=|url=' \
+		| head -3 \
+		| sed 's#^[ 	]*##g' \
+		| sort ))
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
-# echo $INSTALLED_VERSION
-BUILD_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo 600000`
-# echo $BUILD_NUMBER
-INSTALLED_VERSION=$BUILD_NUMBER
+## We end up with 3 lines, which should look something like this:
+# <enclosure url="https://download.texpadapp.com/apps/osx/updates/Texpad_1_8_5__404__f8f30e5.dmg"
+# <title>Texpad 1.8.5 (404)</title>
+# sparkle:version="404"
+## Note that '404' is the LATEST_BUILD number, and it too appears in the <title> so we need to
+## remove that from the LATEST_VERSION information, and _then_ we can get the LATEST_VERSION
+## info by removing everything except digits and any literal '.'
 
-FEED_URL="https://www.texpadapp.com/static-collected/upgrades/texpadappcast.xml"
+URL=`echo "$INFO[1]" | sed 's#<enclosure url="##g ; s#"##g;'`
 
-INFO=($(curl -sfL $FEED_URL \
-| tr ' ' '\012' \
-| egrep '^(url|sparkle:shortVersionString|sparkle:version)=' \
-| head -2 \
-| awk -F'"' '//{print $2}'))
-# echo $INFO
-URL="$INFO[1]"
-# echo $URL
-LATEST_VERSION="$INFO[2]"
-# echo $LATEST_VERSION
+LATEST_BUILD=`echo "$INFO[3]" | tr -dc '[0-9]'`
 
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
- then
- 	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
- 	exit 0
+	# Make sure we get LATEST_BUILD _before_ we try to get this
+LATEST_VERSION=`echo "$INFO[2]" | sed "s#\(${LATEST_BUILD}\)##g;" | tr -dc '[0-9]\.'`
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	LATEST_BUILD: $LATEST_BUILD
+	URL: $URL
+	"
+
+	exit 1
 fi
 
-autoload is-at-least
+if [[ -e "$INSTALL_TO" ]]
+then
 
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
 
-if [ "$?" = "0" ]
- then
- 	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
- 	exit 0
- fi
- 
- echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	INSTALLED_BUILD=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
 
+	autoload is-at-least
 
-# FILENAME="$HOME/Downloads/${APPNAME//[[:space:]]/}-${LATEST_VERSION}.zip"
-FILENAME="$HOME/Downloads/${APPNAME//[[:space:]]/}-${LATEST_VERSION}.dmg"
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
+	VERSION_COMPARE="$?"
 
-echo "$NAME: Downloading $URL to $FILENAME"
+	is-at-least "$LATEST_BUILD" "$INSTALLED_BUILD"
+
+	BUILD_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" -a "$BUILD_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up To Date ($INSTALLED_VERSION/$INSTALLED_BUILD)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION/$INSTALLED_BUILD vs $LATEST_VERSION/$LATEST_BUILD"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}_${LATEST_BUILD}.dmg"
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
@@ -72,48 +99,50 @@ EXIT="$?"
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
-if [ -e "$INSTALL_TO" ]
-then
-	pgrep -qx "$APPNAME" && LAUNCH='yes' && killall "$APPNAME"
-	mv -f "$INSTALL_TO" "$HOME/.Trash/$APPNAME.$INSTALLED_VERSION.app"
-fi
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
-echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
-
-	# Extract from the .zip file and install (this will leave the .zip file in place)
-# ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
-# 
-# EXIT="$?"
-# 
-# if [ "$EXIT" = "0" ]
-# then
-# 	echo "$NAME: Installation of $INSTALL_TO was successful."
-# 	
-# 	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-# 	
-# else
-# 	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
-# fi
+echo "$NAME: Mounting $FILENAME:"
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
-		| fgrep -A 1 '<key>mount-point</key>' \
-		| tail -1 \
-		| sed 's#</string>.*##g ; s#.*<string>##g')
+	| fgrep -A 1 '<key>mount-point</key>' \
+	| tail -1 \
+	| sed 's#</string>.*##g ; s#.*<string>##g')
 
-ditto "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+		# Quit app, if running
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
+fi
+
+echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
+
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
 
 EXIT="$?"
 
-if [ "$EXIT" = "0" ]
+if [[ "$EXIT" == "0" ]]
 then
-	echo "$NAME: Installation of $INSTALL_TO was successful."
-	
-	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
-	
+	echo "$NAME: Successfully installed $INSTALL_TO"
 else
-	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	echo "$NAME: ditto failed"
+
+	exit 1
 fi
+
+echo "$NAME: Unmounting $MNTPNT:"
 
 diskutil eject "$MNTPNT"
 
