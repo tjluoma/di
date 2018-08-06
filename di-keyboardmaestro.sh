32f4533c9b4d9e0500f4a4de4c55607292f3c842
TJ Luoma <github@tjluoma.com>
2018-08-06 02:43:58 -0400

    better unzip, 2 methods of looking for LATEST_VERSION

--- di-keyboardmaestro.sh
+++ di-keyboardmaestro.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh -f
-# Purpose: Download and install the latest version of Keyboard Maestro
+# Purpose: Download and install the latest version of Keyboard Maestro from <http://www.keyboardmaestro.com>
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
@@ -16,44 +16,47 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-LAUNCH_APP=""
+LAUNCH_APP="no"
 
 LAUNCH_ENGINE="yes"
 
-LATEST_VERSION=$(curl -sfL http://www.keyboardmaestro.com/main/ | fgrep -i '<title>' | tr -dc '[0-9]\.')
+LATEST_VERSION=$(curl -sfL "http://www.keyboardmaestro.com/main/" | fgrep -i '<title>' | tr -dc '[0-9]\.')
 
-[[ "$LATEST_VERSION" == "" ]] && echo "$NAME: LATEST_VERSION is empty." && exit 1
+if [[ "$LATEST_VERSION" == "" ]]
+then
+	echo "$NAME: Using backup method of trying to get URL and LATEST_VERSION"
 
-LATEST_VERSION_SQUISHED=`echo $LATEST_VERSION | tr -dc '[0-9]'`
+	INFO=($(curl -sL 'http://files.stairways.com' \
+			| egrep -i 'keyboardmaestro.*\.zip' \
+			| head -1 \
+			| sed 's#<li><a href="##g; s#">Keyboard Maestro # #g; s#</a></li>##g'))
 
-URL="https://files.stairways.com/keyboardmaestro-$LATEST_VERSION_SQUISHED.zip"
+	URL="http://files.stairways.com/$INFO[1]"
 
-# INFO=($(curl -sL 'http://files.stairways.com' \
-# 		| egrep -i 'keyboardmaestro.*\.zip' \
-# 		| head -1 \
-# 		| sed 's#<li><a href="##g; s#">Keyboard Maestro # #g; s#</a></li>##g'))
-#
-# LATEST_VERSION="$INFO[2]"
-#
-# URL="http://files.stairways.com/$INFO[1]"
-#
-#
+	LATEST_VERSION="$INFO[2]"
+
+else
+
+	LATEST_VERSION_SQUISHED=`echo "${LATEST_VERSION}" | tr -dc '[0-9]'`
+
+	URL="https://files.stairways.com/keyboardmaestro-$LATEST_VERSION_SQUISHED.zip"
+fi
+
 	# If any of these are blank, we should not continue
-# if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
-# then
-# 	echo "$NAME: Error: bad data received:
-# 	INFO: $INFO
-# 	LATEST_VERSION: $LATEST_VERSION
-# 	URL: $URL
-# 	"
-#
-# 	exit 1
-# fi
+if [ "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
 
 if [[ -e "$INSTALL_TO" ]]
 then
 
-	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString`
 
 	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
 	then
@@ -90,9 +93,25 @@ EXIT="$?"
 
 [[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
-ENGINE_SEPARATE=''
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
 
-if [ -e "$INSTALL_TO" ]
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+
+ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Unzip successful"
+else
+		# failed
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
 then
 
 	pgrep -q 'Keyboard Maestro Engine' \
@@ -106,50 +125,35 @@ then
 		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/Keyboard Maestro.$INSTALLED_VERSION.app"
 
-	ENGINE_SEPARATE="/Applications/Keyboard Maestro Engine.app"
-
-	if [[ -d "$ENGINE_SEPARATE" ]]
-	then
-		mv -vf "$ENGINE_SEPARATE" "$HOME/.Trash/Keyboard Maestro Engine.$INSTALLED_VERSION.app"
-	fi
 fi
 
+echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
 
-
-echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
-
-ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+	# Move the file out of the folder
+mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
 
 EXIT="$?"
 
-if [ "$EXIT" = "0" ]
+if [[ "$EXIT" = "0" ]]
 then
+	[[ "$LAUNCH_ENGINE" == "yes" ]]	&& open -a "${INSTALL_TO}/Contents/MacOS/Keyboard Maestro Engine.app"
 
-	echo "$NAME: Installation successful"
+	[[ "$LAUNCH_APP" == "yes" ]] 		&& open -a "${INSTALL_TO}"
 
-		# move the engine to the /Applications/ folder if it was there before
-	if [[ "$ENGINE_SEPARATE" != "" ]]
-	then
-		mv -vf "$INSTALL_TO/Contents/MacOS/Keyboard Maestro Engine.app" "$INSTALL_TO:h/"
+	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
 
-	fi
-
-	# Rename the downloaded file based on the version number we leanred
+	# Rename the downloaded file based on the version number we learned
 	# Note: CFBundleVersion is the same so we don't include it
 	NEW_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString`
 
 	[[ "$NEW_VERSION" != "" ]] &&
-	mv -fv "$FILENAME" "$FILENAME:h/KeyboardMaestro-$NEW_VERSION.zip"
+	mv -fv "$FILENAME" "$FILENAME:h/KeyboardMaestro-${NEW_VERSION}.zip"
 
 else
-	echo "$NAME: ditto failed (\$EXIT = $EXIT)"
+	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
 
 	exit 1
 fi
-
-[[ "$LAUNCH_ENGINE" == "yes" ]]	&& open -b com.stairways.keyboardmaestro.engine 2>&1
-
-[[ "$LAUNCH_APP" == "yes" ]] 		&& open -b com.stairways.keyboardmaestro.editor 2>&1
 
 exit 0
 #EOF
