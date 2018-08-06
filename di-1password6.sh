6a87791e541b3ccb79faf492b6421d04c919077b
TJ Luoma <github@tjluoma.com>
2018-08-06 14:57:00 -0400

    updated to deal with new .pkg format and fix URL

--- di-1password6.sh
+++ di-1password6.sh
@@ -16,34 +16,18 @@ fi
 
 INSTALL_TO='/Applications/1Password 6.app'
 
-# https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/600008
-# https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/601003
-# where '600008' = CFBundleVersion
+# 	https://app-updates.agilebits.com/download/OPM4
+# currently redirects to:
+# 	https://c.1password.com/dist/1P/mac4/1Password-6.8.9.pkg
 
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '0'`
+URL=$(curl -sfL --head 'https://app-updates.agilebits.com/download/OPM4' | awk -F' |\r' '/^.ocation/{print $2}' | tail -1)
 
-BUILD_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo 600000`
-
-FEED_URL="https://app-updates.agilebits.com/check/1/15.2.0/OPM4/en/$BUILD_NUMBER"
-
-	# json
-INFO=($(curl -sfL "$FEED_URL" | tr -s '"' '\012"'))
-
-if [[ "$INFO" == "{ available : 0 }" ]]
-then
-	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
-	exit 0
-fi
-
-LATEST_VERSION="$INFO[8]"
-
-URL="$INFO[22]"
+LATEST_VERSION=$(echo "$URL:t:r" | sed 's#1Password-##g' | tr -dc '[0-9]\.')
 
 	# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+if [ "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
 	echo "$NAME: Error: bad data received:
-	INFO: $INFO
 	LATEST_VERSION: $LATEST_VERSION
 	URL: $URL
 	"
@@ -51,54 +35,84 @@ then
 	exit 1
 fi
 
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+if [[ -e "$INSTALL_TO" ]]
 then
-	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
-	exit 0
+
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up To Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
 fi
 
-autoload is-at-least
+FILENAME="$HOME/Downloads/1Password-${LATEST_VERSION}.pkg"
 
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
-if [ "$?" = "0" ]
-then
-	echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
-	exit 0
-fi
-
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-
-FILENAME="$HOME/Downloads/1Password-${LATEST_VERSION}.zip"
-
-echo "$NAME: Downloading $URL to $FILENAME"
-
- curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
-echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
-	# Extract from the .zip file and install (this will leave the .zip file in place)
-ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
-EXIT="$?"
+function die
+{
+	echo "$NAME: $@"
+	exit 1
+}
 
-if [ "$EXIT" = "0" ]
+if (( $+commands[unpkg.py] ))
 then
-	echo "$NAME: Installation of $INSTALL_TO was successful."
+	# Get unpkg.py from https://github.com/tjluoma/unpkg/blob/master/unpkg.py
 
-	[[ "$LAUNCH" == "yes" ]] && open -a "$INSTALL_TO"
+	echo "$NAME: running 'unpkg.py' on '$FILENAME':"
+
+	UNPKG=`unpkg.py "$FILENAME" 2>&1`
+
+	[[ "$UNPKG" == "" ]] && die "unpkg.py failed"
+
+	EXTRACTED_TO=$(echo "$UNPKG" | egrep '^Extracted to ' | sed 's#Extracted to "##g ; s#".##g')
+
+	[[ "$EXTRACTED_TO" == "" ]] && die "unpkg.py failed (EXTRACTED_TO empty)"
+
+	if [[ -e "$INSTALL_TO" ]]
+	then
+			# If there's an existing installation, move it to the trash
+		mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}.app"
+	fi
+
+	mv -vf "$EXTRACTED_TO/$INSTALL_TO:t" "$INSTALL_TO" || die 'move failed'
+
+elif (( $+commands[pkginstall.sh] ))
+then
+
+	pkginstall.sh "$FILENAME" || die "pkginstall.sh failed"
 
 else
-	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+
+	sudo /usr/sbin/installer -verbose -pkg "$FILENAME" -dumplog -target / -lang en 2>&1 || die "sudo installer failed"
 fi
-
-
-
 
 exit 0
 #EOF
