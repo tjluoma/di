c46f0681105d15af49100e89e1b920f787d43028
TJ Luoma <github@tjluoma.com>
2018-08-08 00:18:14 -0400

    Release Note Support & Updated variable names

--- di-feeder3.sh
+++ di-feeder3.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh -f
-# Purpose: Download and install latest version of Feeder 3
+# Purpose: Download and install latest version of Feeder 3 from <https://reinventedsoftware.com/feeder/>
 #
 # From:	Tj Luo.ma
 # Mail:	luomat at gmail dot com
@@ -19,43 +19,38 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-
 INFO=($(curl -sfL "$XML_FEED" \
-| tr ' ' '\012' \
-|sed 's#>#>\
-#g' \
-| egrep '^url="|^sparkle:version|^sparkle:shortVersionString' \
-| head -3 \
-| awk -F'"' '//{print $2}'))
+	| tr ' |>|<' '\012' \
+	| egrep '^url="|^sparkle:version|^sparkle:shortVersionString' \
+	| head -3 \
+	| sort \
+	| awk -F'"' '//{print $2}'))
 
-URL="$INFO[1]"
+LATEST_VERSION="$INFO[1]"
+LATEST_BUILD="$INFO[2]"
+URL="$INFO[3]"
 
-REMOTE_BUNDLE_VERSION="$INFO[2]"
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	LATEST_BUILD: $LATEST_BUILD
+	URL: $URL
+	"
 
-REMOTE_READABLE_VERSION="$INFO[3]"
-
-	## If any of these are blank, we should not continue
-	## @TODO - edit this for the variables used by this script
-
-# if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
-# then
-# 	echo "$NAME: Error: bad data received:
-# 	REMOTE_BUNDLE_VERSION: $REMOTE_BUNDLE_VERSION
-# 	REMOTE_READABLE_VERSION: $REMOTE_READABLE_VERSION
-# 	URL: $URL
-# 	"
-#
-# 	exit 1
-# fi
+	exit 1
+fi
 
 if [[ -e "$INSTALL_TO" ]]
 then
 
 	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info"  CFBundleShortVersionString 2>/dev/null || echo '0'`
 
-	INSTALLED_BUNDLE_VERSION=`defaults read "$INSTALL_TO/Contents/Info"  CFBundleVersion 2>/dev/null || echo '0'`
+	INSTALLED_BUILD=`defaults read "$INSTALL_TO/Contents/Info"  CFBundleVersion 2>/dev/null || echo '0'`
 
-	if [ "$REMOTE_BUNDLE_VERSION" = "$INSTALLED_BUNDLE_VERSION" -a "$REMOTE_READABLE_VERSION" = "$INSTALLED_VERSION" ]
+	if [ "$LATEST_BUILD" = "$INSTALLED_BUILD" -a "$LATEST_VERSION" = "$INSTALLED_VERSION" ]
 	then
 		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
 		exit 0
@@ -63,19 +58,37 @@ then
 
 	autoload is-at-least
 
-	is-at-least "$REMOTE_BUNDLE_VERSION" "$INSTALLED_BUNDLE_VERSION"
+	is-at-least "$LATEST_BUILD" "$INSTALLED_BUILD"
 
 	if [ "$?" = "0" ]
 	then
-		echo "$NAME: Installed version ($INSTALLED_BUNDLE_VERSION) is ahead of official version $REMOTE_BUNDLE_VERSION"
+		echo "$NAME: Installed version ($INSTALLED_BUILD) is ahead of official version $LATEST_BUILD"
 		exit 0
 	fi
 
-	echo "$NAME: Outdated (Installed = $INSTALLED_BUNDLE_VERSION vs Latest = $REMOTE_BUNDLE_VERSION)"
+	echo "$NAME: Outdated (Installed = $INSTALLED_BUILD vs Latest = $LATEST_BUILD)"
 
 fi
 
-FILENAME="$HOME/Downloads/Feeder-${REMOTE_READABLE_VERSION}-${REMOTE_BUNDLE_VERSION}.dmg"
+if (( $+commands[lynx] ))
+then
+
+	RELEASE_NOTES_URL=`curl -sfL "$XML_FEED" \
+	| egrep '<sparkle:releaseNotesLink>.*</sparkle:releaseNotesLink>' \
+	| head -1 \
+	| sed 's#.*<sparkle:releaseNotesLink>##g; s#</sparkle:releaseNotesLink>.*##g;' `
+
+	echo -n "$NAME: Release Notes for $INSTALL_TO:t:r Version $LATEST_VERSION / $LATEST_BUILD: "
+
+	curl -sfL "$RELEASE_NOTES_URL" \
+	| sed '1,/<h2>/d; /<h2>/,$d' \
+	| lynx -dump -nomargins -nonumbers -width=10000 -assume_charset=UTF-8 -pseudo_inlines  -stdin
+
+	echo "Source: <$RELEASE_NOTES_URL>"
+fi
+
+	# Note that we include 'Feeder' because we don't want 'Feeder 3'
+FILENAME="$HOME/Downloads/Feeder-${LATEST_VERSION}-${LATEST_BUILD}.dmg"
 
 echo "$NAME: Downloading $URL to $FILENAME"
 
@@ -106,7 +119,7 @@ fi
 	# Move the old version (if any) to trash
 if [ -e "$INSTALL_TO" ]
 then
-	mv -vf "$INSTALL_TO" "$HOME/.Trash/Feeder 3.${INSTALLED_VERSION}.app"
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}.app"
 fi
 
 echo "$NAME: Installing $MNTPNT/$INSTALL_TO:t to $INSTALL_TO..."
