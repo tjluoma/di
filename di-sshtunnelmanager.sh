9810cc996a2f48f9440850492cf2b76adfee2951
TJ Luoma <github@tjluoma.com>
2018-08-09 21:49:27 -0400

    added RELEASE_NOTES_URL, fixed variable names, better unzip

--- di-sshtunnelmanager.sh
+++ di-sshtunnelmanager.sh
@@ -28,17 +28,17 @@ INFO=($(curl -sfL "$XML_FEED" \
 	| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
-MAJOR_VERSION="$INFO[1]"
-LATEST_VERSION="$INFO[2]"
+LATEST_VERSION="$INFO[1]"
+LATEST_BUILD="$INFO[2]"
 URL="$INFO[3]"
 
 	# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" -o "$MAJOR_VERSION" = "" ]
+if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
 then
 	echo "$NAME: Error: bad data received:
 	INFO: $INFO
-	MAJOR_VERSION: $MAJOR_VERSION
 	LATEST_VERSION: $LATEST_VERSION
+	LATEST_BUILD: $LATEST_BUILD
 	URL: $URL
 	"
 	exit 1
@@ -49,7 +49,7 @@ then
 
 	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '0'`
 
-	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	if [[ "$LATEST_BUILD" == "$INSTALLED_VERSION" ]]
 	then
 		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
 		exit 0
@@ -57,20 +57,38 @@ then
 
 	autoload is-at-least
 
-	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+	is-at-least "$LATEST_BUILD" "$INSTALLED_VERSION"
 
 	if [ "$?" = "0" ]
 	then
-		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_BUILD"
 		exit 0
 	fi
 
-	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_BUILD)"
 fi
 
-FILENAME="$HOME/Downloads/SSHTunnelManager-${MAJOR_VERSION}-${LATEST_VERSION}.zip"
+if (( $+commands[lynx] ))
+then
 
-echo "$NAME: Downloading $URL to $FILENAME"
+	RELEASE_NOTES_URL=$(curl -sfL "${XML_FEED}" \
+		| fgrep '<sparkle:releaseNotesLink>' \
+		| head -1 \
+		| sed 's#.*<sparkle:releaseNotesLink>##g ; s#</sparkle:releaseNotesLink>##g')
+
+	echo -n "$NAME: Release Notes for "
+
+	curl -sfL "${RELEASE_NOTES_URL}" \
+		| fgrep -v 'dm-rn-head-title-fixed' \
+		| lynx -dump -nomargins -nonumbers -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin \
+		| tr -s '_' '_'
+
+	echo "\nSource: <$RELEASE_NOTES_URL>"
+fi
+
+FILENAME="$HOME/Downloads/SSHTunnelManager-${LATEST_VERSION}-${LATEST_BUILD}.zip"
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
@@ -79,31 +97,69 @@ EXIT="$?"
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
-if [[ -e "$INSTALL_TO" ]]
-then
-	pgrep -xq "SSH Tunnel Manager" \
-	&& LAUNCH='yes' \
-	&& osascript -e 'tell application "SSH Tunnel Manager" to quit'
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
-		# move installed version to trash
-	mv -vf "$INSTALL_TO" "$HOME/.Trash/SSH Tunnel Manager.$INSTALLED_VERSION.app"
-fi
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
-echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
 
-ditto --noqtn -xk "$FILENAME" "$INSTALL_TO:h/"
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+
+ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
 
 EXIT="$?"
 
-if [ "$EXIT" = "0" ]
+if [[ "$EXIT" == "0" ]]
 then
-	echo "$NAME: Installation of $INSTALL_TO was successful."
-
+	echo "$NAME: Unzip successful"
 else
-	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+		# failed
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+
+	exit 1
 fi
 
-[[ "$LAUNCH" = "yes" ]] open -a "$INSTALL_TO"
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+	echo "$NAME: Moving existing (old) \"$INSTALL_TO\" to \"$HOME/.Trash/\"."
+
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
+
+		exit 1
+	fi
+fi
+
+echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
+
+	# Move the file out of the folder
+mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" = "0" ]]
+then
+
+	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+else
+	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+	exit 1
+fi
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
 
 exit 0
 #EOF
