2b78fb0f7561663948053e824dd7fe1a86ba588f
TJ Luoma <github@tjluoma.com>
2018-08-11 23:24:16 -0400

    added beta / nightly support

--- di-handbrake.sh
+++ di-handbrake.sh
@@ -10,8 +10,6 @@ NAME="$0:t:r"
 
 INSTALL_TO='/Applications/HandBrake.app'
 
-XML_FEED="https://handbrake.fr/appcast.x86_64.xml"
-
 RELEASE_NOTES_URL='https://handbrake.fr/appcast/stable.html'
 
 if [ -e "$HOME/.path" ]
@@ -23,29 +21,62 @@ fi
 
 UA='curl/7.54.0'
 
-INFO=($(curl -A "$UA" -sfL "${XML_FEED}" \
-		| tr -s ' ' '\012' \
-		| egrep 'sparkle:version|sparkle:shortVersionString|url=' \
-		| head -3 \
-		| sort \
-		| awk -F'"' '/^/{print $2}'))
 
-	# "Sparkle" will always come before "url" because of "sort"
-LATEST_VERSION="$INFO[1]"
-LATEST_BUILD="$INFO[2]"
-URL=$(echo "$INFO[3]" | sed 's#\&amp;#\&#g')
-
-	# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+	# create a file (empty, if you like) at "$HOME/.config/di/handbrake-prefer-betas.txt"
+	# if you want to install beta releases
+if [[ -e "$HOME/.config/di/handbrake-prefer-betas.txt" ]]
 then
-	echo "$NAME: Error: bad data received:
-	INFO: $INFO
-	LATEST_VERSION: $LATEST_VERSION
-	LATEST_BUILD: $LATEST_BUILD
-	URL: $URL
-	"
+		# This is for betas
+	XML_FEED='https://imageoptim.com/appcast-test.xml'
+	NAME="$NAME (beta releases)"
+	BETA='yes'
 
-	exit 1
+	URL=$(curl -A "$UA_CURL" -sfL "http://handbrake.fr/nightly.php" \
+	| fgrep -i '.dmg' \
+	| tr '"|\047' '\012' \
+	| awk -F'^' '/https/{print $1}' \
+	| fgrep -iv 'HandBrakeCLI')
+
+	# https://nightly.handbrake.fr/HandBrake-20180810154915-d667a3d-master.dmg
+
+	LATEST_VERSION=$(echo "$URL:t:r" | sed 's#HandBrake-##g')
+
+	LATEST_BUILD=""
+
+	FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.dmg"
+
+else
+	BETA='no'
+
+		## This is for official, non-beta versions
+	XML_FEED="https://handbrake.fr/appcast.x86_64.xml"
+
+	INFO=($(curl -A "$UA" -sfL "${XML_FEED}" \
+			| tr -s ' ' '\012' \
+			| egrep 'sparkle:version|sparkle:shortVersionString|url=' \
+			| head -3 \
+			| sort \
+			| awk -F'"' '/^/{print $2}'))
+
+		# "Sparkle" will always come before "url" because of "sort"
+	LATEST_VERSION="$INFO[1]"
+	LATEST_BUILD="$INFO[2]"
+	URL=$(echo "$INFO[3]" | sed 's#\&amp;#\&#g')
+
+		# If any of these are blank, we should not continue
+	if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+	then
+		echo "$NAME: Error: bad data received:
+		INFO: $INFO
+		LATEST_VERSION: $LATEST_VERSION
+		LATEST_BUILD: $LATEST_BUILD
+		URL: $URL
+		"
+
+		exit 1
+	fi
+
+	FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}_${LATEST_BUILD}.dmg"
 fi
 
 if [[ -e "$INSTALL_TO" ]]
@@ -53,25 +84,38 @@ then
 
 	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
 
-	INSTALLED_BUILD=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
-
 	autoload is-at-least
 
 	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
 	VERSION_COMPARE="$?"
 
-	is-at-least "$LATEST_BUILD" "$INSTALLED_BUILD"
-
-	BUILD_COMPARE="$?"
-
-	if [ "$VERSION_COMPARE" = "0" -a "$BUILD_COMPARE" = "0" ]
+	if [[ "$LATEST_BUILD" == "" ]]
 	then
-		echo "$NAME: Up-To-Date ($INSTALLED_VERSION/$INSTALLED_BUILD)"
-		exit 0
-	fi
 
-	echo "$NAME: Outdated: $INSTALLED_VERSION/$INSTALLED_BUILD vs $LATEST_VERSION/$LATEST_BUILD"
+		if [ "$VERSION_COMPARE" = "0" ]
+		then
+			echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+			exit 0
+		fi
+
+		echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	else
+		INSTALLED_BUILD=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
+
+		is-at-least "$LATEST_BUILD" "$INSTALLED_BUILD"
+
+		BUILD_COMPARE="$?"
+
+		if [ "$VERSION_COMPARE" = "0" -a "$BUILD_COMPARE" = "0" ]
+		then
+			echo "$NAME: Up-To-Date ($INSTALLED_VERSION/$INSTALLED_BUILD)"
+			exit 0
+		fi
+
+		echo "$NAME: Outdated: $INSTALLED_VERSION/$INSTALLED_BUILD vs $LATEST_VERSION/$LATEST_BUILD"
+	fi
 
 	FIRST_INSTALL='no'
 
@@ -80,20 +124,27 @@ else
 	FIRST_INSTALL='yes'
 fi
 
-if (( $+commands[lynx] ))
+if [[ "$BETA" == "no" ]]
 then
+	if (( $+commands[lynx] ))
+	then
 
-	RELEASE_NOTES_URL='https://handbrake.fr/appcast/stable.html'
+		RELEASE_NOTES_URL='https://handbrake.fr/appcast/stable.html'
 
-	echo -n "$NAME: Release Notes for "
+		echo -n "$NAME: Release Notes for "
 
-	lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines "$RELEASE_NOTES_URL"
+		lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines "$RELEASE_NOTES_URL"
 
-	echo "\nSource: <$RELEASE_NOTES_URL>"
+		echo "\nSource: <$RELEASE_NOTES_URL>"
+
+	fi
+else
+		# This _is_ a beta / nightly
+
+	echo "$NAME: No release notes available for nightly builds, but you can see recent changes at:"
+	echo "<https://github.com/HandBrake/HandBrake/commits/master>"
 
 fi
-
-FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}_${LATEST_BUILD}.dmg"
 
 echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
