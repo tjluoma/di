459b8cdda36ceda9e15a8986f18baceddacf74d5
TJ Luoma <github@tjluoma.com>
2018-08-13 14:40:37 -0400

    added (optional) beta support

--- di-defaultfolder.sh
+++ di-defaultfolder.sh
@@ -16,95 +16,156 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-	# This is the URL actually given by the feed itself
-	# 	XML_FEED='http://www.stclairsoft.com/updates/DefaultFolderX5.xml'
-	# but this is the URL found in the app itself
-XML_FEED='https://www.stclairsoft.com/cgi-bin/sparkle.cgi?DX5'
-
-if [[ -e "$INSTALL_TO" ]]
+	# create a file (empty, if you like) at "$HOME/.config/di/defaultfolderx-prefer-betas.txt"
+	# if you want to install beta releases
+if [ -e "$HOME/.config/di/defaultfolderx-prefer-betas.txt" ]
 then
+		# This is for betas
+	URL=$(curl -sfL 'http://www.stclairsoft.com/cgi-bin/dl.cgi?DX-B' \
+		| egrep -i '<META HTTP-EQUIV="Refresh" CONTENT="0;URL=' \
+		| sed 's#.*https#https#g; s#.dmg.*#.dmg#g')
 
-	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString`
+	## This is another way of calculating the URL from the same page
+	#WEB_SCRAPING_URL=$(curl -sfL 'http://www.stclairsoft.com/cgi-bin/dl.cgi?DX-B' \
+	#	| tr -s '"|\047|=' '\012' \
+	#	| egrep '^https.*\.dmg' \
+	#	| tail -1)
 
-	INSTALLED_BUILD=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion`
+	LATEST_VERSION=$(echo "$URL:t:r" | sed 's#DefaultFolderX-##g' )
 
-		# User Agent = Default Folder X/5.0b1 Sparkle/58
-	UA="Default Folder X/$INSTALLED_VERSION Sparkle/$INSTALLED_BUILD"
+	NAME="$NAME (beta releases)"
 
-else
-		# This is current info as of 2018-08-02
-	UA="Default Folder X/5.2.5 Sparkle/483"
-fi
-
-INFO=($(curl -sfL -A "$UA" "$XML_FEED" \
-		 | tr -s ' ' '\012' \
-		 | egrep '^(sparkle:shortVersionString|url|sparkle:version)=' \
-		 | sort \
-		 | head -3 \
-		 | awk -F'"' '//{print $2}'))
-
-## Expected output something like:
-#
-# 5.2.5
-# 483
-# https://www.stclairsoft.com/download/DefaultFolderX-5.2.5.dmg
-
-LATEST_VERSION="$INFO[1]"
-LATEST_BUILD="$INFO[2]"
-URL="$INFO[3]"
-
-if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" -o "$LATEST_BUILD" = "" ]
-then
-	echo "$NAME Error: bad data received:
-	INFO: $INFO
-	LATEST_VERSION: $LATEST_VERSION
-	LATEST_BUILD: $LATEST_BUILD
-	URL: $URL\n"
-
-	exit 1
-fi
-
-if [[ -e "$INSTALL_TO" ]]
-then
-
-	if [[ "$INSTALLED_VERSION" = "$LATEST_VERSION" ]]
+	if [[ -e "$INSTALL_TO" ]]
 	then
-		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
-		exit 0
+
+		INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+		autoload is-at-least
+
+		is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+		VERSION_COMPARE="$?"
+
+		if [ "$VERSION_COMPARE" = "0" ]
+		then
+			echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+			exit 0
+		fi
+
+		echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+		FIRST_INSTALL='no'
+
+	else
+
+		FIRST_INSTALL='yes'
 	fi
 
-	autoload is-at-least
+	RELEASE_NOTES_URL="https://www.stclairsoft.com/DefaultFolderX/beta.html"
 
-	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
-
-	if [ "$?" = "0" ]
-	then
-		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
-		exit 0
-	fi
-
-	echo "$NAME: Outdated (Installed = $INSTALLED_BUILD vs Latest = $LATEST_VERSION)"
-
-fi
-
-if (( $+commands[lynx] ))
-then
-
-	RELEASE_NOTES_URL=$(curl -sfL "$XML_FEED" \
-	| sed '1,/<sparkle:releaseNotesLink xml:lang="en">/d; /<\/sparkle:releaseNotesLink>/,$d' \
-	| tr -d '[:blank:]')
-
-	echo "$NAME: Release Notes for $INSTALL_TO:t:r version $LATEST_VERSION/$LATEST_BUILD: \n"
+	echo "$NAME: Release Notes for Default Folder X:"
 
 	curl -sfL "$RELEASE_NOTES_URL" \
-	| sed '1,/<h3>/d; /<h3>/,$d' \
-	| lynx -dump -nomargins -width=10000 -assume_charset=UTF-8 -pseudo_inlines -stdin
+	| sed '1,/<!-- InstanceBeginEditable name="main editable" -->/d; /<H3>How to be a beta tester:<\/H3>/,$d' \
+	| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin
 
-	echo "\nSource: <${RELEASE_NOTES_URL}>"
+	echo "\nSource: <$RELEASE_NOTES_URL>"
+
+	FILENAME="$HOME/Downloads/DefaultFolderX-beta-${LATEST_VERSION}.dmg"
+
+else
+
+		# This is the URL actually given by the feed itself
+		# 	XML_FEED='http://www.stclairsoft.com/updates/DefaultFolderX5.xml'
+		# but this is the URL found in the app itself
+	XML_FEED='https://www.stclairsoft.com/cgi-bin/sparkle.cgi?DX5'
+
+	if [[ -e "$INSTALL_TO" ]]
+	then
+
+		INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString`
+
+		INSTALLED_BUILD=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion`
+
+			# User Agent = Default Folder X/5.0b1 Sparkle/58
+		UA="Default Folder X/$INSTALLED_VERSION Sparkle/$INSTALLED_BUILD"
+
+	else
+			# This is current info as of 2018-08-02
+		UA="Default Folder X/5.2.5 Sparkle/483"
+	fi
+
+	INFO=($(curl -sfL -A "$UA" "$XML_FEED" \
+			 | tr -s ' ' '\012' \
+			 | egrep '^(sparkle:shortVersionString|url|sparkle:version)=' \
+			 | sort \
+			 | head -3 \
+			 | awk -F'"' '//{print $2}'))
+
+	## Expected output something like:
+	#
+	# 5.2.5
+	# 483
+	# https://www.stclairsoft.com/download/DefaultFolderX-5.2.5.dmg
+
+	LATEST_VERSION="$INFO[1]"
+	LATEST_BUILD="$INFO[2]"
+	URL="$INFO[3]"
+
+	if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" -o "$LATEST_BUILD" = "" ]
+	then
+		echo "$NAME Error: bad data received:
+		INFO: $INFO
+		LATEST_VERSION: $LATEST_VERSION
+		LATEST_BUILD: $LATEST_BUILD
+		URL: $URL\n"
+
+		exit 1
+	fi
+
+	if [[ -e "$INSTALL_TO" ]]
+	then
+
+		if [[ "$INSTALLED_VERSION" = "$LATEST_VERSION" ]]
+		then
+			echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+			exit 0
+		fi
+
+		autoload is-at-least
+
+		is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+		if [ "$?" = "0" ]
+		then
+			echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+			exit 0
+		fi
+
+		echo "$NAME: Outdated (Installed = $INSTALLED_BUILD vs Latest = $LATEST_VERSION)"
+
+	fi
+
+	if (( $+commands[lynx] ))
+	then
+
+		RELEASE_NOTES_URL=$(curl -sfL "$XML_FEED" \
+		| sed '1,/<sparkle:releaseNotesLink xml:lang="en">/d; /<\/sparkle:releaseNotesLink>/,$d' \
+		| tr -d '[:blank:]')
+
+		echo "$NAME: Release Notes for $INSTALL_TO:t:r version $LATEST_VERSION/$LATEST_BUILD: \n"
+
+		curl -sfL "$RELEASE_NOTES_URL" \
+		| sed '1,/<h3>/d; /<h3>/,$d' \
+		| lynx -dump -nomargins -width=10000 -assume_charset=UTF-8 -pseudo_inlines -stdin
+
+		echo "\nSource: <${RELEASE_NOTES_URL}>"
+
+	fi
+
+	FILENAME="$HOME/Downloads/DefaultFolderX-${LATEST_VERSION}_${LATEST_BUILD}.dmg"
 
 fi
-
-FILENAME="$HOME/Downloads/DefaultFolderX-${LATEST_VERSION}_${LATEST_BUILD}.dmg"
 
 echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
@@ -132,15 +193,28 @@ fi
 
 echo "$NAME: Installing $FILENAME to $INSTALL_TO:h/"
 
+echo "$NAME: Mounting $FILENAME:"
+
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
-		| fgrep -A 1 '<key>mount-point</key>' \
-		| tail -1 \
-		| sed 's#</string>.*##g ; s#.*<string>##g')
+	| fgrep -A 1 '<key>mount-point</key>' \
+	| tail -1 \
+	| sed 's#</string>.*##g ; s#.*<string>##g')
 
 if [[ "$MNTPNT" == "" ]]
 then
-	echo "$NAME: failed to mount $FILENAME. (MNTPNT is empty)"
+	echo "$NAME: MNTPNT is empty"
 	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+		# Quit app, if running
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
 fi
 
 echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
@@ -149,18 +223,20 @@ ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
 
 EXIT="$?"
 
-if [[ "$EXIT" != "0" ]]
+if [[ "$EXIT" == "0" ]]
 then
+	echo "$NAME: Successfully installed $INSTALL_TO"
+else
 	echo "$NAME: ditto failed"
 
 	exit 1
 fi
 
-echo "$NAME: Installation successful. Ejecting $MNTPNT:"
+[[ "$LAUNCH" = "yes" ]] && echo "$NAME: Re-Launching '$INSTALL_TO':"  && open -a "$INSTALL_TO"
+
+echo "$NAME: Unmounting $MNTPNT:"
 
 diskutil eject "$MNTPNT"
-
-[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
 
 if (is-growl-running-and-unpaused.sh)
 then
