0c6b3e662c440b5df5e1d9c2fdb81522a3296677
TJ Luoma <github@tjluoma.com>
2018-08-15 11:28:19 -0400

    Download & Install Microsoft OneDrive

---
+++ di-onedrive.sh
@@ -0,0 +1,89 @@
+#!/bin/zsh -f
+# Purpose: Download and install the latest OneDrive client from Microsoft: <https://onedrive.live.com/about/en-us/download/>
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2018-08-15
+
+NAME="$0:t:r"
+
+INSTALL_TO="/Applications/OneDrive.app"
+
+#   https://rink.hockeyapp.net/api/2/apps/58bdcef2b1db4db38e0c2fb8a84ac168 -- outdated?
+
+if [[ -e "$HOME/.path" ]]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+URL=$(curl -sSfL --head 'https://go.microsoft.com/fwlink/?LinkId=823060' | awk -F' |\r' '/^.ocation/{print $2}' | tail -1)
+
+LATEST_VERSION=$(echo "$URL:h")
+
+LATEST_VERSION=$(echo "$LATEST_VERSION:t")
+
+	# If either of these are blank, we should not continue
+if [ "$URL" = "" -o "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+echo "$LATEST_VERSION"
+
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.pkg"
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+if (( $+commands[pkginstall.sh] ))
+then
+	pkginstall.sh "$FILENAME"
+else
+	sudo /usr/sbin/installer -verbose -pkg "$FILENAME" -dumplog -target / -lang en 2>&1
+fi
+
+exit 0
+#EOF
