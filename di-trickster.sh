78d6538081a0f1b83f2daeb482b9a18db459e9c3
TJ Luoma <github@tjluoma.com>
2018-08-16 19:46:27 -0400

    added Mac App Store link for MAS installs

--- di-trickster.sh
+++ di-trickster.sh
@@ -69,6 +69,15 @@ then
 
 	FIRST_INSTALL='no'
 
+	if [[ -e "$INSTALL_TO/Contents/_MASReceipt/receipt" ]]
+	then
+		echo "$NAME: $INSTALL_TO was installed from the Mac App Store and cannot be updated by this script."
+		echo "	See <https://itunes.apple.com/us/app/trickster/id509641724?mt=12> or"
+		echo "	<macappstore://itunes.apple.com/us/app/trickster/id509641724>"
+		echo "	Please use the App Store app to update it: <macappstore://showUpdatesPage?scan=true>"
+		exit 0
+	fi
+
 else
 
 	FIRST_INSTALL='yes'
