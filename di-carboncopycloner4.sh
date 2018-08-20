e1269369e12e92a9a51e8be8897e938c4701df2c
TJ Luoma <github@tjluoma.com>
2018-08-20 00:20:49 -0400

    fixed bug where it would install v5 over v4

--- di-carboncopycloner4.sh
+++ di-carboncopycloner4.sh
@@ -16,60 +16,45 @@ fi
 
 INSTALL_TO='/Applications/Carbon Copy Cloner 4.app'
 
-	## NOTE: If nothing is installed, we need to pretend we have at least version 4
-	## 			or else we will get version 3
-INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '4.0.0'`
+URL=$(curl -sfL --head 'https://bombich.com/software/download_ccc.php?v=4.1.23.4644' | awk -F' |\r' '/^.ocation/{print $2}')
 
-	## NOTE: If nothing is installed, we need to pretend we have at least version 4000
-	## 			or else we will get version 3
-INSTALLED_BUNDLE_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '4000'`
+LATEST_VERSION="4.1.23"
 
-OS_MINOR=`sw_vers -productVersion | cut -d. -f 2`
-
-OS_BUGFIX=`sw_vers -productVersion | cut -d. -f 3`
-
-XML_FEED="https://bombich.com/software/updates/ccc.php?os_minor=$OS_MINOR&os_bugfix=$OS_BUGFIX&ccc=$INSTALLED_BUNDLE_VERSION&beta=0&locale=en"
-
-INFO=($(curl -sfL "$XML_FEED" \
-		| gunzip \
-		| tr -s ' ' '\012' \
-		| egrep 'sparkle:version|url="' \
-		| head -2 \
-		| sort \
-		| awk -F'"' '//{print $2}'))
-
-# "Sparkle" will always come before "url" because of "sort"
-LATEST_VERSION="$INFO[1]"
-URL="$INFO[2]"
-
-# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+	# If either of these are blank, we should not continue
+if [ "$URL" = "" -o "$LATEST_VERSION" = "" ]
 then
-	echo "$NAME: Error: bad data received from ${XML_FEED}\nINFO: $INFO"
-	exit 0
+	echo "$NAME: Error: bad data received:
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
 fi
 
-if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+if [[ -e "$INSTALL_TO" ]]
 then
-	echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
-	exit 0
+
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
 fi
-
-autoload is-at-least
-
-is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
-
-if [ "$?" = "0" ]
-then
-	echo "$NAME: Up-To-Date (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
-	exit 0
-fi
-
-echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
 FILENAME="$HOME/Downloads/CarbonCopyCloner-${LATEST_VERSION}.zip"
 
-echo "$NAME: Downloading $URL to $FILENAME"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
