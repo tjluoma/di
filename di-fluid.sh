8d2a270b164d554bb2870c41d39069d01fb0a39b
TJ Luoma <github@tjluoma.com>
2018-08-20 11:45:36 -0400

    di-fluid1 and di-fluid2 combined to di-fluid.sh

---
+++ di-fluid.sh
@@ -0,0 +1,193 @@
+#!/bin/zsh -f
+# Purpose: Download and install the latest version of Fluid (1 or 2, depending on which, if either, is installed)
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2018-08-20
+
+NAME="$0:t:r"
+
+INSTALL_TO='/Applications/Fluid.app'
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
+fi
+
+	# default to Fluid 2, unless Fluid 1 is already installed
+XML_FEED="https://fluidapp.com/appcast/fluid2.rss"
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	MAJOR_VERSION=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d. -f1)
+
+	if [[ "$MAJOR_VERSION" == "1" ]]
+	then
+		XML_FEED="https://fluidapp.com/appcast/fluid1.rss"
+	fi
+
+fi
+
+INFO=($(curl -sfL "$XML_FEED" \
+		| tr '[:blank:]' '\012' \
+		| egrep '^(sparkle:version="|sparkle:shortVersionString="|url="http.*\.zip")' \
+		| head -3 \
+		| sort \
+		| awk -F'"' '//{print $2}'))
+
+	## Sort order will result in something like this:
+	# sparkle:shortVersionString="2.0"
+	# sparkle:version="1905"
+	# url="https://fluidapp.com/dist/Fluid_2.0.zip"
+	#
+	## NOTE: sparkle:version is changes even when sparkle:shortVersionString doesn't
+
+LATEST_VERSION="$INFO[1]"
+
+LATEST_BUILD="$INFO[2]"
+
+URL="$INFO[3]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	LATEST_BUILD: $LATEST_BUILD
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+	INSTALLED_BUILD=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	is-at-least "$LATEST_BUILD" "$INSTALLED_BUILD"
+
+	BUILD_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" -a "$BUILD_COMPARE" = "0" ]
+	then
+		echo -n "$NAME: Up-To-Date ($INSTALLED_VERSION/$INSTALLED_BUILD)"
+
+		if [[ "$MAJOR_VERSION" == "1" ]]
+		then
+			echo " (Note that version 2 is available. See <https://www.fluidapp.com> for details.)"
+		else
+			echo
+		fi
+
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION/$INSTALLED_BUILD vs $LATEST_VERSION/$LATEST_BUILD"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+if (( $+commands[lynx] ))
+then
+
+	RELEASE_NOTES_URL="$XML_FEED"
+
+	echo -n "$NAME: Release Notes for: "
+
+	curl -sfL "$RELEASE_NOTES_URL" \
+	| sed '1,/<description>/d; /<\/description>/,$d ; s#&lt;#<#g' \
+	| lynx -dump -nomargins -width=10000 -assume_charset=UTF-8 -pseudo_inlines -stdin
+
+	echo "\nSource: XML_FEED <$RELEASE_NOTES_URL>"
+
+fi
+
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}_${LATEST_BUILD}.zip"
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
+
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+
+ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Unzip successful"
+else
+		# failed
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+	echo "$NAME: Moving existing (old) \"$INSTALL_TO\" to \"$HOME/.Trash/\"."
+
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
+
+		exit 1
+	fi
+fi
+
+echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
+
+	# Move the file out of the folder
+mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" = "0" ]]
+then
+
+	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+else
+	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+	exit 1
+fi
+
+exit 0
+
+#
+#EOF
