29daddc0f79e645fbed3137130c5f239f0c77d5e
TJ Luoma <github@tjluoma.com>
2018-08-21 04:56:02 -0400

    updated to install versions 8, 9, or 10 (latest)

--- di-pdfpenpro.sh
+++ di-pdfpenpro.sh
@@ -1,18 +1,13 @@
 #!/bin/zsh -f
-# Purpose: Download and install latest version of PDFPenPro from <https://smilesoftware.com/pdfpenpro/>
+# Purpose: Download and install PDFPenPro version 8, 9, or 10 from <https://smilesoftware.com/pdfpenpro/>
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
-# Date:	2015-02-12
-## 2015-10-26 - rewritten to use
-## https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml
-#
-# 2018-07-17 - found alternate URL which seems to have identical info:
-# https://updates.devmate.com/com.smileonmymac.PDFpenPro.xml
+# Date:	2018-08-21
 
 NAME="$0:t:r"
+
 INSTALL_TO='/Applications/PDFpenPro.app'
-XML_FEED='https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml'
 
 if [ -e "$HOME/.path" ]
 then
@@ -21,30 +16,96 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-INFO=($(curl -sfL "$XML_FEED" \
-		| tr -s ' ' '\012' \
-		| egrep '^(sparkle:version|sparkle:shortVersionString|url=)' \
-		| head -3 \
-		| sort \
-		| awk -F'"' '/^/{print $2}'))
+function use_v8 {
 
-LATEST_VERSION="$INFO[1]"
+	USE_VERSION='8'
 
-LATEST_BUILD="$INFO[2]"
+	LATEST_VERSION="8.3.4"
 
-URL="$INFO[3]"
+	LATEST_BUILD="834.1"
 
-	# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+	URL="https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/834.1/1491270914/PDFpenPro-834.1.zip"
+
+	ASTERISK='(Note that version 10 is also available.)'
+
+}
+
+function use_v9 {
+
+	USE_VERSION='9'
+
+	LATEST_VERSION="9.2.3"
+
+	LATEST_BUILD="923.0"
+
+	URL="https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/923.0/1510025685/PDFpenPro-923.0.zip"
+
+	ASTERISK='(Note that version 10 is also available.)'
+
+}
+
+function use_v10 {
+
+	USE_VERSION='10'
+
+	ITUNES_URL='itunes.apple.com/us/app/pdfpenpro-10/id1359910358'
+
+		# 2018-07-17 - found alternate URL which seems to have identical info:
+		# https://updates.devmate.com/com.smileonmymac.PDFpenPro.xml
+
+	XML_FEED='https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml'
+
+	INFO=($(curl -sfL "$XML_FEED" \
+			| tr -s ' ' '\012' \
+			| egrep '^(sparkle:version|sparkle:shortVersionString|url=)' \
+			| head -3 \
+			| sort \
+			| awk -F'"' '/^/{print $2}'))
+
+	LATEST_VERSION="$INFO[1]"
+
+	LATEST_BUILD="$INFO[2]"
+
+	URL="$INFO[3]"
+
+		# If any of these are blank, we should not continue
+	if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+	then
+		echo "$NAME: Error: bad data received:
+		INFO: $INFO
+		LATEST_VERSION: $LATEST_VERSION
+		LATEST_BUILD: $LATEST_BUILD
+		URL: $URL
+		"
+
+		exit 1
+	fi
+}
+
+if [[ -e "$INSTALL_TO" ]]
 then
-	echo "$NAME: Error: bad data received:
-	INFO: $INFO
-	LATEST_VERSION: $LATEST_VERSION
-	LATEST_BUILD: $LATEST_BUILD
-	URL: $URL
-	"
+		# if v8 or 9 are installed, check that. Otherwise, use v10
+	MAJOR_VERSION=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d. -f1)
 
-	exit 1
+	if   [[ "$MAJOR_VERSION" == "8" ]]
+	then
+		use_v8
+	elif [[ "$MAJOR_VERSION" == "9" ]]
+	then
+		use_v9
+	else
+		use_v10
+	fi
+else
+	if   [ "$1" = "--use8" -o "$1" = "-8" ]
+	then
+		use_v8
+	elif [ "$1" = "--use9" -o "$1" = "-9" ]
+	then
+		use_v9
+	else
+		use_v10
+	fi
 fi
 
 if [[ -e "$INSTALL_TO" ]]
@@ -66,7 +127,7 @@ then
 
 	if [ "$VERSION_COMPARE" = "0" -a "$BUILD_COMPARE" = "0" ]
 	then
-		echo "$NAME: Up-To-Date ($INSTALLED_VERSION/$INSTALLED_BUILD)"
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION/$INSTALLED_BUILD) $ASTERISK"
 		exit 0
 	fi
 
@@ -77,8 +138,13 @@ then
 	if [[ -e "$INSTALL_TO/Contents/_MASReceipt/receipt" ]]
 	then
 		echo "$NAME: $INSTALL_TO was installed from the Mac App Store and cannot be updated by this script."
-		echo "	See <https://itunes.apple.com/us/app/pdfpenpro-10/id1359910358?mt=12> or"
-		echo "	<macappstore://itunes.apple.com/us/app/pdfpenpro-10/id1359910358>"
+
+		if [[ "$ITUNES_URL" != "" ]]
+		then
+			echo "	See <https://$ITUNES_URL?mt=12> or"
+			echo "	<macappstore://$ITUNES_URL>"
+		fi
+
 		echo "	Please use the App Store app to update it: <macappstore://showUpdatesPage?scan=true>"
 		exit 0
 	fi
@@ -88,23 +154,27 @@ else
 	FIRST_INSTALL='yes'
 fi
 
-if (( $+commands[lynx] ))
+if [[ "$USE_VERSION" == "10" ]]
 then
 
-	RELEASE_NOTES_URL=$(curl -sfL "$XML_FEED" \
-		| fgrep '<sparkle:releaseNotesLink>' \
-		| head -1 \
-		| sed 's#.*<sparkle:releaseNotesLink>##g ; s#</sparkle:releaseNotesLink>##g')
+	if (( $+commands[lynx] ))
+	then
 
-	echo -n "$NAME: Release Notes for "
+		RELEASE_NOTES_URL=$(curl -sfL "$XML_FEED" \
+			| fgrep '<sparkle:releaseNotesLink>' \
+			| head -1 \
+			| sed 's#.*<sparkle:releaseNotesLink>##g ; s#</sparkle:releaseNotesLink>##g')
 
-	curl -sfL "$RELEASE_NOTES_URL" \
-	| fgrep -v 'please note: PDFpenPro 10 is a paid upgrade' \
-	| sed '1,/<div class="dm-rn-head-title-fixed">/d' \
-	| sed '/<\/ul><\/li>/,$d' \
-	| lynx -dump -nomargins -width='90' -assume_charset=UTF-8 -pseudo_inlines -stdin
+		echo -n "$NAME: Release Notes for "
 
-	echo "\nSource: <$RELEASE_NOTES_URL>"
+		curl -sfL "$RELEASE_NOTES_URL" \
+		| fgrep -v 'please note: PDFpenPro 10 is a paid upgrade' \
+		| sed '1,/<div class="dm-rn-head-title-fixed">/d' \
+		| sed '/<\/ul><\/li>/,$d' \
+		| lynx -dump -nomargins -width='90' -assume_charset=UTF-8 -pseudo_inlines -stdin
+
+		echo "\nSource: <$RELEASE_NOTES_URL>"
+	fi
 fi
 
 ## Download it
