5886ec59a9b86139e84febd491c5851f819f8f45
TJ Luoma <github@tjluoma.com>
2018-08-21 00:39:08 -0400

    Will install either v6 or 7

---
+++ di-omnigraffle.sh
@@ -0,0 +1,169 @@
+#!/bin/zsh -f
+# Purpose: Download and install the latest version of OmniGraffle 6 or 7
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2016-05-10
+
+NAME="$0:t:r"
+
+INSTALL_TO='/Applications/OmniGraffle.app'
+
+if [ -e "$HOME/.path" ]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+function use_v6 { XML_FEED="http://update.omnigroup.com/appcast/com.omnigroup.OmniGraffle6" }
+
+function use_v7 { XML_FEED="http://update.omnigroup.com/appcast/com.omnigroup.OmniGraffle7" }
+
+if [[ -e "$INSTALL_TO" ]]
+then
+		# if v6 is installed, check that. Otherwise, use v7
+	MAJOR_VERSION=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d. -f1)
+
+	if [[ "$MAJOR_VERSION" == "6" ]]
+	then
+		use_v6
+	else
+		use_v7
+	fi
+else
+	if [ "$1" = "--use6" -o "$1" = "-6" ]
+	then
+		use_v6
+	else
+		use_v7
+	fi
+fi
+
+LAUNCH='no'
+
+## Note: Downloads are available in tbz2 and dmg but dmg has EULA so I use tbz2
+
+INFO=($(curl -sfL $XML_FEED \
+		| tidy 	--char-encoding utf8 --force-output yes --indent no --input-xml yes --markup yes --output-xhtml no \
+				--output-xml yes --quiet yes --quote-ampersand yes --quote-marks yes --quote-nbsp no --show-errors 0 \
+				--show-warnings no --uppercase-attributes no --uppercase-tags no --wrap 0 \
+		| egrep '<omniappcast:marketingVersion>|url=.*\.tbz2' \
+		| head -2 \
+		| sed 's#<omniappcast:marketingVersion>##g ; s#<\/omniappcast:marketingVersion>##g ; s#.*url="##g ; s#".*##g ; '))
+
+LATEST_VERSION="$INFO[1]"
+
+URL="$INFO[2]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info"  CFBundleShortVersionString 2>/dev/null || echo '0'`
+
+	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	if [ "$?" = "0" ]
+	then
+		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version $LATEST_VERSION"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+
+fi
+
+if (( $+commands[lynx] ))
+then
+
+	RELEASE_NOTES_URL=$(curl -sfL $XML_FEED \
+		| tidy 	--char-encoding utf8 --force-output yes --indent no --input-xml yes --markup yes --output-xhtml no \
+				--output-xml yes --quiet yes --quote-ampersand yes --quote-marks yes --quote-nbsp no --show-errors 0 \
+				--show-warnings no --uppercase-attributes no --uppercase-tags no --wrap 0 \
+		| fgrep '<omniappcast:releaseNotesLink>' \
+		| sed 's#<omniappcast:releaseNotesLink>##g ; s#<\/omniappcast:releaseNotesLink>##g ; ' \
+		| head -1)
+
+	curl -sfL "$RELEASE_NOTES_URL" \
+	| sed "1,/<section class='latest'>/d; /<\/section>/,\$d" \
+	| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin
+
+	echo "\nSource: <$RELEASE_NOTES_URL>"
+
+fi
+
+FILENAME="$HOME/Downloads/OmniGraffle-$LATEST_VERSION.tbz2"
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
+
+echo "$NAME: Unpacking '$FILENAME' to '$UNZIP_TO':"
+
+tar -x -C "$UNZIP_TO" -f "$FILENAME"
+
+EXIT="$?"
+
+if [[ "$EXIT" != "0" ]]
+then
+	echo "$NAME: tar failed (\$EXIT = $EXIT)"
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
+		exit 1
+	fi
+fi
+	# Note name difference
+mv -vf "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+exit 0
+
+#EOF
