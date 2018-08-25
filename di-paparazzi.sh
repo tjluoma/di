5d1838b674b1c362bb3f6bf25ee3d2862104771f
TJ Luoma <github@tjluoma.com>
2018-08-25 12:48:58 -0400

    just a stub to remind me to do this later

---
+++ di-paparazzi.sh
@@ -0,0 +1,22 @@
+#!/bin/zsh -f
+# Purpose: https://derailer.org/paparazzi/
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2018-08-25
+
+NAME="$0:t:r"
+
+if [[ -e "$HOME/.path" ]]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+
+# @todo - finish
+
+
+exit 0
+#EOF
