3dac2bdead0f75e097b7eaa17a117dcd478636bd
TJ Luoma <github@tjluoma.com>
2018-09-07 01:15:13 -0400

    info, 'teefile', and 'nobrowse' improved

--- di-audiobookbuilder.sh
+++ di-audiobookbuilder.sh
@@ -14,6 +14,16 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
+HOMEPAGE="http://www.splasm.com/audiobookbuilder/"
+
+DOWNLOAD_PAGE="http://www.splasm.com/downloads/audiobookbuilder/Audiobook%20Builder.dmg"
+
+SUMMARY="Audiobook Builder makes it easy to turn your audio CDs and files into audiobooks for your iPhone, iPod or iPad. Join audio, create enhanced chapter stops, adjust quality settings and let Audiobook Builder handle the rest. When it finishes you get one or a few audiobook tracks in iTunes instead of hundreds or even thousands of music tracks!"
+
+	## Since the XML_FEED doesn't specify an enclosure url, I assume this
+	## will always point to the latest version
+URL="http://www.splasm.com/downloads/audiobookbuilder/Audiobook%20Builder.dmg"
+
 	## Where should the app be installed to?
 INSTALL_TO='/Applications/Audiobook Builder.app'
 
@@ -87,83 +97,88 @@ fi
 	## If we get here, we need to update
 echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
 
-	if [[ -e "$INSTALL_TO/Contents/_MASReceipt/receipt" ]]
-	then
-		echo "$NAME: $INSTALL_TO was installed from the Mac App Store and cannot be updated by this script."
-		echo "	See <https://itunes.apple.com/us/app/audiobook-builder/id406226796?mt=12> or"
-		echo "	<macappstore://itunes.apple.com/us/app/audiobook-builder/id406226796>"
-		echo "	Please use the App Store app to update it: <macappstore://showUpdatesPage?scan=true>"
-		exit 0
-	fi
+if [[ -e "$INSTALL_TO/Contents/_MASReceipt/receipt" ]]
+then
+	echo "$NAME: $INSTALL_TO was installed from the Mac App Store and cannot be updated by this script."
+	echo "	See <https://itunes.apple.com/us/app/audiobook-builder/id406226796?mt=12> or"
+	echo "	<macappstore://itunes.apple.com/us/app/audiobook-builder/id406226796>"
+	echo "	Please use the App Store app to update it: <macappstore://showUpdatesPage?scan=true>"
+	exit 0
+fi
+
+	## Save the DMG but put the version number in the filename
+	## so I'll know what version it is later
+FILENAME="$HOME/Downloads/AudioBookBuilder-${LATEST_VERSION}.dmg"
 
 if (( $+commands[lynx] ))
 then
 
 	RELEASE_NOTES_URL="$XML_FEED"
 
-	echo "$NAME: Release Notes for $INSTALL_TO:t:r version $LATEST_VERSION:"
-
-	curl -sfL "$RELEASE_NOTES_URL" \
-	| sed '1,/<message>/d; /<\/message>/,$d ; s#\]\]\>##g ; s#<\!\[CDATA\[##g' \
-	| lynx -dump -nomargins -width=10000 -assume_charset=UTF-8 -pseudo_inlines -stdin
-
-	echo "\nSource: XML_FEED: <$XML_FEED>"
+	( echo "$NAME: Release Notes for $INSTALL_TO:t:r version $LATEST_VERSION:" ;
+		curl -sfL "$RELEASE_NOTES_URL" \
+		| sed '1,/<message>/d; /<\/message>/,$d ; s#\]\]\>##g ; s#<\!\[CDATA\[##g' \
+		| lynx -dump -nomargins -width=10000 -assume_charset=UTF-8 -pseudo_inlines -stdin ;
+		echo "\nSource: XML_FEED: <$XML_FEED>" ) | tee -a "$FILENAME:r.txt"
 fi
 
-	## Since the XML_FEED doesn't specify an enclosure url, I assume this
-	## will always point to the latest version
-URL="http://www.splasm.com/downloads/audiobookbuilder/Audiobook%20Builder.dmg"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
-	## Save the DMG but put the version number in the filename
-	## so I'll know what version it is later
-FILENAME="$HOME/Downloads/AudioBookBuilder-${LATEST_VERSION}.dmg"
-
-	## tell the user we are downloading it
-echo "$NAME: Downloading $URL to $FILENAME"
-
-	## n.b. have to pretend to be Safari or else website will ignore us
-curl -A Safari --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
 	## exit 22 means 'the file was already fully downloaded'
 [ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
 
-	## Mount the DMG that we downloaded
-MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
-		| fgrep -A 1 '<key>mount-point</key>' \
-		| tail -1 \
-		| sed 's#</string>.*##g ; s#.*<string>##g')
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
-	## Make sure that it mounted
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+echo "$NAME: Mounting $FILENAME:"
+
+MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
+	| fgrep -A 1 '<key>mount-point</key>' \
+	| tail -1 \
+	| sed 's#</string>.*##g ; s#.*<string>##g')
+
 if [[ "$MNTPNT" == "" ]]
 then
 	echo "$NAME: MNTPNT is empty"
 	exit 1
+else
+	echo "$NAME: MNTPNT is $MNTPNT"
 fi
 
-	## If there is already an installed version we need to move it aside
-if [ -e "$INSTALL_TO" ]
+if [[ -e "$INSTALL_TO" ]]
 then
-		## DON'T quit app
-		## because if it is in the middle of making an audiobook
-		## that would be Very Bad.
-		##
-		## Instead, just move it to the Trash and let it keep running
-		## until user quits it.
+		# Quit app, if running
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
 
-			# move installed version to trash
-		mv -vf "$INSTALL_TO" "$HOME/.Trash/AudioBook Builder.$INSTALLED_VERSION.app"
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
 fi
 
-	## tell the user what we are installing, and to where
-echo "\n$NAME Installing \"$MNTPNT/Audiobook Builder.app\" to $INSTALL_TO:"
+echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
 
-	## This is where it actually installs
-	## and, if successful, will unmount/eject the DMG
-ditto --noqtn -v "$MNTPNT/Audiobook Builder.app" "$INSTALL_TO" \
-&& diskutil eject "$MNTPNT"
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
 
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Successfully installed $INSTALL_TO"
+else
+	echo "$NAME: ditto failed"
+
+	exit 1
+fi
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
 
 exit 0
 #EOF
