55f653bbf6cfb380726067ef1df82ade0fa4d314
TJ Luoma <github@tjluoma.com>
2018-09-07 16:08:27 -0400

    Initial Commit

---
+++ di-omniweb.sh
@@ -0,0 +1,150 @@
+#!/bin/zsh -f
+# Purpose: Download and install the latest version of OmniWeb
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2018-09-07
+
+NAME="$0:t:r"
+
+if [[ -e "$HOME/.path" ]]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+INSTALL_TO="/Applications/OmniWeb.app"
+
+HOMEPAGE="https://www.omnigroup.com/more"
+
+DOWNLOAD_PAGE="https://omnistaging.omnigroup.com/omniweb/"
+
+SUMMARY="OmniWeb is a powerful, award-winning, feature-rich alternative to the standard web browser. Better organization and more fun with the original side-tabbed browser."
+
+	# 2018-09-07 - this seems to be outdated
+	# XML_FEED="https://update.omnigroup.com/appcast/com.omnigroup.OmniWeb6"
+
+INFO=($(curl -sfLS 'https://omnistaging.omnigroup.com/omniweb/' \
+		| egrep -i '.dmg".*Release notes' \
+		| head -1 \
+		| tr '"' ' ' \
+		| awk '{print "https://omnistaging.omnigroup.com/omniweb/"$9" "$21}'))
+
+URL="$INFO[1]"
+
+RELEASE_NOTES_URL="$INFO[2]"
+
+LATEST_VERSION=$(echo "$URL:t:r" | tr -dc '[0-9]')
+
+	# If either of these are blank, we cannot continue
+if [ "$URL" = "" -o "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion | cut -d. -f3)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.dmg"
+
+if (( $+commands[lynx] ))
+then
+
+	(echo "$NAME: Release notes for OmniWeb:\n\n"
+	curl -sfLS "$RELEASE_NOTES_URL" \
+	| awk '/<h3/{i++}i==2' \
+	| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin) \
+	| tee -a "$FILENAME:r.txt"
+
+fi
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+echo "$NAME: Agreeing to the EULA and mounting $FILENAME: (sorry if this opens a Finder window): "
+
+MNTPNT=$(echo -n "Y" \
+		| hdid -plist "$FILENAME" 2>/dev/null \
+		| fgrep -A 1 '<key>mount-point</key>' \
+		| tail -1 \
+		| sed 's#</string>.*##g ; s#.*<string>##g')
+
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+else
+	echo "$NAME: MNTPNT is $MNTPNT"
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+		# Quit app, if running
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
+fi
+
+echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
+
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Successfully installed $INSTALL_TO"
+else
+	echo "$NAME: ditto failed"
+
+	exit 1
+fi
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
+
+exit 0
+#EOF
