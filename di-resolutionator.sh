c5c98488c62d3bbd95e2cb14af6b2b7ec3bd39bd
TJ Luoma <github@tjluoma.com>
2018-09-07 12:52:03 -0400

    adds info and release notes

--- di-resolutionator.sh
+++ di-resolutionator.sh
@@ -9,6 +9,16 @@ NAME="$0:t:r"
 
 INSTALL_TO='/Applications/Resolutionator.app'
 
+XML_FEED='https://manytricks.com/resolutionator/appcast.xml'
+
+RELEASE_NOTES_URL="https://manytricks.com/resolutionator/releasenotes/"
+
+HOMEPAGE="https://manytricks.com/resolutionator/"
+
+DOWNLOAD_PAGE="https://manytricks.com/download/resolutionator"
+
+SUMMARY="Resolutionator makes it simple to use any of your display's available resolutions. "
+
 if [ -e "$HOME/.path" ]
 then
 	source "$HOME/.path"
@@ -16,16 +26,14 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-XML_FEED='https://manytricks.com/resolutionator/appcast.xml'
-
 ## sparkle:version exists in feed but does not seem important
 
 INFO=($(curl -A Safari -sfL "$XML_FEED" \
-| tr -s ' ' '\012' \
-| egrep 'sparkle:shortVersionString=|url=' \
-| head -2 \
-| sort \
-| awk -F'"' '/^/{print $2}'))
+		| tr -s ' ' '\012' \
+		| egrep 'sparkle:shortVersionString=|url=' \
+		| head -2 \
+		| sort \
+		| awk -F'"' '/^/{print $2}'))
 
 	# "Sparkle" will always come before "url" because of "sort"
 LATEST_VERSION="$INFO[1]"
@@ -68,77 +76,76 @@ then
 
 fi
 
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.dmg"
+
 if (( $+commands[lynx] ))
 then
 
-	RELEASE_NOTES_URL="https://manytricks.com/resolutionator/releasenotes/?rref=appcast"
-
-	echo -n "$NAME: Release Notes for "
-
-	(curl -sSfL "${RELEASE_NOTES_URL}" \
-	| sed '1,/BODY STARTS HERE/d; /<\/ul>/,$d' ; echo '</ul>') \
-	| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin
-
-	echo "\nSource: <$RELEASE_NOTES_URL>"
+	( echo -n "$NAME: Release Notes for " ;
+		(curl -sSfL "${RELEASE_NOTES_URL}" \
+		| sed '1,/BODY STARTS HERE/d; /<\/ul>/,$d' ; echo '</ul>') \
+		| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin ;
+		echo "\nSource: <$RELEASE_NOTES_URL>" ) | tee -a "$FILENAME:r.txt"
 
 fi
 
-FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.dmg"
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
-echo "$NAME: Downloading $URL to $FILENAME"
-
-curl -A Safari --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
 
 EXIT="$?"
 
 	## exit 22 means 'the file was already fully downloaded'
-[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download failed (EXIT = $EXIT)" && exit 0
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+echo "$NAME: Mounting $FILENAME:"
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
-		| fgrep -A 1 '<key>mount-point</key>' \
-		| tail -1 \
-		| sed 's#</string>.*##g ; s#.*<string>##g')
+	| fgrep -A 1 '<key>mount-point</key>' \
+	| tail -1 \
+	| sed 's#</string>.*##g ; s#.*<string>##g')
 
 if [[ "$MNTPNT" == "" ]]
 then
 	echo "$NAME: MNTPNT is empty"
 	exit 1
+else
+	echo "$NAME: MNTPNT is $MNTPNT"
 fi
 
-if [ -e "$INSTALL_TO" ]
+if [[ -e "$INSTALL_TO" ]]
 then
 		# Quit app, if running
-	pgrep -xq "Resolutionator" \
+	pgrep -xq "$INSTALL_TO:t:r" \
 	&& LAUNCH='yes' \
-	&& osascript -e 'tell application "Resolutionator" to quit'
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
 
 		# move installed version to trash
-	mv -vf "$INSTALL_TO" "$HOME/.Trash/Resolutionator.$INSTALLED_VERSION.app"
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
 fi
 
-echo "$NAME: Installing \"$MNTPNT/Resolutionator.app\" to $INSTALL_TO"
+echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
 
-ditto --noqtn -v "$MNTPNT/Resolutionator.app" "$INSTALL_TO"
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
 
 EXIT="$?"
 
 if [[ "$EXIT" == "0" ]]
 then
-	echo "$NAME: Installation of $INSTALL_TO was successful."
-
-	[[ "$LAUNCH" = "yes" ]] && open -b com.manytricks.Resolutionator
-
+	echo "$NAME: Successfully installed $INSTALL_TO"
 else
-	echo "$NAME: Installation of $INSTALL_TO failed (\$EXIT = $EXIT)\nThe downloaded file can be found at $FILENAME."
+	echo "$NAME: ditto failed"
+
 	exit 1
 fi
 
-if (( $+commands[unmount.sh] ))
-then
-	unmount.sh "$MNTPNT"
-else
-	diskutil eject "$MNTPNT"
-fi
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
 
 exit 0
 #EOF
