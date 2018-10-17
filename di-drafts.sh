2c9952715cfed178c8746aeb6509d3ceaf6f05ae
TJ Luoma <github@tjluoma.com>
2018-10-17 19:11:40 -0400

    now does downloading & installing / upgrading

--- di-drafts.sh
+++ di-drafts.sh
@@ -22,24 +22,111 @@ INSTALLED_BUILD=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/d
 
 LATEST_BUILD=$(curl -sfLS 'https://getdrafts.com/mac/beta/' | fgrep -i '<li>Version' | awk '{print $2}' | tr -d ',')
 
-autoload msg
-
-GROWL_APP='Drafts'
-
 if [[ "$LATEST_BUILD" == "" ]]
 then
+	echo "$NAME: LATEST_BUILD is empty."
+	exit 1
+fi
 
-	msg --sticky --url https://getdrafts.com/mac/beta/ "LATEST_BUILD is empty. Click to check webpage."
-
-elif [[ "$INSTALLED_BUILD" == "$LATEST_BUILD" ]]
+if [[ "$INSTALLED_BUILD" == "$LATEST_BUILD" ]]
 then
 
-	msg "Drafts is up-to-date"
+	echo "$NAME: Drafts is up-to-date ($INSTALLED_BUILD)"
+
+	exit 0
 
 else
 
-	msg --sticky --url https://getdrafts.com/mac/beta/ "Drafts is outdated. Click to view/download."
+	echo "$NAME: Drafts is outdated ($INSTALLED_BUILD vs $LATEST_BUILD)."
+
 fi
+
+URL='https://s3-us-west-2.amazonaws.com/downloads.agiletortoise.com/Drafts.app.zip'
+
+FILENAME="$HOME/Downloads/${${INSTALL_TO:t:r}}-${LATEST_BUILD}.zip"
+
+if (( $+commands[lynx] ))
+then
+
+	( curl -sfLS 'https://getdrafts.com/mac/beta/changelog' \
+	| awk '/<h4 id="/{i++}i==1'\
+	| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -nonumbers -nolist -stdin ) \
+	| tee -a "$FILENAME:r.txt"
+
+fi
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --progress-bar --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
+
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+
+ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Unzip successful"
+else
+		# failed
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+	echo "$NAME: Moving existing (old) '$INSTALL_TO' to '$HOME/.Trash/'."
+
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
+
+		exit 1
+	fi
+fi
+
+echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
+
+	# Move the file out of the folder
+mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" = "0" ]]
+then
+
+	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+else
+	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+
+	exit 1
+fi
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
 
 exit 0
 #EOF
