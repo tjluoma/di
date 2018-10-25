3dd7c95d383a5a7b21010ce1d86efdd0bdb60755
TJ Luoma <github@tjluoma.com>
2018-10-25 15:54:11 -0400

    adds RELEASE_NOTES_URL support, better 'unzip'

--- di-filejuicer.sh
+++ di-filejuicer.sh
@@ -56,7 +56,19 @@ fi
 
 FILENAME="$HOME/Downloads/FileJuicer-$LATEST_VERSION.zip"
 
-echo "$NAME: Downloading \"$URL\" to \"$FILENAME\":"
+if (( $+commands[lynx] ))
+then
+
+	RELEASE_NOTES_URL='https://echoone.com/filejuicer/ReadMe'
+
+	( lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -nonumbers -nolist "$RELEASE_NOTES_URL" \
+		| sed "1,/^What\'s New/d" \
+		| awk '/^Version /{i++}i==1' ) \
+	| tee -a "$FILENAME:r.txt"
+
+fi
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
 curl --continue-at - --fail --location --output "$FILENAME" "$URL"
 
@@ -71,9 +83,9 @@ EXIT="$?"
 
 UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
 
-echo "$NAME: Unzipping $FILENAME to $UNZIP_TO:"
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
 
-ditto --noqtn -xk "$FILENAME" "$UNZIP_TO"
+ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
 
 EXIT="$?"
 
@@ -82,14 +94,19 @@ then
 	echo "$NAME: Unzip successful"
 else
 		# failed
-	echo "$NAME failed (ditto --noqtn -xkv \"$FILENAME\" \"$UNZIP_TO\")"
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
 
 	exit 1
 fi
 
 if [[ -e "$INSTALL_TO" ]]
 then
-	echo "$NAME: Moving existing (old) \"$INSTALL_TO\" to \"$HOME/.Trash/\"."
+
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+	echo "$NAME: Moving existing (old) '$INSTALL_TO' to '$HOME/.Trash/'."
 
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
 
@@ -104,7 +121,7 @@ then
 	fi
 fi
 
-echo "$NAME: Moving new version of \"$INSTALL_TO:t\" (from \"$UNZIP_TO\") to \"$INSTALL_TO\"."
+echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
 
 	# Move the file out of the folder
 mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
@@ -114,13 +131,15 @@ EXIT="$?"
 if [[ "$EXIT" = "0" ]]
 then
 
-	echo "$NAME: Successfully installed \"$UNZIP_TO/$INSTALL_TO:t\" to \"$INSTALL_TO\"."
+	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
 
 else
-	echo "$NAME: Failed to move \"$UNZIP_TO/$INSTALL_TO:t\" to \"$INSTALL_TO\"."
+	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
 
 	exit 1
 fi
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
 
 exit 0
 #EOF
