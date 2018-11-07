55ed2cbad8aafa5776c6e6728ac5759f25c3f68a
TJ Luoma <github@tjluoma.com>
2018-11-06 23:15:29 -0500

    removed Mailplane 3 support

--- di-mailplane.sh
+++ di-mailplane.sh
@@ -6,6 +6,8 @@
 # Web: 	http://RhymesWithDiploma.com
 # Date:	2018-08-20
 
+## 2018-11-05 - the format of the XML_FEED seems to have changed
+
 NAME="$0:t:r"
 
 HOMEPAGE="https://mailplaneapp.com"
@@ -14,110 +16,67 @@ DOWNLOAD_PAGE="https://update.mailplaneapp.com/mailplane_4.php"
 
 SUMMARY="The best way to use Gmail on your Mac."
 
-INSTALL_V3_TO='/Applications/Mailplane 3.app'
-INSTALL_V4_TO='/Applications/Mailplane.app'
-
 # no RELEASE_NOTES_URL found
 
-function use_v3 {
+INSTALL_TO="/Applications/Mailplane.app"
 
-	INSTALL_TO="/Applications/Mailplane 3.app"
+OS_VER=$(sw_vers -productVersion)
 
-	URL=$(curl -sfL --head "http://update.mailplaneapp.com/mailplane_3.php" \
-		| awk -F'\r|: ' '/^Location/{print $2}' \
-		| tail -1)
+INSTALLED_VERSION=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString 2>/dev/null || echo '4.0.0')
 
-	LATEST_VERSION=`echo "$URL:t:r" | sed 's#Mailplane_3_##g'`
+INSTALLED_BUILD=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '4000')
 
-	FILENAME="$HOME/Downloads/MailPlane-3-${LATEST_VERSION}.tbz"
+XML_FEED="https://update.mailplaneapp.com/appcast.php?osVersion=${OS_VER}&appVersion=${INSTALLED_BUILD}&shortVersionString=${INSTALLED_VERSION}&selectedLanguage=en-US"
 
-	ASTERISK='(Note that version 4 is also available.)'
-}
+INFO=($(curl -sfL "$XML_FEED" \
+		| tr -s ' ' '\012' \
+		| egrep 'sparkle:shortVersionString=|sparkle:version=|url=' \
+		| head -3 \
+		| sort \
+		| awk -F'"' '/^/{print $2}' ))
 
-function use_v4 {
+	# "Sparkle" will always come before "url" because of "sort"
+LATEST_VERSION="$INFO[1]"
+LATEST_BUILD="$INFO[2]"
+URL="$INFO[3]"
 
-	INSTALL_TO="/Applications/Mailplane.app"
-
-	OS_VER=`sw_vers -productVersion`
-
-	INSTALLED_BUILD=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '4000'`
-
-	XML_FEED="https://update.mailplaneapp.com/appcast.php?appName=Mailplane%203&osVersion=${OS_VER}&appVersion=${INSTALLED_BUILD}&selectedLanguage=en"
-
-	INFO=($(curl -sfL "$XML_FEED" \
-			| tr -s ' ' '\012' \
-			| egrep 'sparkle:shortVersionString=|sparkle:version=|url=' \
-			| head -3 \
-			| sort \
-			| awk -F'"' '/^/{print $2}' ))
-
-		# "Sparkle" will always come before "url" because of "sort"
-		# We aren't using INFO[1] because we really just need the build
-	LATEST_VERSION="$INFO[2]"
-	URL="$INFO[3]"
-
-		# n.b. 'http://update.mailplaneapp.com/mailplane_4.php' does redirect to the latest v4 version
-		# (after a few redirects along the way). Here's the current version
-		# Location: https://update.mailplaneapp.com/builds/Mailplane_4_4516.tbz
-
-	FILENAME="$HOME/Downloads/MailPlane-4-${LATEST_VERSION}.tbz"
-}
-
-	# if the user explicitly askes for version 3, use it, regardless of the above
-if [ "$1" = "--use3" -o "$1" = "-3" ]
-then
-	use_v3
-elif [ "$1" = "--use4" -o "$1" = "-4" ]
-then
-	use_v4
-else
-	if [ -e "$INSTALL_V3_TO" -a -e "$INSTALL_V4_TO" ]
-	then
-		echo "$NAME: Both versions 3 and 4 of Mailplane are installed. I will _only_ check for updates for version 4 in this situation."
-		echo "	If you want to check for updates for version 3, add the argument '--use3' i.e. '$0:t --use3' "
-		echo "	To avoid this message in the future, add the argument '--use4' i.e. '$0:t --use4' "
-
-		use_v4
-
-	elif [ ! -e "$INSTALL_V3_TO" -a -e "$INSTALL_V4_TO" ]
-	then
-			# version 3 is not installed but version 4 is
-		use_v4
-	elif [ -e "$INSTALL_V3_TO" -a ! -e "$INSTALL_V4_TO" ]
-	then
-			# version 3 is installed but version 4 is not
-		use_v3
-	else
-			# neither v3 or v4 are installed
-		use_v4
-	fi
-fi
+	# n.b. 'http://update.mailplaneapp.com/mailplane_4.php' does redirect to the latest v4 version
+	# (after a few redirects along the way). Here's the current version
+	# Location: https://update.mailplaneapp.com/builds/Mailplane_4_4516.tbz
 
 if [[ -e "$INSTALL_TO" ]]
 then
 
-	INSTALLED_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion 2>/dev/null || echo '0'`
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
 
-	if [[ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]]
-	then
-		echo "$NAME: Up-To-Date ($INSTALLED_VERSION) $ASTERISK"
-		exit 0
-	fi
+	INSTALLED_BUILD=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
 
 	autoload is-at-least
 
 	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
-	if [ "$?" = "0" ]
+	VERSION_COMPARE="$?"
+
+	is-at-least "$LATEST_BUILD" "$INSTALLED_BUILD"
+
+	BUILD_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" -a "$BUILD_COMPARE" = "0" ]
 	then
-		echo "$NAME: Installed version ($INSTALLED_VERSION) is ahead of official version ($LATEST_VERSION)"
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION/$INSTALLED_BUILD)"
 		exit 0
 	fi
 
-	echo "$NAME: Outdated (Installed = $INSTALLED_VERSION vs Latest = $LATEST_VERSION)"
+	echo "$NAME: Outdated: $INSTALLED_VERSION/$INSTALLED_BUILD vs $LATEST_VERSION/$LATEST_BUILD"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
 fi
 
-# FILENAME is defined above
+FILENAME="$HOME/Downloads/MailPlane-4-${LATEST_VERSION}_${LATEST_BUILD}.tbz"
 
 echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
@@ -174,4 +133,3 @@ fi
 exit 0
 #
 #EOF
-
