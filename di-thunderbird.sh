c9a5fb0bd6cdab6e909b4cf078ab2b1abfbb311a
TJ Luoma <github@tjluoma.com>
2018-11-16 23:08:21 -0500

    Initial Commit

---
+++ di-thunderbird.sh
@@ -0,0 +1,144 @@
+#!/bin/zsh -f
+# Purpose: Download and install the latest version of Thunderbird
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2018-11-16
+
+NAME="$0:t:r"
+
+if [[ -e "$HOME/.path" ]]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+INSTALL_TO='/Applications/Thunderbird.app'
+
+# https://www.thunderbird.net/en-US/thunderbird/releases/
+
+	# https://ftp.mozilla.org/pub/thunderbird/releases/60.3.1/mac/en-US/Thunderbird%2060.3.1.dmg
+	# https://www.thunderbird.net/en-US/thunderbird/60.3.1/releasenotes/
+
+	# LATEST_BUILD is identical to LATEST_VERSION
+LATEST_VERSION=$(curl -sfLS 'https://www.thunderbird.net/en-US/thunderbird/releases/' \
+				| egrep -i 'en-US/thunderbird/.*/releasenotes/' \
+				| sed 's#.*/releasenotes/">##g' \
+				| tr -dc '[0-9]\.\n' \
+				| sort -n \
+				| tail -1)
+
+	# If any of these are blank, we cannot continue
+if [ "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	LATEST_VERSION: $LATEST_VERSION
+	"
+
+	exit 1
+fi
+
+URL="https://ftp.mozilla.org/pub/thunderbird/releases/${LATEST_VERSION}/mac/en-US/Thunderbird%20${LATEST_VERSION}.dmg"
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+FILENAME="$HOME/Downloads/${${INSTALL_TO:t:r}// /}-${LATEST_VERSION}.dmg"
+
+RELEASE_NOTES_URL="https://www.thunderbird.net/en-US/thunderbird/$LATEST_VERSION/releasenotes/"
+
+if (( $+commands[lynx] ))
+then
+
+	( echo "Release Notes for Thunderbird version $LATEST_VERSION:\n\n" ; \
+		curl -sfLS "$RELEASE_NOTES_URL" \
+		| sed '1,/<div class="main-column">/d; /<aside id="sidebar" class="sidebar">/,$d' \
+		| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -nonumbers -nolist -stdin) \
+	| tee -a "$FILENAME:r.txt"
+
+fi
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+echo "$NAME: Mounting $FILENAME:"
+
+MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
+	| fgrep -A 1 '<key>mount-point</key>' \
+	| tail -1 \
+	| sed 's#</string>.*##g ; s#.*<string>##g')
+
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+else
+	echo "$NAME: MNTPNT is $MNTPNT"
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+		# Quit app, if running
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e 'tell application "$INSTALL_TO:t:r" to quit'
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
+fi
+
+echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
+
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Successfully installed $INSTALL_TO"
+else
+	echo "$NAME: ditto failed"
+
+	exit 1
+fi
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
+
+exit 0
+#EOF
