f5d93ddebcd9e38ede2e02e66502db1d685b6ca7
TJ Luoma <github@tjluoma.com>
2019-06-07 19:58:21 -0400

    fix 'tee'

--- di-vlc.sh
+++ di-vlc.sh
@@ -89,7 +89,7 @@ then
 		 | sed  '1,/<td valign="top">/d;' \
 		 ; echo '</ul>') \
 		| lynx -dump -nomargins -width=10000 -assume_charset=UTF-8 -pseudo_inlines -stdin ;
-	echo "\nSource: <${RELEASE_NOTES_URL}>" ) | tee -a "$FILENAME:r.txt"
+	echo "\nSource: <${RELEASE_NOTES_URL}>" ) | tee "$FILENAME:r.txt"
 
 fi
 
