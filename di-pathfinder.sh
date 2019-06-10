41170070a55d8d21be25a9da86637f375fecacd8
TJ Luoma <github@tjluoma.com>
2019-06-10 09:55:49 -0400

    drops support for v7, download changed from zip to DMG

--- di-pathfinder.sh
+++ di-pathfinder.sh
@@ -1,19 +1,9 @@
 #!/bin/zsh -f
-# Purpose: Download and install the latest version of PathFinder 7 or 8 from <https://cocoatech.com/>
+# Purpose: Download and install the latest version of PathFinder 8 from <https://cocoatech.com/>
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
 # Date:	2018-08-21
-
-NAME="$0:t:r"
-
-INSTALL_TO='/Applications/Path Finder.app'
-
-HOMEPAGE="https://cocoatech.com/"
-
-DOWNLOAD_PAGE="https://get.cocoatech.com/PF8.zip"
-
-SUMMARY="File manager for macOS."
 
 if [ -e "$HOME/.path" ]
 then
@@ -22,55 +12,28 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-function use_v7 {
+NAME="$0:t:r"
 
-	URL="https://get.cocoatech.com/PF7.zip"
+INSTALL_TO='/Applications/Path Finder.app'
 
-		## I determined LATEST_VERSION & LATEST_BUILD by downloading 'https://get.cocoatech.com/PF7.zip'
-		# on 2018-07-17. I do not think PathFinder 7 will be updated anymore,
-		# so this is likely to be the last version.
-	LATEST_VERSION="7.6.2"
-	LATEST_BUILD="1729"
-	ASTERISK='(Note that version 8 is also available.)'
-	USE_VERSION='7'
+HOMEPAGE="https://cocoatech.com/"
 
-}
+DOWNLOAD_PAGE="https://get.cocoatech.com/PF8.dmg"
 
-function use_v8 {
+SUMMARY="File manager for macOS."
 
-	USE_VERSION='8'
-	XML_FEED="http://sparkle.cocoatech.com/PF8.xml"
+XML_FEED="http://sparkle.cocoatech.com/PF8.xml"
 
-	INFO=($(curl -sfL $XML_FEED \
-			| egrep '<(build|version|url)>' \
-			| sort \
-			| awk -F'>|<' '//{print $3}'))
+INFO=($(curl -sfL "$XML_FEED" \
+		| egrep '<(build|version|url)>' \
+		| sort \
+		| awk -F'>|<' '//{print $3}'))
 
-	LATEST_BUILD="$INFO[1]"
-	URL="$INFO[2]"
-	LATEST_VERSION="$INFO[3]"
+LATEST_BUILD="$INFO[1]"
 
-}
+URL="$INFO[2]"
 
-if [[ -e "$INSTALL_TO" ]]
-then
-		# if v7 is installed, check that. Otherwise, use v8
-	MAJOR_VERSION=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d. -f1)
-
-	if [[ "$MAJOR_VERSION" == "7" ]]
-	then
-		use_v7
-	else
-		use_v8
-	fi
-else
-	if [ "$1" = "--use7" -o "$1" = "-7" ]
-	then
-		use_v7
-	else
-		use_v8
-	fi
-fi
+LATEST_VERSION="$INFO[3]"
 
 if [[ -e "$INSTALL_TO" ]]
 then
@@ -104,22 +67,19 @@ else
 	FIRST_INSTALL='yes'
 fi
 
-FILENAME="$HOME/Downloads/PathFinder-${LATEST_VERSION}_${LATEST_BUILD}.zip"
+FILENAME="$HOME/Downloads/PathFinder-${LATEST_VERSION}_${LATEST_BUILD}.dmg"
 
-if [[ "$USE_VERSION" == "8" ]]
+if (( $+commands[lynx] ))
 then
-	if (( $+commands[lynx] ))
-	then
 
-		RELEASE_NOTES_URL="$XML_FEED"
+	RELEASE_NOTES_URL="$XML_FEED"
 
-		( curl -sfLS "$RELEASE_NOTES_URL" \
-			| sed '1,/<\!\[CDATA\[/d; /\]\]>/,$d' \
-			| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin \
-			| sed '/./,/^$/!d' \
-			| sed 's#^ *##g' ;
-			echo "\nSource: XML_FEED <$RELEASE_NOTES_URL>" ) | tee -a "$FILENAME:r.txt"
-	fi
+	( curl -sfLS "$RELEASE_NOTES_URL" \
+		| sed '1,/<\!\[CDATA\[/d; /\]\]>/,$d' \
+		| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin \
+		| sed '/./,/^$/!d' \
+		| sed 's#^ *##g' ;
+		echo "\nSource: XML_FEED <$RELEASE_NOTES_URL>" ) | tee "$FILENAME:r.txt"
 fi
 
 echo "$NAME: Downloading '$URL' to '$FILENAME':"
@@ -135,75 +95,47 @@ EXIT="$?"
 
 [[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
-UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
+echo "$NAME: Accepting EULA and mounting '$FILENAME' (sorry if this opens a Finder window, it's not my fault):"
 
-echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+MNTPNT=$(echo -n "Y" | hdid -plist "$FILENAME" 2>/dev/null | fgrep '/Volumes/' | sed 's#</string>##g ; s#.*<string>##g')
 
-ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+else
+	echo "$NAME: MNTPNT is $MNTPNT"
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+		# Quit app, if running
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e "tell application \"$INSTALL_TO:t:r\" to quit"
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
+fi
+
+echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
+
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
 
 EXIT="$?"
 
 if [[ "$EXIT" == "0" ]]
 then
-	echo "$NAME: Unzip successful"
+	echo "$NAME: Successfully installed $INSTALL_TO"
 else
-		# failed
-	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
-
-	exit 1
-fi
-
-if [[ -e "$INSTALL_TO" ]]
-then
-
-	pgrep -xq "$INSTALL_TO:t:r" \
-	&& LAUNCH='yes' \
-	&& osascript -e "tell application \"$INSTALL_TO:t:r\" to quit"
-
-	echo "$NAME: Moving existing (old) '$INSTALL_TO' to '$HOME/.Trash/'."
-
-	local TRASH="$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
-
-	COUNT='0'
-
-	while [ -e "$TRASH" ]
-	do
-		((COUNT++))
-		TRASH="$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.($COUNT).app"
-	done
-
-	mv -vf "$INSTALL_TO" "$TRASH"
-
-	EXIT="$?"
-
-	if [[ "$EXIT" != "0" ]]
-	then
-
-		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
-
-		exit 1
-	fi
-fi
-
-echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
-
-	# Move the file out of the folder
-mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
-
-EXIT="$?"
-
-if [[ "$EXIT" = "0" ]]
-then
-
-	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
-
-else
-	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+	echo "$NAME: ditto failed"
 
 	exit 1
 fi
 
 [[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
 
 exit 0
 
