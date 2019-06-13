feaccda30547ded47d043e3e683d1e2a602e12c0
TJ Luoma <github@tjluoma.com>
2019-06-13 12:31:19 -0400

    Updated for version 7, removed Release Notes

--- di-expandrive.sh
+++ di-expandrive.sh
@@ -10,21 +10,15 @@ NAME="$0:t:r"
 
 INSTALL_TO='/Applications/ExpanDrive.app'
 
-## @TODO - Version 7 now uses 'https://updates.expandrive.com/appcast/expandrive7.json?version=7.0.15'
+	# Do Not Use: http://updates.expandrive.com/apps/expandrive.xml
 
-XML_FEED="http://updates.expandrive.com/appcast/expandrive.xml?version=5"
+XML_FEED='https://updates.expandrive.com/appcast/expandrive7.json?version=7.0.0'
 
 HOMEPAGE="https://www.expandrive.com"
 
 DOWNLOAD_PAGE="https://www.expandrive.com/download-expandrive/"
 
 SUMMARY="Access files in the cloud from Finder or Explorer without having to sync or use disk space. ExpanDrive mounts OneDrive, Google Drive, Dropbox, Box, Sharepoint, Amazon S3, FTP, SFTP and more as a Network Drive."
-
-RELEASE_NOTES_URL=$(curl -sfL "$XML_FEED" \
-	| sed '1,/<sparkle:releaseNotesLink>/d; /<\/sparkle:releaseNotesLink>/,$d' \
-	| awk -F' ' '/http/{print $1}')
-
-# Do Not Use: http://updates.expandrive.com/apps/expandrive.xml
 
 if [ -e "$HOME/.path" ]
 then
@@ -33,36 +27,14 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-# sparkle:version= is the only version string in XML_FEED
-# CFBundleShortVersionString and CFBundleVersion are identical in app plist
+URL=$(curl -sfLS "$XML_FEED" | awk -F'"' '/url/{print $4}')
 
-INFO=($(curl -sfL "${XML_FEED}" \
-		| tr -s ' ' '\012' \
-		| egrep 'sparkle:version=|url=' \
-		| head -2 \
-		| sort \
-		| awk -F'"' '/^/{print $2}'))
-
-	# "Sparkle" will always come before "url" because of "sort"
-LATEST_VERSION="$INFO[1]"
-URL="$INFO[2]"
+LATEST_VERSION=$(echo "$URL" | awk -F'/' '/http/{print $7}' | tr '-' '.')
 
 	# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+if [ "$LATEST_VERSION" = "" -o "$URL" = "" ]
 then
-	echo "$NAME: Error: bad data received:\nINFO: $INFO\nLATEST_VERSION: $LATEST_VERSION\nURL: $URL"
-	exit 1
-fi
-
-	# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
-then
-	echo "$NAME: Error: bad data received:
-	INFO: $INFO
-	LATEST_VERSION: $LATEST_VERSION
-	URL: $URL
-	"
-
+	echo "$NAME: Error: bad data received:\nLATEST_VERSION: $LATEST_VERSION\nURL: $URL"
 	exit 1
 fi
 
@@ -93,19 +65,6 @@ else
 fi
 
 FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.zip"
-
-if (( $+commands[lynx] ))
-then
-
-	( echo "$NAME: Release Notes for $INSTALL_TO:t:r version $LATEST_VERSION:" ;
-	curl -sfL "$RELEASE_NOTES_URL" \
-	| sed '1,/<body>/d; /<\body>/,$d ; s#</span><span>#: #g' \
-	| lynx -dump -nomargins -width=10000 -assume_charset=UTF-8 -pseudo_inlines -stdin \
-	| fgrep -v 'ExpanDrive 6 is a paid upgrade and costs $24.95 if you purchased before April 1, 2017.' \
-	| fgrep -v '  * Improvements in ExpanDrive ' ;
-	echo "\nSource: <$RELEASE_NOTES_URL>" ) | tee -a "$FILENAME:r.txt"
-
-fi
 
 echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
