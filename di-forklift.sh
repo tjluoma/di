2d19d106e3f5e593a09c6e560cf23afddafe00a3
TJ Luoma <github@tjluoma.com>
2019-06-20 06:35:45 -0400

    fix 'tee'

--- di-forklift.sh
+++ di-forklift.sh
@@ -92,7 +92,7 @@ if (( $+commands[lynx] ))
 then
 
 	lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines "${RELEASE_NOTES_URL}" \
-	| tee -a "$FILENAME:r.txt"
+	| tee "$FILENAME:r.txt"
 
 fi
 
