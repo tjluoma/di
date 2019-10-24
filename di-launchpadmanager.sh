74916736140ebbf7d541c8f37b277808debcb4e1
TJ Luoma <github@tjluoma.com>
2019-10-24 15:44:00 -0400

    first full working version

--- di-launchpadmanager.sh
+++ di-launchpadmanager.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh -f
-# Purpose:
+# Purpose: Download and install/update LaunchPad Manager
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
@@ -14,8 +14,144 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-# http://launchpadmanager.com/appyos/sparkle.rss
+	## Note: the app wants to call itself 'Launchpad Manager Yosemite' but that's gross, so… no.
+INSTALL_TO='/Applications/Launchpad Manager.app'
 
+XML_FEED='http://launchpadmanager.com/appyos/sparkle.rss'
+
+INFO=$(curl -sfLS "$XML_FEED" | awk '/<item>/{i++}i==1')
+
+URL=$(echo "$INFO" | tr '"' '\012' | egrep '^http.*\.dmg')
+
+LATEST_VERSION=$(echo "$INFO" | tr -s ' |\t' '\012' | awk -F'"' '/^sparkle:version/{print $2}')
+
+	# If any of these are blank, we cannot continue
+if [ "$INFO" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+FILENAME="$HOME/Downloads/${${INSTALL_TO:t:r}// /}-${LATEST_VERSION}.dmg"
+
+if (( $+commands[lynx] ))
+then
+
+	RELEASE_NOTES=$(echo "$INFO" \
+					| tr -s ' |\t|\012' ' '  \
+					| sed 's#.*\<\!\[CDATA\[##g ; s# \]\]\>.*##g' \
+					| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin)
+
+	echo "$RELEASE_NOTES\nURL: $URL" | tee "$FILENAME:r.txt"
+
+fi
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+(cd "$FILENAME:h" ; echo "\nLocal sha256:" ; shasum -a 256 -p "$FILENAME:t" ) >>| "$FILENAME:r.txt"
+
+echo "$NAME: Mounting $FILENAME:"
+
+MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
+	| fgrep -A 1 '<key>mount-point</key>' \
+	| tail -1 \
+	| sed 's#</string>.*##g ; s#.*<string>##g')
+
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
+	exit 1
+else
+	echo "$NAME: MNTPNT is $MNTPNT"
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+		# Quit app, if running
+	pgrep -xq "$INSTALL_TO:t:r" \
+	&& LAUNCH='yes' \
+	&& osascript -e "tell application \"$INSTALL_TO:t:r\" to quit"
+
+		# move installed version to trash
+	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move '$INSTALL_TO' to Trash. ('mv' \$EXIT = $EXIT)"
+
+		exit 1
+	fi
+
+fi
+
+## The app name is '/Volumes/Launchpad Manager Yosemite/Launchpad Manager Yosemite.app'
+## which is not the name that I want to use, or anyone wants to use.
+## Presumably if the app is ever updated, they might rename it something better
+## but regardless, we'll just use whatever '.app' is found in the $MNTPNT
+
+MNTAPPNAME=$(find "$MNTPNT" -type d -iname '*.app' -maxdepth 1 | head -1)
+
+ditto --noqtn -v "$MNTAPPNAME" "$INSTALL_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Successfully installed $INSTALL_TO"
+else
+	echo "$NAME: ditto failed"
+
+	exit 1
+fi
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
 
 
 exit 0
