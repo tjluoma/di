54c2b8523846fb9ca7fe4b8be841ccdc09d69265
TJ Luoma <github@tjluoma.com>
2019-11-01 21:02:12 -0400

    Version 2.0 (app in a pkg in a zip)

---
+++ di-choosy.sh
@@ -0,0 +1,169 @@
+#!/usr/bin/env zsh -f
+# Purpose: Download Choosy v2
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2019-11-01
+
+NAME="$0:t:r"
+
+if [[ -e "$HOME/.path" ]]
+then
+	source "$HOME/.path"
+else
+	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
+fi
+
+INSTALL_TO='/Applications/Choosy.app'
+
+XML_FEED='https://www.choosyosx.com/sparkle/feed'
+
+HOMEPAGE="https://www.choosyosx.com"
+
+DOWNLOAD_PAGE="https://www.choosyosx.com"
+
+SUMMARY="Instead of opening links in the default browser, Choosy sends them to the right browser. Every time."
+
+	# sparkle:version= is the only version information available
+INFO=($(curl -sfL "$XML_FEED" \
+		| tr -s ' ' '\012' \
+		| egrep "sparkle:version=|url=" \
+		| head -2 \
+		| awk -F'"' '/^/{print $2}'))
+
+RELEASE_NOTES_URL=$(curl -sfL "$XML_FEED" \
+	| sed '1,/<description><\!\[CDATA\[/d; /<\/description>/,$d' \
+	| awk -F'"' '/http/{print $2}')
+
+URL="$INFO[2]"
+
+LATEST_VERSION="$INFO[1]"
+
+	# If any of these are blank, we should not continue
+if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+then
+	echo "$NAME: Error: bad data received:
+	INFO: $INFO
+	LATEST_VERSION: $LATEST_VERSION
+	URL: $URL
+	"
+
+	exit 1
+fi
+
+if [[ -e "$INSTALL_TO" ]]
+then
+
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+	autoload is-at-least
+
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
+
+	VERSION_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" ]
+	then
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		exit 0
+	fi
+
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+
+	FIRST_INSTALL='no'
+
+else
+
+	FIRST_INSTALL='yes'
+fi
+
+	# Where to save new download
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-$LATEST_VERSION.zip"
+
+if (( $+commands[lynx] ))
+then
+
+	( echo "$NAME: Release Notes for $INSTALL_TO:t:r version $LATEST_VERSION:\n" ;
+		curl -sfL "$RELEASE_NOTES_URL" \
+		| sed '1,/<h3>Release notes<\/h3>/d; /<h3>Download<\/h3>/,$d' \
+		| lynx -dump -nomargins -width=10000 -assume_charset=UTF-8 -pseudo_inlines -stdin ;
+		echo "\nSource: <$RELEASE_NOTES_URL>" ) \
+	| tee "$FILENAME:r.txt"
+
+fi
+
+echo "$NAME: Downloading '$URL' to '$FILENAME':"
+
+curl --continue-at - --fail --location --output "$FILENAME" "$URL"
+
+EXIT="$?"
+
+	## exit 22 means 'the file was already fully downloaded'
+[ "$EXIT" != "0" -a "$EXIT" != "22" ] && echo "$NAME: Download of $URL failed (EXIT = $EXIT)" && exit 0
+
+[[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
+
+[[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+(cd "$FILENAME:h" ; echo "\nLocal sha256:" ; shasum -a 256 -p "$FILENAME:t" ) >>| "$FILENAME:r.txt"
+
+# <sparkle:minimumSystemVersion>10.14</sparkle:minimumSystemVersion>
+
+
+## make sure that the .zip is valid before we proceed
+(command unzip -l "$FILENAME" 2>&1 )>/dev/null
+
+EXIT="$?"
+
+if [ "$EXIT" = "0" ]
+then
+	echo "$NAME: '$FILENAME' is a valid zip file."
+
+else
+	echo "$NAME: '$FILENAME' is an invalid zip file (\$EXIT = $EXIT)"
+
+	mv -fv "$FILENAME" "$HOME/.Trash/"
+
+	mv -fv "$FILENAME:r".* "$HOME/.Trash/"
+
+	exit 0
+
+fi
+
+## unzip to a temporary directory
+UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
+
+echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
+
+ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
+
+EXIT="$?"
+
+if [[ "$EXIT" == "0" ]]
+then
+	echo "$NAME: Unzip successful"
+else
+		# failed
+	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+
+	exit 1
+fi
+
+PKG=$(find "$UNZIP_TO" -iname '*.pkg' -print )
+
+if (( $+commands[pkginstall.sh] ))
+then
+		# This is a script I wrote that basically just does a lot of extra logging for me.
+ 	pkginstall.sh "$PKG"
+
+else
+
+	sudo /usr/sbin/installer -verbose -pkg "$PKG" -dumplog -target / -lang en 2>&1 | tee -a "$FILENAME:r.install.log" \
+	|| open -a Installer "$FILENAME"
+
+fi
+
+[[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+exit 0
+#EOF
