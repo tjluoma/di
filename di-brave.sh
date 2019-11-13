7a618272c5a66a5a9b7dbec851753f788cd31b8e
TJ Luoma <github@tjluoma.com>
2019-11-13 17:47:56 -0500

    now supports LATEST_BUILD and Release Notes

--- di-brave.sh
+++ di-brave.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh -f
+#!/usr/bin/env zsh -f
 # Purpose: Download and install the latest version of Brave from: <https://brave.com>
 #
 # From:	Timothy J. Luoma
@@ -6,17 +6,6 @@
 # Date:	2018-08-04
 
 INSTALL_TO='/Applications/Brave Browser.app'
-
-## @TODO - new app release
-##   appcast 'https://updates.bravesoftware.com/sparkle/Brave-Browser/stable/appcast.xml'
-##   url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser.dmg"
-##
-##  get-appversion.sh -v /Volumes/Brave\ Browser/Brave\ Browser.app
-##/Volumes/Brave Browser/Brave Browser.app:
-##	CFBundleShortVersionString: 75.0.65.118
-##	CFBundleVersion: 65.118
-##
-## apparently 'CFBundleVersion' is the 'version number' ?
 
 NAME="$0:t:r"
 
@@ -30,36 +19,45 @@ fi
 HOMEPAGE="https://brave.com"
 
 DOWNLOAD_PAGE="https://brave.com/download/"
-# https://laptop-updates.brave.com/latest/osx
-# aka https://brave-browser-downloads.s3.brave.com/latest/Brave-Browser.dmg
+
+RELEASE_NOTES_URL='https://github.com/brave/brave-browser/blob/master/CHANGELOG.md'
 
 SUMMARY="Much more than a browser, Brave is a new way of thinking about how the web works."
 
 XML_FEED='https://updates.bravesoftware.com/sparkle/Brave-Browser/stable/appcast.xml'
 
-INFO=($(curl -sfLS "$XML_FEED" | tr ' ' '\012' | egrep '^(url|sparkle:version)=' | tail -2 | sort | awk -F'"' '//{print $2}'))
+INFO=$(curl -sfLS "$XML_FEED" \
+| tr -s '\012' ' ' \
+| sed 's# <item> #\
+<item>#g' \
+| egrep '^<item>' \
+| tail -1)
 
-LATEST_VERSION="$INFO[1]"
+URL=$(echo "$INFO" | sed 's#.*enclosure url="##g ; s#".*##g')
 
-URL="$INFO[2]"
+LATEST_BUILD=$(echo "$INFO" | sed 's#.* sparkle:version="##g ; s#".*##g')
+
+LATEST_VERSION=$(echo "$INFO" | sed 's#.* sparkle:shortVersionString="##g ; s#".*##g')
 
 	# If any of these are blank, we cannot continue
-if [ "$INFO" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
+if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
 then
 	echo "$NAME: Error: bad data received:
 	INFO: $INFO
 	LATEST_VERSION: $LATEST_VERSION
+	LATEST_BUILD: $LATEST_BUILD
 	URL: $URL
 	"
 
 	exit 1
 fi
 
-
 if [[ -e "$INSTALL_TO" ]]
 then
 
-	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
+	INSTALLED_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
+
+	INSTALLED_BUILD=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
 
 	autoload is-at-least
 
@@ -67,13 +65,17 @@ then
 
 	VERSION_COMPARE="$?"
 
-	if [ "$VERSION_COMPARE" = "0" ]
+	is-at-least "$LATEST_BUILD" "$INSTALLED_BUILD"
+
+	BUILD_COMPARE="$?"
+
+	if [ "$VERSION_COMPARE" = "0" -a "$BUILD_COMPARE" = "0" ]
 	then
-		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION/$INSTALLED_BUILD)"
 		exit 0
 	fi
 
-	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
+	echo "$NAME: Outdated: $INSTALLED_VERSION/$INSTALLED_BUILD vs $LATEST_VERSION/$LATEST_BUILD"
 
 	FIRST_INSTALL='no'
 
@@ -82,7 +84,21 @@ else
 	FIRST_INSTALL='yes'
 fi
 
-FILENAME="$HOME/Downloads/${${INSTALL_TO:t:r}// /}-${LATEST_VERSION}.dmg"
+FILENAME="$HOME/Downloads/${${INSTALL_TO:t:r}// /}-${LATEST_VERSION}_${LATEST_BUILD}.dmg"
+
+if (( $+commands[lynx] ))
+then
+
+# DO NOT INDENT THAT ONE LINE
+	RELEASE_NOTES=$(curl -sfLS "$RELEASE_NOTES_URL" \
+	| sed -e 's#<#\
+<#g' -e '1,/<article/d' -e '/<\/ul>/,$d' \
+	| tr -d '\012' \
+	| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin -hiddenlinks=ignore)
+
+	echo "$RELEASE_NOTES\n\nSource: ${RELEASE_NOTES_URL}\nURL: ${URL}" | tee "$FILENAME:r.txt"
+
+fi
 
 echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
@@ -96,6 +112,8 @@ EXIT="$?"
 [[ ! -e "$FILENAME" ]] && echo "$NAME: $FILENAME does not exist." && exit 0
 
 [[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
+
+(cd "$FILENAME:h" ; echo "\nLocal sha256:" ; shasum -a 256 -p "$FILENAME:t" ) >>| "$FILENAME:r.txt"
 
 echo "$NAME: Mounting $FILENAME:"
 
@@ -121,6 +139,17 @@ then
 
 		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move '$INSTALL_TO' to Trash. ('mv' \$EXIT = $EXIT)"
+
+		exit 1
+	fi
+
 fi
 
 echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
@@ -144,3 +173,28 @@ echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
 
 exit 0
 #EOF
+
+
+# @TODO - release notes - https://github.com/brave/brave-browser/releases/latest - parse with lynx to get full URLs?
+# Will https://github.com/brave/brave-browser/releases/latest show beta / nightly releases too? If so that might break this
+
+# use this instead
+# https://updates.bravesoftware.com/sparkle/Brave-Browser/stable/appcast.xml
+
+
+
+
+## @TODO - new app release
+##   appcast 'https://updates.bravesoftware.com/sparkle/Brave-Browser/stable/appcast.xml'
+##   url "https://github.com/brave/brave-browser/releases/download/v#{version}/Brave-Browser.dmg"
+##
+##  get-appversion.sh -v /Volumes/Brave\ Browser/Brave\ Browser.app
+##/Volumes/Brave Browser/Brave Browser.app:
+##	CFBundleShortVersionString: 75.0.65.118
+##	CFBundleVersion: 65.118
+##
+## apparently 'CFBundleVersion' is the 'version number' ?
+
+
+# https://laptop-updates.brave.com/latest/osx
+# aka https://brave-browser-downloads.s3.brave.com/latest/Brave-Browser.dmg
