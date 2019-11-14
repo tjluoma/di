b2071e7497dc0f72cd6424368935d1c39954303f
TJ Luoma <github@tjluoma.com>
2019-11-14 17:14:28 -0500

    major overhaul, much better version detection

--- di-finereaderpro.sh
+++ di-finereaderpro.sh
@@ -3,32 +3,15 @@
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
-# Date:	2015-11-09
-
+# Date:	2015-11-09 ; 2019-11-14 update
 
 NAME="$0:t:r"
-
-echo "$NAME: @TODO - this keeps installing the wrong version"
-
-exit 1
 
 INSTALL_TO='/Applications/FineReader.app'
 
 HOMEPAGE="https://www.abbyy.com/en-us/finereader/pro-for-mac/"
 
-## Found this URL -  https://www.abbyy.com/finereader-pro-mac-downloads/
-# curl -sfLS "https://www.abbyy.com/finereader-pro-mac-downloads/" | fgrep 'Build #:' | sed 's#</p>##g ; s#.*>##g' | tr -dc '[0-9]\.'
-## Result: '12.1.13.1043958'
-## 	/Applications/FineReader.app:
-## 		CFBundleShortVersionString: 12.1.13
-## 		CFBundleVersion: 1043958
-##
-## Release Notes: <https://support.abbyy.com/hc/en-us/articles/360001026229-FineReader-Pro-for-Mac-Change-Log>
-##
-## Trial Version <https://downloads.abbyy.com/fr/fr_mac/current/ABBYYFineReaderPro.dmg?secure=EOkGXxyiKvTXMK8VEYyT1g==>
-##
-## The trial version is the same as the URL version just with a different name
-
+RELEASE_NOTES_URL='https://support.abbyy.com/hc/en-us/articles/360001026229-FineReader-Pro-for-Mac-Change-Log'
 
 DOWNLOAD_PAGE="http://fr7.abbyy.com/mac/fr/ABBYY_FineReader_Pro_ESD.dmg"
 
@@ -43,88 +26,54 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-if [ -e "$INSTALL_TO" ]
+LATEST_VERSION=$(curl -sfLS "https://www.abbyy.com/finereader-pro-mac-downloads/" | fgrep 'Build #:' | sed 's#</p>##g ; s#.*>##g' | tr -dc '[0-9]\.')
+
+URL="http://fr7.abbyy.com/mac/fr/ABBYY_FineReader_Pro_ESD.dmg"
+
+if [[ -e "$INSTALL_TO" ]]
 then
 
-		# TurnsOut™, the DYNAMIC_URL doesn't technically need a valid serial number to work,
-		# but we'll use one if we find it, otherwise we'll use a fake one instead.
-	FINE_READER_PRO_SERIAL=$(defaults read com.abbyy.FineReaderPro com.abbyy.fine-reader.serial-number 2>/dev/null || echo 'FSAR00000000000000000000')
+	SHORT_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleShortVersionString)
 
-	BUILD=`defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion`
+	BUNDLE_VERSION=$(defaults read "${INSTALL_TO}/Contents/Info" CFBundleVersion)
 
-	MAJOR_VERSION=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d . -f 1,2`
+	INSTALLED_VERSION=$(echo "$SHORT_VERSION.$BUNDLE_VERSION")
 
-	REVISION_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d . -f 3`
+	autoload is-at-least
 
-	PART_NUMBER=`defaults read "$INSTALL_TO/Contents/Info" PartNumber`
+	is-at-least "$LATEST_VERSION" "$INSTALLED_VERSION"
 
-	# THIS IS NEW as of 2018-07-10
-DYNAMIC_URL="https://www.abbyy.com/en-us/checkforupdates/?Product=FRProMac&Distributive=Retail&Serial=${FINE_READER_PRO_SERIAL}&Language=en&PartNumber=1215%2f10&Target=CheckUpdate&Version=${MAJOR_VERSION}&Revision=${REVISION_NUMBER}&Build=${BUILD}"
+	VERSION_COMPARE="$?"
 
-		##
-		## WARNING: 2018-07-10 - this is web scraping, so very prone to breaking
-		##
-	CURRENT_PART_NUMBER=`curl -sfL "$DYNAMIC_URL" \
-	| fgrep 'Update Part Number:' \
-	| sed 's#<br/>##g' \
-	| tr -dc '[0-9]/'`
-
-	if [[ "$CURRENT_PART_NUMBER" == "$PART_NUMBER" ]]
+	if [ "$VERSION_COMPARE" = "0" ]
 	then
-		echo "$NAME: Up-To-Date ($PART_NUMBER)"
-		## echo "$DYNAMIC_URL" # can be useful for debugging
+		echo "$NAME: Up-To-Date ($INSTALLED_VERSION)"
 		exit 0
-	elif [[ "$CURRENT_PART_NUMBER" == "" ]]
-	then
-		echo "$NAME: \$CURRENT_PART_NUMBER is empty. Web scraping has apparently failed."
-		exit 1
 	fi
 
-fi
+	echo "$NAME: Outdated: $INSTALLED_VERSION vs $LATEST_VERSION"
 
-## If we get here, either an update is available or the app isn't installed at all
-
-
-if [[ "$CURRENT_PART_NUMBER" != "" ]]
-then
-
-	CURRENT_PART_NUMBER_SAFE=`echo "$CURRENT_PART_NUMBER" | tr '/' '.'`
-
-	FILENAME="$HOME/Downloads/FineReaderPro-$CURRENT_PART_NUMBER_SAFE.dmg"
-
-	RENAME_AFTER_INSTALL='no'
+	FIRST_INSTALL='no'
 
 else
-	zmodload zsh/datetime
 
-	DATE=`strftime "%Y-%m-%d" "$EPOCHSECONDS"`
-
-	FILENAME="$HOME/Downloads/FineReaderPro-${DATE}.dmg"
-
-	RENAME_AFTER_INSTALL='yes'
+	FIRST_INSTALL='yes'
 fi
 
-if [[ "$DYNAMIC_URL" != "" ]]
+FILENAME="$HOME/Downloads/${${INSTALL_TO:t:r}// /}-${LATEST_VERSION}.dmg"
+
+if (( $+commands[lynx] ))
 then
-	if (( $+commands[lynx] ))
-	then
 
-		RELEASE_NOTES_URL="$DYNAMIC_URL"
+	RELEASE_NOTES=$(curl -sfLS "$RELEASE_NOTES_URL" \
+		| awk '/<h3>/{i++}i==1' \
+		| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin -nonumbers -nolist)
 
-		(curl -sfLS "$RELEASE_NOTES_URL" \
-			| sed '1,/<article class="release-note">/d; /<\/article>/,$d' \
-			| lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines -stdin ;
-		 echo "\nSource: <$RELEASE_NOTES_URL>") | tee "$FILENAME:r.txt"
+	echo "${RELEASE_NOTES}\n\nSource: ${RELEASE_NOTES_URL}\n\nURL: $URL" | tee "$FILENAME:r.txt"
 
-	fi
 fi
 
 echo "$NAME: Downloading '$URL' to '$FILENAME':"
-
-if [[ "$DYNAMIC_URL" != "" ]]
-then
-	echo "	See <$DYNAMIC_URL> for release notes."
-fi
 
 curl --continue-at - --fail --location --output "$FILENAME" "$URL"
 
@@ -137,6 +86,8 @@ EXIT="$?"
 
 [[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
+(cd "$FILENAME:h" ; echo "\nLocal sha256:" ; shasum -a 256 -p "$FILENAME:t" ) >>| "$FILENAME:r.txt"
+
 echo "$NAME: Mounting $FILENAME:"
 
 MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
@@ -148,6 +99,8 @@ if [[ "$MNTPNT" == "" ]]
 then
 	echo "$NAME: MNTPNT is empty"
 	exit 1
+else
+	echo "$NAME: MNTPNT is $MNTPNT"
 fi
 
 if [[ -e "$INSTALL_TO" ]]
@@ -159,6 +112,17 @@ then
 
 		# move installed version to trash
 	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
+
+	EXIT="$?"
+
+	if [[ "$EXIT" != "0" ]]
+	then
+
+		echo "$NAME: failed to move '$INSTALL_TO' to Trash. ('mv' \$EXIT = $EXIT)"
+
+		exit 1
+	fi
+
 fi
 
 echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
@@ -178,23 +142,23 @@ fi
 
 [[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
 
-echo -n "$NAME: Unmounting $MNTPNT: "
-
-diskutil eject "$MNTPNT"
-
-BUILD=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleVersion)
-
-MAJOR_VERSION=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d . -f 1,2)
-
-REVISION_NUMBER=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d . -f 3)
-
-	# note that this is what shows up on the website, so it's first, but we have to replace '/'
-	# so I chose a '.'
-PART_NUMBER=$(defaults read "$INSTALL_TO/Contents/Info" PartNumber | tr '/' '.')
-
-	# rename the download the have a lot of important bits of information in the name
-mv -vn "$FILENAME" "$HOME/Downloads/FineReaderPro-${PART_NUMBER}-${MAJOR_VERSION}-${REVISION_NUMBER}-${BUILD}.dmg"
+echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
 
 exit 0
 
 # EOF
+
+
+
+## Found this URL -  https://www.abbyy.com/finereader-pro-mac-downloads/
+# curl -sfLS "https://www.abbyy.com/finereader-pro-mac-downloads/" | fgrep 'Build #:' | sed 's#</p>##g ; s#.*>##g' | tr -dc '[0-9]\.'
+## Result: '12.1.13.1043958'
+## 	/Applications/FineReader.app:
+## 		CFBundleShortVersionString: 12.1.13
+## 		CFBundleVersion: 1043958
+##
+## Release Notes: <https://support.abbyy.com/hc/en-us/articles/360001026229-FineReader-Pro-for-Mac-Change-Log>
+##
+## Trial Version <https://downloads.abbyy.com/fr/fr_mac/current/ABBYYFineReaderPro.dmg?secure=EOkGXxyiKvTXMK8VEYyT1g==>
+##
+## The trial version is the same as the URL version just with a different name
