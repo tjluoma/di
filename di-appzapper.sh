2a696052f272d672f11a1927de4e5f73039f2485
TJ Luoma <github@tjluoma.com>
2019-11-16 17:07:50 -0500

    big update; before/after El Capitan

--- di-appzapper.sh
+++ di-appzapper.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh -f
+#!/usr/bin/env zsh -f
 # Purpose: Download and install the latest version of AppZapper
 #
 # From:	Timothy J. Luoma
@@ -22,44 +22,16 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-XML_FEED="https://www.appzapper.com/az2appcast.xml"
+OS_VER=$(sw_vers -productVersion | cut -d. -f2)
 
-RELEASE_NOTES_URL='https://www.appzapper.com/az2sparklenotes.html'
-
-INFO=($(curl -sfL "$XML_FEED" \
-		| tr -s ' ' '\012' \
-		| egrep 'sparkle:version=|url=' \
-		| head -2 \
-		| sort \
-		| awk -F'"' '/^/{print $2}'))
-
-LATEST_VERSION="$INFO[1]"
-
-URL="$INFO[2]"
-
-	# If any of these are blank, we should not continue
-if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+if [ "$OS_VER" -gt "11" ]
 then
-	echo "$NAME: Error: bad data received:
-	INFO: $INFO
-	LATEST_VERSION: $LATEST_VERSION
-	URL: $URL
-	"
-
-	exit 1
-fi
-
-## 2018-07-17 - as of this writing, the XML_FEED only shows version 2.0.1 but the website shows 2.0.2
-## I don't know if the app will ever be updated in the future, so
-## if we get 2.0.1 from the XML_FEED, we're going to silently replace it with 2.0.2
-
-if [[ "$LATEST_VERSION" == "2.0.1" ]]
-then
-
-	URL="https://appzapper.com/downloads/appzapper202.zip"
-
-	LATEST_VERSION="2.0.2"
-
+	URL='https://appzapper.com/downloads/appzapper202.zip'
+	LATEST_VERSION='2.0.2'
+else
+		# this version works back to 10.6.2
+	URL='https://appzapper.com/downloads/AppZapper2.0.1.zip'
+	LATEST_VERSION='2.0.1'
 fi
 
 if [[ -e "$INSTALL_TO" ]]
@@ -88,15 +60,6 @@ then
 fi
 
 FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}.zip"
-
-if (( $+commands[lynx] ))
-then
-
-	( echo "$NAME: Release Notes for $INSTALL_TO:t:r ($LATEST_VERSION):" ;
-		lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines "$RELEASE_NOTES_URL" ;
-		echo "\nSource: <$RELEASE_NOTES_URL>" ) | tee "$FILENAME:r.txt"
-
-fi
 
 echo "$NAME: Downloading '$URL' to '$FILENAME':"
 
@@ -172,5 +135,58 @@ fi
 [[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
 
 exit 0
+
+## The app does not seem to be under active development. If that changes, the following might be useful.
+## For now, it's just reference
+##
+# XML_FEED="https://www.appzapper.com/az2appcast.xml"
+#
+# RELEASE_NOTES_URL='https://www.appzapper.com/az2sparklenotes.html'
+#
+# INFO=($(curl -sfL "$XML_FEED" \
+# 		| tr -s ' ' '\012' \
+# 		| egrep 'sparkle:version=|url=' \
+# 		| head -2 \
+# 		| sort \
+# 		| awk -F'"' '/^/{print $2}'))
+#
+# LATEST_VERSION="$INFO[1]"
+#
+# URL="$INFO[2]"
+#
+# 	# If any of these are blank, we should not continue
+# if [ "$INFO" = "" -o "$LATEST_VERSION" = "" -o "$URL" = "" ]
+# then
+# 	echo "$NAME: Error: bad data received:
+# 	INFO: $INFO
+# 	LATEST_VERSION: $LATEST_VERSION
+# 	URL: $URL
+# 	"
+#
+# 	exit 1
+# fi
+#
+# ## 2018-07-17 - as of this writing, the XML_FEED only shows version 2.0.1 but the website shows 2.0.2
+# ## I don't know if the app will ever be updated in the future, so
+# ## if we get 2.0.1 from the XML_FEED, we're going to silently replace it with 2.0.2
+#
+# if [[ "$LATEST_VERSION" == "2.0.1" ]]
+# then
+#
+# 	URL="https://appzapper.com/downloads/appzapper202.zip"
+#
+# 	LATEST_VERSION="2.0.2"
+#
+# fi
+#
+# if (( $+commands[lynx] ))
+# then
+#
+# 	( echo "$NAME: Release Notes for $INSTALL_TO:t:r ($LATEST_VERSION):" ;
+# 		lynx -dump -nomargins -width='10000' -assume_charset=UTF-8 -pseudo_inlines "$RELEASE_NOTES_URL" ;
+# 		echo "\nSource: <$RELEASE_NOTES_URL>" ) | tee "$FILENAME:r.txt"
+#
+# fi
+
 #
 #EOF
