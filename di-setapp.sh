a0e812223591c9a0c4fc388d9be0db6792cdda9e
TJ Luoma <github@tjluoma.com>
2019-12-29 04:03:26 -0500

    alt installation location

--- di-setapp.sh
+++ di-setapp.sh
@@ -14,7 +14,13 @@ else
 	PATH='/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin'
 fi
 
-INSTALL_TO='/Applications/Setapp.app'
+	# This is where the app will be installed or updated.
+if [[ -d '/Volumes/Applications' ]]
+then
+	INSTALL_TO='/Volumes/Applications/Setapp.app'
+else
+	INSTALL_TO='/Applications/Setapp.app'
+fi
 
 XML_FEED='https://updates.devmate.com/com.setapp.DesktopClient.xml'
 
