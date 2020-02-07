dbe5a802bf4ecaeb4f60226202465f7eebd92b85
TJ Luoma <github@tjluoma.com>
2020-02-07 12:55:00 -0500

    placeholder

---
+++ di-noisebuddy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env zsh -f
+# Purpose:
+#
+# From:	Timothy J. Luoma
+# Mail:	luomat at gmail dot com
+# Date:	2020-02-07
+
+NAME="$0:t:r"
+
+if [[ -e "$HOME/.path" ]]
+then
+	source "$HOME/.path"
+else
+	PATH="$HOME/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin"
+fi
+
+# └─(mba:%) curl -sfLS "https://github.com/insidegui/NoiseBuddy/releases.atom"|less
+
+
+
+exit 0
+#EOF
