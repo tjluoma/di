568e16c108158f63db662b0c91af17fd0d2ad094
TJ Luoma <github@tjluoma.com>
2020-05-16 03:07:52 -0400

    feed outdated? Newer version appeared in MacUpdater

--- di-iterm.sh
+++ di-iterm.sh
@@ -76,7 +76,8 @@ then
 
 	else
 			# Stable releases update rarely but have no serious bugs.
-		XML_FEED='https://iterm2.com/appcasts/final_new.xml'
+		# XML_FEED='https://iterm2.com/appcasts/final_new.xml'
+		XML_FEED='https://iterm2.com/appcasts/final.xml'
 		PREFERS='[Stable]'
 	fi
 
