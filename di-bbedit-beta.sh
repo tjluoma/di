c257c4c64ff3a58519651a2cad797842e074b3fc
TJ Luoma <github@tjluoma.com>
2020-06-23 07:44:10 -0400

    just the URL, for now

---
+++ di-bbedit-beta.sh
@@ -0,0 +1,7 @@
+https://s3.amazonaws.com/BBSW-download/BBEdit-13.1.2_414049.dmg
+
+curl -sfLS "https://versioncheck.barebones.com/BBEdit-414.xml?uuid=0A7434FEE6A46EEE2351D6D055D02A032D878919"
+
+
+curl -sfLS "https://versioncheck.barebones.com/BBEdit-414.xml"
+
