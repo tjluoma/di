c338cdd82195d9947cf26c04a860d60aba956033
TJ Luoma <github@tjluoma.com>
2020-11-30 13:54:58 -0500

    updated for only version 12

--- di-pdfpenpro.sh
+++ di-pdfpenpro.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install PDFPenPro version 8, 9, or 10 from <https://smilesoftware.com/pdfpenpro/>
+# Purpose: Download and install PDFPenPro version 12 from <https://smilesoftware.com/pdfpenpro/>
 #
 # From:	Timothy J. Luoma
 # Mail:	luomat at gmail dot com
@@ -22,97 +22,17 @@ else
 	PATH=/usr/local/scripts:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin
 fi
 
-function use_v8 {
+XML_FEED='https://smilesoftware.com/appcast/PDFpenPro12.xml'
 
-	USE_VERSION='8'
+INFO=$(curl -sfLS "$XML_FEED" | tr -d '\012' | tr -s ' ' ' ')
 
-	LATEST_VERSION="8.3.4"
+LATEST_VERSION=$(echo "$INFO" | sed -e 's#.*sparkle:shortVersionString="##g' -e 's#".*##g')
 
-	LATEST_BUILD="834.1"
+LATEST_BUILD=$(echo "$INFO" | sed -e 's#.*sparkle:version="##g' -e 's#".*##g')
 
-	URL="https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/834.1/1491270914/PDFpenPro-834.1.zip"
+URL=$(echo "$INFO" | sed -e 's#.*enclosure url="##g' -e 's#".*##g')
 
-	ASTERISK='(Note that version 10 is also available.)'
 
-}
-
-function use_v9 {
-
-	USE_VERSION='9'
-
-	LATEST_VERSION="9.2.3"
-
-	LATEST_BUILD="923.0"
-
-	URL="https://dl.smilesoftware.com/com.smileonmymac.PDFpenPro/923.0/1510025685/PDFpenPro-923.0.zip"
-
-	ASTERISK='(Note that version 10 is also available.)'
-
-}
-
-function use_v10 {
-
-	USE_VERSION='10'
-
-	ITUNES_URL='apps.apple.com/us/app/pdfpenpro-10/id1359910358'
-
-		# 2018-07-17 - found alternate URL which seems to have identical info:
-		# https://updates.devmate.com/com.smileonmymac.PDFpenPro.xml
-
-	XML_FEED='https://updates.smilesoftware.com/com.smileonmymac.PDFpenPro.xml'
-
-	INFO=($(curl -sfL "$XML_FEED" \
-			| tr -s ' ' '\012' \
-			| egrep '^(sparkle:version|sparkle:shortVersionString|url=)' \
-			| head -3 \
-			| sort \
-			| awk -F'"' '/^/{print $2}'))
-
-	LATEST_VERSION="$INFO[1]"
-
-	LATEST_BUILD="$INFO[2]"
-
-	URL="$INFO[3]"
-
-		# If any of these are blank, we should not continue
-	if [ "$INFO" = "" -o "$LATEST_BUILD" = "" -o "$URL" = "" -o "$LATEST_VERSION" = "" ]
-	then
-		echo "$NAME: Error: bad data received:
-		INFO: $INFO
-		LATEST_VERSION: $LATEST_VERSION
-		LATEST_BUILD: $LATEST_BUILD
-		URL: $URL
-		"
-
-		exit 1
-	fi
-}
-
-if [[ -e "$INSTALL_TO" ]]
-then
-		# if v8 or 9 are installed, check that. Otherwise, use v10
-	MAJOR_VERSION=$(defaults read "$INSTALL_TO/Contents/Info" CFBundleShortVersionString | cut -d. -f1)
-
-	if   [[ "$MAJOR_VERSION" == "8" ]]
-	then
-		use_v8
-	elif [[ "$MAJOR_VERSION" == "9" ]]
-	then
-		use_v9
-	else
-		use_v10
-	fi
-else
-	if   [ "$1" = "--use8" -o "$1" = "-8" ]
-	then
-		use_v8
-	elif [ "$1" = "--use9" -o "$1" = "-9" ]
-	then
-		use_v9
-	else
-		use_v10
-	fi
-fi
 
 if [[ -e "$INSTALL_TO" ]]
 then
@@ -160,28 +80,31 @@ else
 	FIRST_INSTALL='yes'
 fi
 
-RELEASE_NOTES_URL=$(curl -sfL "$XML_FEED" \
-	| fgrep '<sparkle:releaseNotesLink>' \
-	| head -1 \
-	| sed 's#.*<sparkle:releaseNotesLink>##g ; s#</sparkle:releaseNotesLink>##g')
+FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}_${LATEST_BUILD}.dmg"
 
-FILENAME="$HOME/Downloads/$INSTALL_TO:t:r-${LATEST_VERSION}_${LATEST_BUILD}.zip"
+RELEASE_NOTES_TXT="$FILENAME:r.txt"
 
-if [[ "$USE_VERSION" == "10" ]]
+if [[ -e "$RELEASE_NOTES_TXT" ]]
 then
+
+	cat "$RELEASE_NOTES_TXT"
+
+else
 
 	if (( $+commands[lynx] ))
 	then
 
-		( echo -n "$NAME: Release Notes for " ;
-		curl -sfL "$RELEASE_NOTES_URL" \
-		| fgrep -v 'please note: PDFpenPro 10 is a paid upgrade' \
-		| sed '1,/<div class="dm-rn-head-title-fixed">/d' \
-		| sed '/<\/ul><\/li>/,$d' \
-		| lynx -dump -nomargins -width='90' -assume_charset=UTF-8 -pseudo_inlines -stdin ;
-		echo "\nSource: <$RELEASE_NOTES_URL>" ) | tee "$FILENAME:r.txt"
+		RELEASE_NOTES_HTML=$(echo "$INFO" | \
+			sed	-e 's#Please note that PDFpenPro 12 is a paid upgrade for users of PDFpenPro 11 and earlier.##g' \
+				-e 's#.*\<\!\[CDATA\[##g' \
+				-e 's#\]\]\>.*##g')
+
+	(echo "$RELEASE_NOTES_HTML" \
+		| lynx -dump -width='10000' -display_charset=UTF-8 -assume_charset=UTF-8 -pseudo_inlines -stdin -nomargins;
+		echo "\nSource: <$XML_FEED>" ) | tee "$FILENAME:r.txt"
 
 	fi
+
 fi
 
 ## Download it
@@ -199,65 +122,73 @@ EXIT="$?"
 
 [[ ! -s "$FILENAME" ]] && echo "$NAME: $FILENAME is zero bytes." && rm -f "$FILENAME" && exit 0
 
-UNZIP_TO=$(mktemp -d "${TMPDIR-/tmp/}${NAME}-XXXXXXXX")
-
-echo "$NAME: Unzipping '$FILENAME' to '$UNZIP_TO':"
-
-ditto -xk --noqtn "$FILENAME" "$UNZIP_TO"
+egrep -q '^Local sha256:$' "$FILENAME:r.txt" 2>/dev/null
 
 EXIT="$?"
 
-if [[ "$EXIT" == "0" ]]
+if [ "$EXIT" = "1" -o ! -e "$FILENAME:r.txt" ]
 then
-	echo "$NAME: Unzip successful"
-else
-		# failed
-	echo "$NAME failed (ditto -xkv '$FILENAME' '$UNZIP_TO')"
+	(cd "$FILENAME:h" ; \
+	echo "\n\nLocal sha256:" ; \
+	shasum -a 256 "$FILENAME:t" \
+	)  >>| "$FILENAME:r.txt"
+fi
 
+echo "$NAME: Mounting $FILENAME:"
+
+MNTPNT=$(hdiutil attach -nobrowse -plist "$FILENAME" 2>/dev/null \
+	| fgrep -A 1 '<key>mount-point</key>' \
+	| tail -1 \
+	| sed 's#</string>.*##g ; s#.*<string>##g')
+
+if [[ "$MNTPNT" == "" ]]
+then
+	echo "$NAME: MNTPNT is empty"
 	exit 1
+else
+	echo "$NAME: MNTPNT is $MNTPNT"
 fi
 
 if [[ -e "$INSTALL_TO" ]]
 then
-
+		# Quit app, if running
 	pgrep -xq "$INSTALL_TO:t:r" \
 	&& LAUNCH='yes' \
 	&& osascript -e "tell application \"$INSTALL_TO:t:r\" to quit"
 
-	echo "$NAME: Moving existing (old) '$INSTALL_TO' to '$HOME/.Trash/'."
-
-	mv -vf "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.$INSTALLED_VERSION.app"
+		# move installed version to trash
+	echo "$NAME: moving old installed version to '$HOME/.Trash'..."
+	mv -f "$INSTALL_TO" "$HOME/.Trash/$INSTALL_TO:t:r.${INSTALLED_VERSION}_${INSTALLED_BUILD}.app"
 
 	EXIT="$?"
 
 	if [[ "$EXIT" != "0" ]]
 	then
 
-		echo "$NAME: failed to move existing $INSTALL_TO to $HOME/.Trash/"
+		echo "$NAME: failed to move '$INSTALL_TO' to '$HOME/.Trash'. ('mv' \$EXIT = $EXIT)"
 
 		exit 1
 	fi
 fi
 
-echo "$NAME: Moving new version of '$INSTALL_TO:t' (from '$UNZIP_TO') to '$INSTALL_TO'."
+echo "$NAME: Installing '$MNTPNT/$INSTALL_TO:t' to '$INSTALL_TO': "
 
-	# Move the file out of the folder
-mv -vn "$UNZIP_TO/$INSTALL_TO:t" "$INSTALL_TO"
+ditto --noqtn -v "$MNTPNT/$INSTALL_TO:t" "$INSTALL_TO"
 
 EXIT="$?"
 
-if [[ "$EXIT" = "0" ]]
+if [[ "$EXIT" == "0" ]]
 then
-
-	echo "$NAME: Successfully installed '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
-
+	echo "$NAME: Successfully installed $INSTALL_TO"
 else
-	echo "$NAME: Failed to move '$UNZIP_TO/$INSTALL_TO:t' to '$INSTALL_TO'."
+	echo "$NAME: ditto failed"
 
 	exit 1
 fi
 
 [[ "$LAUNCH" = "yes" ]] && open -a "$INSTALL_TO"
+
+echo -n "$NAME: Unmounting $MNTPNT: " && diskutil eject "$MNTPNT"
 
 exit 0
 
