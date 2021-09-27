5b562feba316749bc4adc5d6601cc205636e27d3
TJ Luoma <github@tjluoma.com>
2021-09-27 18:58:47 -0400

    alternate install dir

--- di-bunch.sh
+++ di-bunch.sh
@@ -7,12 +7,11 @@
 
 NAME="$0:t:r"
 
-if [[ -e "$HOME/.path" ]]
-then
-	source "$HOME/.path"
-fi
+[[ -e "$HOME/.path" ]] && source "$HOME/.path"
 
-INSTALL_TO='/Applications/Bunch.app'
+[[ -e "$HOME/.config/di/defaults.sh" ]] && source "$HOME/.config/di/defaults.sh"
+
+INSTALL_TO="${INSTALL_DIR_ALTERNATE-/Applications}/Bunch.app"
 
 XML_FEED='https://brettterpstra.com/updates/bunch/appcast.xml'
 
