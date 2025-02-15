08517a17ffa087984862de3a3d49efafd78dff6b
TJ Luoma <github@tjluoma.com>
2025-02-15 12:56:27 -0500

    Verified 2025-02-15

--- di-desktopcurtain.sh
+++ di-desktopcurtain.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of Desktop Curtain
+# Purpose: 	Download and install the latest version of Desktop Curtain
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2016-02-01
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2016-02-01
+# Verified: 2025-02-15
 
 NAME="$0:t:r"
 
