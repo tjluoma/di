4705e6e7f52dfb2c163e6f99a7ef1d45cc6b34a6
TJ Luoma <github@tjluoma.com>
2025-02-22 17:12:18 -0500

    Sunset in 2021

--- discontinued/di-virtualhostx.sh
+++ discontinued/di-virtualhostx.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of VirtualHostX
+# Purpose: 	Download and install the latest version of VirtualHostX
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2018-09-07
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2018-09-07
+# Verified:	2025-02-22
 
 if [[ -e "$HOME/.path" ]]
 then
