eb0fdf71638e6bd594a655233aed1d313d393273
TJ Luoma <github@tjluoma.com>
2025-02-23 18:40:08 -0500

    Verified 2025-02-23

--- di-taskpaper.sh
+++ di-taskpaper.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of TaskPaper
+# Purpose: 	Download and install the latest version of TaskPaper
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2015-11-18
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2015-11-18
+# Verified:	2025-02-23
 
 NAME="$0:t:r"
 
