6a013e575f0509a0a6fe617f69d86032af36a0d8
TJ Luoma <github@tjluoma.com>
2025-02-23 19:46:17 -0500

    Verified 2025-02-23

--- di-spamsieve.sh
+++ di-spamsieve.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of SpamSieve from <https://c-command.com/spamsieve/>
+# Purpose: 	Download and install the latest version of SpamSieve from <https://c-command.com/spamsieve/>
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2018-08-12
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2018-08-12
+# Verified:	2025-02-23
 
 NAME="$0:t:r"
 
