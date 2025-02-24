fe04252bf19785e0214605f7e72891a04ead9f7d
TJ Luoma <github@tjluoma.com>
2025-02-24 13:39:26 -0500

    Verified 2025-02-24

--- di-fsnotes.sh
+++ di-fsnotes.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install latest FSNotes
+# Purpose: 	Download and install latest FSNotes
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2019-06-09
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2019-06-09
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
