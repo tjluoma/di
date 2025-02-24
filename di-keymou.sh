1ff11c798c58e3d1c7b9237473655193025530e7
TJ Luoma <github@tjluoma.com>
2025-02-24 13:40:42 -0500

    Verified 2025-02-24

--- di-keymou.sh
+++ di-keymou.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of Keymou
+# Purpose: 	Download and install the latest version of Keymou
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2016-05-22
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2016-05-22
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
