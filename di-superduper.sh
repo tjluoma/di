bc91fdd659c493400eaa83610154ea98ee5ff2f4
TJ Luoma <github@tjluoma.com>
2025-02-23 19:02:38 -0500

    Verified 2025-02-23

--- di-superduper.sh
+++ di-superduper.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of SuperDuper
+# Purpose: 	Download and install the latest version of SuperDuper
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2015-11-19
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2015-11-19
+# Verified:	2025-02-23
 
 NAME="$0:t:r"
 
