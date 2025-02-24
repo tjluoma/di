40948fb1e801dd23a431e5125d049d4387f11da0
TJ Luoma <github@tjluoma.com>
2025-02-24 13:36:20 -0500

    Verified 2025-02-24

--- di-defaultfolderx.sh
+++ di-defaultfolderx.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of Default Folder X
+# Purpose: 	Download and install the latest version of Default Folder X
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2015-11-05
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2015-11-05
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
