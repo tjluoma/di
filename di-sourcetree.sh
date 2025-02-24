43861bbcd1a4802d198b3809dd67f5265c79db1e
TJ Luoma <github@tjluoma.com>
2025-02-23 19:51:19 -0500

    Verified 2025-02-23

--- di-sourcetree.sh
+++ di-sourcetree.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install/update the latest version of Sourcetree from <https://www.sourcetreeapp.com/>
+# Purpose: 	Download and install/update the latest version of Sourcetree from <https://www.sourcetreeapp.com/>
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2018-08-26
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2018-08-26
+# Verified:	2025-02-23
 
 NAME="$0:t:r"
 
