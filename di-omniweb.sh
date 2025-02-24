8f09b2b72f8fb93fc15173f168184175e5a5064f
TJ Luoma <github@tjluoma.com>
2025-02-24 13:43:36 -0500

    Verified 2025-02-24

--- di-omniweb.sh
+++ di-omniweb.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of OmniWeb
+# Purpose: 	Download and install the latest version of OmniWeb
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2018-09-07
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2018-09-07
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
