a6d63b899e3c0c665425b371f779902415ec61bc
TJ Luoma <github@tjluoma.com>
2025-02-24 13:35:35 -0500

    Verified 2025-02-24

--- di-coherencex.sh
+++ di-coherencex.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose:
+# Purpose:	Download and install the latest version of Coherence X
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2021-05-12
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2021-05-12
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
