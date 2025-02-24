391eb66f177f25181547c08f914e004e8e449044
TJ Luoma <github@tjluoma.com>
2025-02-24 13:40:15 -0500

    Verified 2025-02-24

--- di-johnsbackgroundswitcher.sh
+++ di-johnsbackgroundswitcher.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install/update the latest version of John's Background Switcher https://johnsad.ventures/software/backgroundswitcher/
+# Purpose: 	Download and install/update the latest version of John's Background Switcher https://johnsad.ventures/software/backgroundswitcher/
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2020-02-21
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2020-02-21
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
