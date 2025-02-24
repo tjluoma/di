18cdc0ba0a3fe7905b0058225248217f5f895dda
TJ Luoma <github@tjluoma.com>
2025-02-24 13:43:17 -0500

    Verified 2025-02-24

--- di-nottingham.sh
+++ di-nottingham.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of Nottingham
+# Purpose: 	Download and install the latest version of Nottingham
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2018-08-03
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2018-08-03
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
