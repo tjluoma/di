3eecb7e7bdbb486941b39ce111207d41426f52c9
TJ Luoma <github@tjluoma.com>
2025-02-24 13:42:45 -0500

    Verified 2025-02-24

--- di-mimestream.sh
+++ di-mimestream.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose:
+# Purpose:	Download and install the latest version of MIMEStream
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2020-11-23
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2020-11-23
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
