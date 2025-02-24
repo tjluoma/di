c5c6c94b8d58a87c0b0f1d6dfa04944dcfbeae0c
TJ Luoma <github@tjluoma.com>
2025-02-24 13:42:12 -0500

    Verified 2025-02-24

--- di-mailspring.sh
+++ di-mailspring.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: get the latest version of Mailspring from https://getmailspring.com
+# Purpose: 	get the latest version of Mailspring from https://getmailspring.com
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2019-06-20
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2019-06-20
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
