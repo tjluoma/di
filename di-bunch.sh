223933fc2ecc826a287c60bda2fbe3c913f02fd6
TJ Luoma <github@tjluoma.com>
2025-02-24 13:32:24 -0500

    Verified 2025-02-24

--- di-bunch.sh
+++ di-bunch.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose:
+# Purpose:	Download and install the latest version of Bunch
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2021-07-03
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2021-07-03
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
