b6fcae2e225ff07020e55e2195ace42985e93fa0
TJ Luoma <github@tjluoma.com>
2025-02-24 13:40:21 -0500

    Verified 2025-02-24

--- di-keepingyouawake.sh
+++ di-keepingyouawake.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of KeepingYouAwake
+# Purpose: 	Download and install the latest version of KeepingYouAwake
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2015-12-08
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2015-12-08
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
