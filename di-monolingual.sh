917bce975983bb97e2ccc622c2a72c57de9f1539
TJ Luoma <github@tjluoma.com>
2025-02-24 13:42:53 -0500

    Verified 2025-02-24

--- di-monolingual.sh
+++ di-monolingual.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of Monolingual
+# Purpose: 	Download and install the latest version of Monolingual
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2016-05-29
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2016-05-29
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
