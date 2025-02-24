cd6b2059844f937ad0b857a619a8615f60e16c26
TJ Luoma <github@tjluoma.com>
2025-02-24 13:41:02 -0500

    Verified 2025-02-24

--- di-loopback.sh
+++ di-loopback.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install the latest version of Loopback
+# Purpose: 	Download and install the latest version of Loopback
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2021-01-19
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2021-01-19
+# Verified:	2025-02-24
 
 [[ -e "$HOME/.path" ]] && source "$HOME/.path"
 
