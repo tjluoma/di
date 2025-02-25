aa175762e692dec7d459557532ff8616db4768f5
TJ Luoma <github@tjluoma.com>
2025-02-24 21:15:33 -0500

    Verified 2025-02-24

--- di-hookmark.sh
+++ di-hookmark.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env zsh -f
-# Purpose: Download and install/update the latest version of Hook from <https://hookproductivity.com/>
+# Purpose: 	Download and install/update the latest version of Hook from <https://hookproductivity.com/>
 #
-# From:	Timothy J. Luoma
-# Mail:	luomat at gmail dot com
-# Date:	2019-10-12
+# From:		Timothy J. Luoma
+# Mail:		luomat at gmail dot com
+# Date:		2019-10-12
+# Verified:	2025-02-24
 
 NAME="$0:t:r"
 
